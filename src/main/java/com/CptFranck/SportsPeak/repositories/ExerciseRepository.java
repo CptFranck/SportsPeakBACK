0f677ca73f198a7ac48326e3436229d8f45df09e
CptFranck <francois.caillet71@gmail.com>
2024-05-14 22:29:43 +0200

    Bug fix

--- src/main/java/com/CptFranck/SportsPeak/repositories/ExerciseRepository.java
+++ src/main/java/com/CptFranck/SportsPeak/repositories/ExerciseRepository.java
@@ -1,6 +1,7 @@
 package com.CptFranck.SportsPeak.repositories;
 
+import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ExerciseRepository extends CrudRepository<ExerciseRepository, Long> {
+public interface ExerciseRepository extends CrudRepository<ExerciseEntity, Long> {
 }
