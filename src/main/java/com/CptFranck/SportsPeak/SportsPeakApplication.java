9dfe05aeaa97b1e3da9f001969307e1b0ee9f44f
CptFranck <francois.caillet71@gmail.com>
2024-05-14 14:53:19 +0200

    First commit

---
+++ src/main/java/com/CptFranck/SportsPeak/SportsPeakApplication.java
@@ -0,0 +1,13 @@
+package com.CptFranck.SportsPeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsPeakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsPeakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/CptFranck/SportsPeak/SportsPeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CptFranck.SportsPeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsPeakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
