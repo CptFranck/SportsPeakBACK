c48a4bbf0ad176b87d5395f010dc9bdd27e8e094
CptFranck <francois.caillet71@gmail.com>
2024-05-14 17:54:10 +0200

    Add MuscleRepository

---
+++ src/main/java/com/CptFranck/SportsPeak/repositories/MuscleRepository.java
@@ -0,0 +1,7 @@
+package com.CptFranck.SportsPeak.repositories;
+
+import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MuscleRepository extends CrudRepository<MuscleEntity, Long> {
+}
