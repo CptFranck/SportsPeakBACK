054b7f7f58460279e744920ea6943472f9e61781
CptFranck <francois.caillet71@gmail.com>
2024-05-14 18:19:54 +0200

    Add ModelMapper implementation for Muscle entity

---
+++ src/main/java/com/CptFranck/SportsPeak/mappers/Impl/MuscleMapperImpl.java
@@ -0,0 +1,27 @@
+package com.CptFranck.SportsPeak.mappers.Impl;
+
+import com.CptFranck.SportsPeak.domain.dto.MuscleDto;
+import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
+import com.CptFranck.SportsPeak.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MuscleMapperImpl implements Mapper<MuscleEntity, MuscleDto> {
+
+    private final ModelMapper modelMapper;
+
+    public MuscleMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public MuscleDto mapTo(MuscleEntity muscleEntity) {
+        return modelMapper.map(muscleEntity, MuscleDto.class);
+    }
+
+    @Override
+    public MuscleEntity mapFrom(MuscleDto muscleDto) {
+        return modelMapper.map(muscleDto, MuscleEntity.class);
+    }
+}
