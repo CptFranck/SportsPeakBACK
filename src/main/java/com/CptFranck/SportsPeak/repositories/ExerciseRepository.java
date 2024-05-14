27246d49b70f322e036ea382f569782c880c541b
CptFranck <francois.caillet71@gmail.com>
2024-05-14 17:53:34 +0200

    Add ExerciseRepository

---
+++ src/main/java/com/CptFranck/SportsPeak/repositories/ExerciseRepository.java
@@ -0,0 +1,6 @@
+package com.CptFranck.SportsPeak.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExerciseRepository extends CrudRepository<ExerciseRepository, Long> {
+}
