7a48791c5fb3b1ae7357dc60da240ab9046e807a
CptFranck <francois.caillet71@gmail.com>
2024-05-14 18:20:28 +0200

    Add ModelMapper implementation for Exercise entity

---
+++ src/main/java/com/CptFranck/SportsPeak/mappers/Impl/ExerciseMapperImpl.java
@@ -0,0 +1,27 @@
+package com.CptFranck.SportsPeak.mappers.Impl;
+
+import com.CptFranck.SportsPeak.domain.dto.ExerciseDto;
+import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
+import com.CptFranck.SportsPeak.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExerciseMapperImpl implements Mapper<ExerciseEntity, ExerciseDto> {
+
+    private final ModelMapper modelMapper;
+
+    public ExerciseMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public ExerciseDto mapTo(ExerciseEntity exerciseEntity) {
+        return modelMapper.map(exerciseEntity, ExerciseDto.class);
+    }
+
+    @Override
+    public ExerciseEntity mapFrom(ExerciseDto exerciseDto) {
+        return modelMapper.map(exerciseDto, ExerciseEntity.class);
+    }
+}
