a69d9f5e597b9d44a538be62a3404eef17aa5e29
CptFranck <francois.caillet71@gmail.com>
2024-05-14 22:29:26 +0200

    Removing obsolete entities

--- src/main/java/com/CptFranck/SportsPeak/repositories/ClassifiedExerciseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.CptFranck.SportsPeak.repositories;
-
-import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
-import com.CptFranck.SportsPeak.domain.entity.compositeKey.ClassifiedExerciseKey;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClassifiedExerciseRepository extends CrudRepository<MuscleEntity, ClassifiedExerciseKey> {
-}
