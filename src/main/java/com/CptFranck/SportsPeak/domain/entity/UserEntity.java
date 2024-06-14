6cbeed524a3bc2dca1f379a3a6fd4225b75a0b34
CptFranck <francois.caillet71@gmail.com>
2024-06-14 11:15:51 +0200

    Add User entity

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/entity/UserEntity.java
@@ -0,0 +1,36 @@
+package com.CptFranck.SportsPeak.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "app_user_id_seq")
+    @SequenceGenerator(name = "app_user_id_seq", sequenceName = "app_user_id_seq", allocationSize = 1)
+    private Long id;
+    
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "firstName", length = 50, nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", length = 50, nullable = false)
+    private String lastName;
+
+    @Column(name = "username", length = 50, nullable = false)
+    private String username;
+
+    @Column(name = "password", length = 50, nullable = false)
+    private String password;
+}
