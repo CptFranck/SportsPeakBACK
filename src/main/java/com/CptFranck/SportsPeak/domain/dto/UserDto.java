44dd3552a73fda681f82874604568eaedfc93c2b
CptFranck <francois.caillet71@gmail.com>
2024-06-14 11:04:49 +0200

    Add User Dto input

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/dto/UserDto.java
@@ -0,0 +1,25 @@
+package com.CptFranck.SportsPeak.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String token;
+}
