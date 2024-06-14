4fcfa6f3ccf7e6887d71af54d9ecbca0a8f95e63
CptFranck <francois.caillet71@gmail.com>
2024-06-14 11:16:08 +0200

    Add User repository

---
+++ src/main/java/com/CptFranck/SportsPeak/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.CptFranck.SportsPeak.repositories;
+
+import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String login);
+}
