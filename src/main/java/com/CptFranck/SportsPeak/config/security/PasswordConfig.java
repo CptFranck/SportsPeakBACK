f75d97d4edffe7d0568215ba5abaa9e3baa637e2
CptFranck <francois.caillet71@gmail.com>
2024-06-17 16:06:27 +0200

    Move password config file

--- src/main/java/com/CptFranck/SportsPeak/config/security/PasswordConfig.java
+++ src/main/java/com/CptFranck/SportsPeak/config/security/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.CptFranck.SportsPeak.config;
+package com.CptFranck.SportsPeak.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
