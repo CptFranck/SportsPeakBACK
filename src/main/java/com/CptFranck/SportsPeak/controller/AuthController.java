57ebf4df73e460da4b3eb328a5b069ea89022e4d
CptFranck <francois.caillet71@gmail.com>
2024-06-17 17:08:32 +0200

    Refactoring AuthController

--- src/main/java/com/CptFranck/SportsPeak/controller/AuthController.java
+++ src/main/java/com/CptFranck/SportsPeak/controller/AuthController.java
@@ -1,28 +1,38 @@
 package com.CptFranck.SportsPeak.controller;
 
-import com.CptFranck.SportsPeak.config.UserAuthProvider;
+import com.CptFranck.SportsPeak.config.security.JwtProvider;
 import com.CptFranck.SportsPeak.domain.dto.UserDto;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
 import com.CptFranck.SportsPeak.domain.input.credentials.InputCredentials;
 import com.CptFranck.SportsPeak.domain.input.user.InputNewUser;
 import com.CptFranck.SportsPeak.domain.input.user.InputUser;
+import com.CptFranck.SportsPeak.domain.role.UserRole;
 import com.CptFranck.SportsPeak.mappers.Mapper;
 import com.CptFranck.SportsPeak.service.AuthService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.nio.CharBuffer;
 
 @DgsComponent
 public class AuthController {
 
+    private final AuthenticationManager authenticationManager;
     private final AuthService authService;
-    private final UserAuthProvider userAuthProvider;
+    private final JwtProvider userAuthProvider;
     private final Mapper<UserEntity, UserDto> userMapper;
 
-    public AuthController(AuthService authService, UserAuthProvider userAuthProvider, Mapper<UserEntity, UserDto> userMapper) {
+    public AuthController(AuthenticationManager authenticationManager,
+                          AuthService authService,
+                          JwtProvider userAuthProvider,
+                          Mapper<UserEntity, UserDto> userMapper) {
+        this.authenticationManager = authenticationManager;
         this.authService = authService;
         this.userAuthProvider = userAuthProvider;
         this.userMapper = userMapper;
@@ -31,14 +41,20 @@ public class AuthController {
     @DgsQuery
     public UserDto login(@InputArgument InputCredentials inputCredentials) throws Exception {
         UserDto user = userMapper.mapTo(authService.login(inputCredentials));
-        user.setToken(userAuthProvider.createToken(user));
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(inputCredentials.getEmail(), CharBuffer.wrap(inputCredentials.getPassword())));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        user.setToken(userAuthProvider.generateToken(authentication));
         return user;
     }
 
     @DgsMutation
     public UserDto register(@InputArgument InputNewUser inputNewUser) {
         UserDto user = userMapper.mapTo(authService.register(this.inputToEntity(inputNewUser)));
-        user.setToken(userAuthProvider.createToken(user));
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(inputNewUser.getEmail(), CharBuffer.wrap(inputNewUser.getPassword())));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        user.setToken(userAuthProvider.generateToken(authentication));
         return user;
     }
 
@@ -53,7 +69,8 @@ public class AuthController {
                 inputNewUser.getFirstName(),
                 inputNewUser.getLastName(),
                 inputNewUser.getUsername(),
-                CharBuffer.wrap(inputNewUser.getPassword()).toString()
+                CharBuffer.wrap(inputNewUser.getPassword()).toString(),
+                UserRole.USER
         );
     }
 }
