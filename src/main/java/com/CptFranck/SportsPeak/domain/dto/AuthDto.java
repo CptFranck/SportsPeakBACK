51bf22d76a404b074a504d2efa0a9376586ef1a1
CptFranck <francois.caillet71@gmail.com>
2024-06-18 12:00:23 +0200

    Add AuthDto type

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/dto/AuthDto.java
@@ -0,0 +1,15 @@
+package com.CptFranck.SportsPeak.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthDto {
+    private String accessToken;
+    private String tokenType = "Bearer ";
+    private UserDto user;
+
+    public AuthDto(String accessToken, UserDto user) {
+        this.user = user;
+        this.accessToken = accessToken;
+    }
+}
