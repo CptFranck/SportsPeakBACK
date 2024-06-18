236e6c8889cf9c275b9f97495c8beb9360b12d92
CptFranck <francois.caillet71@gmail.com>
2024-06-18 10:49:57 +0200

    Fix JwtAuthenticationFilter constructor with autowired annotation

--- src/main/java/com/CptFranck/SportsPeak/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/CptFranck/SportsPeak/config/security/JwtAuthenticationFilter.java
@@ -6,24 +6,24 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtProvider jwtProvider;
-    private final UserServiceImpl userService;
-
-    public JwtAuthenticationFilter(JwtProvider jwtProvider, UserServiceImpl userService) {
-        this.jwtProvider = jwtProvider;
-        this.userService = userService;
-    }
+    @Autowired
+    private JwtProvider jwtProvider;
+    @Autowired
+    private UserServiceImpl userServiceImpl;
 
     @Override
     protected void doFilterInternal(
@@ -34,7 +34,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = getJWTFromRequest(request);
         if (StringUtils.hasText(token) && this.jwtProvider.validateToken(token)) {
             String username = this.jwtProvider.getUsernameFromJWT(token);
-            UserDetails userDetails = userService.loadUserByUsername(username);
+            UserDetails userDetails = userServiceImpl.loadUserByUsername(username);
             UsernamePasswordAuthenticationToken authenticationToken =
                     new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
