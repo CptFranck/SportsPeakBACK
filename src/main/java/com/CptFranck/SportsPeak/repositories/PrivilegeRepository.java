22428773f8fe821ea24b84f48792412bb8927e2b
CptFranck <francois.caillet71@gmail.com>
2024-06-19 12:36:23 +0200

    Add Privilege repository

---
+++ src/main/java/com/CptFranck/SportsPeak/repositories/PrivilegeRepository.java
@@ -0,0 +1,7 @@
+package com.CptFranck.SportsPeak.repositories;
+
+import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrivilegeRepository extends CrudRepository<PrivilegeEntity, Long> {
+}
