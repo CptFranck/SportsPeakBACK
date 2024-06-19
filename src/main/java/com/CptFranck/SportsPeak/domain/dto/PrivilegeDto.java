5118eed6268039cfb86a2a83609e6ef6e0e9f5c3
CptFranck <francois.caillet71@gmail.com>
2024-06-19 12:36:13 +0200

    Add Privilege Dto

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/dto/PrivilegeDto.java
@@ -0,0 +1,21 @@
+package com.CptFranck.SportsPeak.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrivilegeDto {
+
+    private Long id;
+
+    private String name;
+
+    private Collection<RoleDto> roles;
+}
