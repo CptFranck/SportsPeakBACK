6dbb9190852d5aaea20bcacd9751e0e7fb010e09
CptFranck <francois.caillet71@gmail.com>
2024-06-19 12:37:55 +0200

    Add role entity

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/entity/RoleEntity.java
@@ -0,0 +1,35 @@
+package com.CptFranck.SportsPeak.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_id_seq")
+    @SequenceGenerator(name = "role_id_seq", sequenceName = "role_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<UserEntity> users;
+
+    @ManyToMany
+    @JoinTable(name = "roles_privileges",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "privilege_id"))
+    private Collection<PrivilegeEntity> privileges;
+}
