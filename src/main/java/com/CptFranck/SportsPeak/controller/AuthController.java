d2f47372504b3b06bd2999b48a45eb654635cff7
CptFranck <francois.caillet71@gmail.com>
2024-06-19 12:51:33 +0200

    Refactor auth controller

--- src/main/java/com/CptFranck/SportsPeak/controller/AuthController.java
+++ src/main/java/com/CptFranck/SportsPeak/controller/AuthController.java
@@ -3,13 +3,13 @@ package com.CptFranck.SportsPeak.controller;
 import com.CptFranck.SportsPeak.config.security.JwtProvider;
 import com.CptFranck.SportsPeak.domain.dto.AuthDto;
 import com.CptFranck.SportsPeak.domain.dto.UserDto;
+import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
-import com.CptFranck.SportsPeak.domain.enumType.UserRole;
 import com.CptFranck.SportsPeak.domain.input.credentials.InputCredentials;
 import com.CptFranck.SportsPeak.domain.input.user.InputNewUser;
-import com.CptFranck.SportsPeak.domain.input.user.InputUser;
 import com.CptFranck.SportsPeak.mappers.Mapper;
 import com.CptFranck.SportsPeak.service.AuthService;
+import com.CptFranck.SportsPeak.service.RoleService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
@@ -19,20 +19,26 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+
 @DgsComponent
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final AuthService authService;
+    private final RoleService roleService;
     private final JwtProvider userAuthProvider;
     private final Mapper<UserEntity, UserDto> userMapper;
 
     public AuthController(AuthenticationManager authenticationManager,
-                          AuthService authService,
+                          AuthService authService, RoleService roleService,
                           JwtProvider userAuthProvider,
                           Mapper<UserEntity, UserDto> userMapper) {
         this.authenticationManager = authenticationManager;
         this.authService = authService;
+        this.roleService = roleService;
         this.userAuthProvider = userAuthProvider;
         this.userMapper = userMapper;
     }
@@ -56,18 +62,18 @@ public class AuthController {
     }
 
     private UserEntity inputToEntity(InputNewUser inputNewUser) {
-        Long id = null;
-        if (inputNewUser instanceof InputUser) {
-            id = ((InputUser) inputNewUser).getId();
-        }
+        Optional<RoleEntity> userRole = roleService.findByName("ROLE_USER");
+        Collection<RoleEntity> roles = Set.of(userRole.orElseThrow(
+                () -> new RuntimeException("ROLE_USER not found or does not exist")));
+
         return new UserEntity(
-                id,
+                null,
                 inputNewUser.getEmail(),
                 inputNewUser.getFirstName(),
                 inputNewUser.getLastName(),
                 inputNewUser.getUsername(),
                 inputNewUser.getPassword(),
-                UserRole.USER
+                roles
         );
     }
 }
