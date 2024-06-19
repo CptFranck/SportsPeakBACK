a1e02de0b365c50c64170638bb3dae59360eb2fa
CptFranck <francois.caillet71@gmail.com>
2024-06-19 12:36:53 +0200

    Add Privilege mapper

---
+++ src/main/java/com/CptFranck/SportsPeak/mappers/Impl/PrivilegeMapperImpl.java
@@ -0,0 +1,28 @@
+package com.CptFranck.SportsPeak.mappers.Impl;
+
+import com.CptFranck.SportsPeak.domain.dto.PrivilegeDto;
+import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
+import com.CptFranck.SportsPeak.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrivilegeMapperImpl implements Mapper<PrivilegeEntity, PrivilegeDto> {
+
+    private final ModelMapper modelMapper;
+
+    public PrivilegeMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public PrivilegeDto mapTo(PrivilegeEntity userEntity) {
+        return modelMapper.map(userEntity, PrivilegeDto.class);
+    }
+
+    @Override
+    public PrivilegeEntity mapFrom(PrivilegeDto userDto) {
+        return modelMapper.map(userDto, PrivilegeEntity.class);
+    }
+
+}
