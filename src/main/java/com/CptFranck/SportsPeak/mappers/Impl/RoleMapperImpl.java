c1e7a05d7e2814b60133c37112112e75f3b78147
CptFranck <francois.caillet71@gmail.com>
2024-06-19 12:38:27 +0200

    Add role mapper

---
+++ src/main/java/com/CptFranck/SportsPeak/mappers/Impl/RoleMapperImpl.java
@@ -0,0 +1,28 @@
+package com.CptFranck.SportsPeak.mappers.Impl;
+
+import com.CptFranck.SportsPeak.domain.dto.RoleDto;
+import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
+import com.CptFranck.SportsPeak.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleMapperImpl implements Mapper<RoleEntity, RoleDto> {
+
+    private final ModelMapper modelMapper;
+
+    public RoleMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public RoleDto mapTo(RoleEntity userEntity) {
+        return modelMapper.map(userEntity, RoleDto.class);
+    }
+
+    @Override
+    public RoleEntity mapFrom(RoleDto userDto) {
+        return modelMapper.map(userDto, RoleEntity.class);
+    }
+
+}
