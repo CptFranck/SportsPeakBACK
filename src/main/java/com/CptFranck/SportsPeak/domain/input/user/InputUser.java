c0bd19a36d2005622ba9e39025d282636c9a7478
CptFranck <francois.caillet71@gmail.com>
2024-06-19 12:50:17 +0200

    Refactor user java input

--- src/main/java/com/CptFranck/SportsPeak/domain/input/user/InputUser.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/user/InputUser.java
@@ -3,10 +3,14 @@ package com.CptFranck.SportsPeak.domain.input.user;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.List;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class InputUser extends InputNewUser {
 
     private Long id;
+
+    private List<Long> roleIds;
 
 }
