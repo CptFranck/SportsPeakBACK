3bd0ea7585a46427a519eaa5e4a45d649f8329e3
CptFranck <francois.caillet71@gmail.com>
2024-06-25 13:00:04 +0200

    Refactor user controller

--- src/main/java/com/CptFranck/SportsPeak/controller/UserController.java
+++ src/main/java/com/CptFranck/SportsPeak/controller/UserController.java
@@ -3,10 +3,8 @@ package com.CptFranck.SportsPeak.controller;
 import com.CptFranck.SportsPeak.domain.dto.UserDto;
 import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
-import com.CptFranck.SportsPeak.domain.input.user.InputNewUser;
-import com.CptFranck.SportsPeak.domain.input.user.InputUser;
+import com.CptFranck.SportsPeak.domain.input.user.*;
 import com.CptFranck.SportsPeak.mappers.Mapper;
-import com.CptFranck.SportsPeak.service.AuthService;
 import com.CptFranck.SportsPeak.service.RoleService;
 import com.CptFranck.SportsPeak.service.UserService;
 import com.netflix.graphql.dgs.DgsComponent;
@@ -25,13 +23,11 @@ public class UserController {
 
     private final RoleService roleService;
     private final UserService userService;
-    private final AuthService authService;
     private final Mapper<UserEntity, UserDto> userMapper;
 
-    public UserController(RoleService roleService, UserService userService, AuthService authService, Mapper<UserEntity, UserDto> userMapper) {
+    public UserController(RoleService roleService, UserService userService, Mapper<UserEntity, UserDto> userMapper) {
         this.roleService = roleService;
         this.userService = userService;
-        this.authService = authService;
         this.userMapper = userMapper;
     }
 
@@ -50,17 +46,41 @@ public class UserController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DgsMutation
-    public UserDto addUser(@InputArgument InputNewUser inputNewUser) {
-        return userMapper.mapTo(authService.register(this.inputToEntity(inputNewUser)));
+    public UserDto modifyUserIdentity(@InputArgument InputUserIdentity inputUserIdentity) {
+        UserEntity userEntity = userService.changeIdentity(inputUserIdentity.getId(),
+                inputUserIdentity.getFirstName(),
+                inputUserIdentity.getLastName());
+        return userMapper.mapTo(userEntity);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DgsMutation
-    public UserDto modifyUser(@InputArgument InputUser inputUser) {
-        if (!userService.exists(inputUser.getId())) {
-            return null;
-        }
-        return userMapper.mapTo(inputToEntity(inputUser));
+    public UserDto modifyUserRoles(@InputArgument InputUserRoles inputUserRoles) {
+        Set<Long> roleIds = Sets.newHashSet((inputUserRoles.getRoleIds()));
+        Set<RoleEntity> roles = roleService.findMany(roleIds);
+        UserEntity userEntity = userService.changeRoles(inputUserRoles.getId(), roles);
+        return userMapper.mapTo(userEntity);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DgsMutation
+    public UserDto modifyUserEmail(@InputArgument InputUserEmail inputUserEmail) {
+        UserEntity userEntity = userService.changeEmail(inputUserEmail.getId(), inputUserEmail.getNewEmail());
+        return userMapper.mapTo(userEntity);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DgsMutation
+    public UserDto modifyUserUsername(@InputArgument InputUserUsername inputUserUsername) {
+        UserEntity userEntity = userService.changeUsername(inputUserUsername.getId(), inputUserUsername.getNewUsername());
+        return userMapper.mapTo(userEntity);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DgsMutation
+    public UserDto modifyUserPassword(@InputArgument InputUserPassword inputUserPassword) {
+        UserEntity userEntity = userService.changePassword(inputUserPassword.getId(), inputUserPassword.getPassword());
+        return userMapper.mapTo(userEntity);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -71,25 +91,5 @@ public class UserController {
         }
         userService.delete(userId);
         return userId;
-    }
-
-    private UserEntity inputToEntity(InputNewUser inputNewUser) {
-        Set<Long> roleIds = Sets.newHashSet((inputNewUser.getRoleIds()));
-        Set<RoleEntity> roles = roleService.findMany(roleIds);
-
-        Long id = null;
-        if (inputNewUser instanceof InputUser) {
-            id = ((InputUser) inputNewUser).getId();
-        }
-
-        return new UserEntity(
-                id,
-                inputNewUser.getEmail(),
-                inputNewUser.getFirstName(),
-                inputNewUser.getLastName(),
-                inputNewUser.getUsername(),
-                inputNewUser.getPassword(),
-                roles
-        );
     }
 }
