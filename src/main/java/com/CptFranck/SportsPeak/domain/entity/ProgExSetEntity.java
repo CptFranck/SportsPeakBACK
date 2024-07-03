e4924a893d13370411aa6f0f93ee63504aad9898
CptFranck <francois.caillet71@gmail.com>
2024-07-03 17:28:57 +0200

    Add Prog ex set

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/entity/ProgExSetEntity.java
@@ -0,0 +1,45 @@
+package com.CptFranck.SportsPeak.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Duration;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "prog_ex_set")
+public class ProgExSetEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "prog_ex_set_id_seq")
+    @SequenceGenerator(name = "prog_ex_set_id_seq", sequenceName = "prog_ex_set_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "set_number", nullable = false)
+    private Integer setNumber;
+
+    @Column(name = "repetition_number", nullable = false)
+    private Integer repetitionNumber;
+
+    @Column(name = "weight", nullable = false)
+    private Integer weight;
+
+    @Column(name = "exercise_time", nullable = false)
+    private Duration exerciseTime;
+
+    @Column(name = "rest_time", nullable = false)
+    private Duration restTime;
+
+    @ManyToOne
+    private ProgExerciseEntity progExercise;
+
+    @OneToMany(mappedBy = "progExSet")
+    private Set<SetPerformanceLogsEntity> setPerformanceLogs;
+}
