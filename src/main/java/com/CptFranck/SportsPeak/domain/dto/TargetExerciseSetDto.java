72d9b21f8640f97084b4c99b42e887716c9bbd75
CptFranck <francois.caillet71@gmail.com>
2024-07-04 16:46:48 +0200

    Add Target Exercise Set Dto

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/dto/TargetExerciseSetDto.java
@@ -0,0 +1,37 @@
+package com.CptFranck.SportsPeak.domain.dto;
+
+import lombok.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TargetExerciseSetDto {
+
+    private Long id;
+
+    private Integer setNumber;
+
+    private Integer repetitionNumber;
+
+    private Integer weight;
+
+    private String weightUnit;
+
+    private Duration physicalExertionUnitTime;
+
+    private Duration restTime;
+
+    private LocalDate creationDate;
+
+    private ProgExerciseDto progExercise;
+
+    private TargetExerciseSetDto exerciseSet;
+
+    @EqualsAndHashCode.Exclude
+    private Set<PerformanceLogDto> performanceLogs;
+}
