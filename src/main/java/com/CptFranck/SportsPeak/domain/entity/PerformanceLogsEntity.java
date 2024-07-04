b249685407b0e1bb37274d5771ae0953d09ca547
CptFranck <francois.caillet71@gmail.com>
2024-07-04 14:42:59 +0200

    Update SetPerformanceLogsEntity

--- src/main/java/com/CptFranck/SportsPeak/domain/entity/PerformanceLogsEntity.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/entity/PerformanceLogsEntity.java
@@ -7,18 +7,19 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Duration;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "prog_ex_set")
-public class SetPerformanceLogsEntity {
+@Table(name = "performance_logs")
+public class PerformanceLogsEntity {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "prog_ex_set_id_seq")
-    @SequenceGenerator(name = "prog_ex_set_id_seq", sequenceName = "prog_ex_set_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "performance_logs_id_seq")
+    @SequenceGenerator(name = "performance_logs_id_seq", sequenceName = "performance_logs_id_seq", allocationSize = 1)
     private Long id;
 
     @Column(name = "set_number", nullable = false)
@@ -30,12 +31,16 @@ public class SetPerformanceLogsEntity {
     @Column(name = "weight", nullable = false)
     private Integer weight;
 
-    @Column(name = "exercise_time", nullable = false)
-    private Duration exerciseTime;
+    @Column(name = "weight_unit", length = 50, nullable = false)
+    private String weightUnit;
 
     @Column(name = "rest_time", nullable = false)
     private Duration restTime;
 
+    @Column(name = "log_date", nullable = false)
+    private LocalDate logDate;
+
     @ManyToOne
-    private ProgExSetEntity progExSet;
+    @Column(name = "target_exercise_set_id", nullable = false)
+    private TargetExerciseSetEntity targetExerciseSet;
 }
