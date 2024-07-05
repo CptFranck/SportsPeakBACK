6ed59c982068b9d0506be04b737df547623bc965
CptFranck <francois.caillet71@gmail.com>
2024-07-05 13:53:47 +0200

    Add Performance Log Mapper Impl

---
+++ src/main/java/com/CptFranck/SportsPeak/mappers/Impl/PerformanceLogMapperImpl.java
@@ -0,0 +1,27 @@
+package com.CptFranck.SportsPeak.mappers.Impl;
+
+import com.CptFranck.SportsPeak.domain.dto.PerformanceLogDto;
+import com.CptFranck.SportsPeak.domain.entity.PerformanceLogEntity;
+import com.CptFranck.SportsPeak.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PerformanceLogMapperImpl implements Mapper<PerformanceLogEntity, PerformanceLogDto> {
+
+    private final ModelMapper modelMapper;
+
+    public PerformanceLogMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public PerformanceLogDto mapTo(PerformanceLogEntity performanceLogEntity) {
+        return modelMapper.map(performanceLogEntity, PerformanceLogDto.class);
+    }
+
+    @Override
+    public PerformanceLogEntity mapFrom(PerformanceLogDto performanceLogDto) {
+        return modelMapper.map(performanceLogDto, PerformanceLogEntity.class);
+    }
+}
