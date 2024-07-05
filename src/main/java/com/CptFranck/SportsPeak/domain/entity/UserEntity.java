a043f0a251e9e76ed2f79cc2239109a8001d5fdb
CptFranck <francois.caillet71@gmail.com>
2024-07-05 17:27:36 +0200

    Fix wrong table name

--- src/main/java/com/CptFranck/SportsPeak/domain/entity/UserEntity.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/entity/UserEntity.java
@@ -44,7 +44,7 @@ public class UserEntity {
     private Set<RoleEntity> roles;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "users_prog_exercise",
+    @JoinTable(name = "users_prog_exercises",
             joinColumns = @JoinColumn(name = "app_user_id"),
             inverseJoinColumns = @JoinColumn(name = "prog_exercise_id"))
     private Set<ProgExerciseEntity> progExercises;
