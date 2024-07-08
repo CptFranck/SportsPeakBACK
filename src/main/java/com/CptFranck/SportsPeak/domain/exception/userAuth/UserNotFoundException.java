cbf478f8e6d47752bc6714eb80a7afef070c5f55
CptFranck <francois.caillet71@gmail.com>
2024-07-08 14:47:52 +0200

    Modify UserNotFoundException to add id to the log

--- src/main/java/com/CptFranck/SportsPeak/domain/exception/userAuth/UserNotFoundException.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/exception/userAuth/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.CptFranck.SportsPeak.domain.exception.userAuth;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException() {
-        super("User not found");
+    public UserNotFoundException(Long id) {
+        super(String.format("User with id %s has been not found", id.toString()));
     }
 }
