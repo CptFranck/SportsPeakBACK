552662c03b78f3bdcb2fe26fe7cc4ae5f798684f
CptFranck <francois.caillet71@gmail.com>
2024-07-08 15:51:25 +0200

    Add findByProgExerciseId to  TargetSetRepository

--- src/main/java/com/CptFranck/SportsPeak/repositories/TargetSetRepository.java
+++ src/main/java/com/CptFranck/SportsPeak/repositories/TargetSetRepository.java
@@ -4,6 +4,9 @@ import com.CptFranck.SportsPeak.domain.entity.TargetSetEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TargetSetRepository extends CrudRepository<TargetSetEntity, Long> {
+    List<TargetSetEntity> findByProgExerciseId(Long progExerciseId);
 }
