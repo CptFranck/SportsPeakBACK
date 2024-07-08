710ca8874c7d8a296ef6473ed491153f0cbf3613
CptFranck <francois.caillet71@gmail.com>
2024-07-08 17:31:20 +0200

    Add findAllByTargetSetId in PerformanceLogRepository

--- src/main/java/com/CptFranck/SportsPeak/repositories/PerformanceLogRepository.java
+++ src/main/java/com/CptFranck/SportsPeak/repositories/PerformanceLogRepository.java
@@ -4,6 +4,9 @@ import com.CptFranck.SportsPeak.domain.entity.PerformanceLogEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PerformanceLogRepository extends CrudRepository<PerformanceLogEntity, Long> {
+    List<PerformanceLogEntity> findAllByTargetSetId(Long targetSetId);
 }
