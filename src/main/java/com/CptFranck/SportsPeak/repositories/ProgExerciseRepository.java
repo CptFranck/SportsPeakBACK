f13924fe72a782173fb65ecddc1fbfc60abaf59b
CptFranck <francois.caillet71@gmail.com>
2024-07-08 15:01:35 +0200

    Add findBySubscribedUsersId to ProgExerciseRepository

--- src/main/java/com/CptFranck/SportsPeak/repositories/ProgExerciseRepository.java
+++ src/main/java/com/CptFranck/SportsPeak/repositories/ProgExerciseRepository.java
@@ -4,6 +4,9 @@ import com.CptFranck.SportsPeak.domain.entity.ProgExerciseEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProgExerciseRepository extends CrudRepository<ProgExerciseEntity, Long> {
+    List<ProgExerciseEntity> findBySubscribedUsersId(Long id);
 }
