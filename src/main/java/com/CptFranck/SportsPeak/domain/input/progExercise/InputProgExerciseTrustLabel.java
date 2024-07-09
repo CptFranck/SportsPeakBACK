7978be5dd33a288e763a07c8b2b2c42c24aa3bd4
CptFranck <francois.caillet71@gmail.com>
2024-07-09 12:24:56 +0200

    Add InputProgExerciseTrustLabel java type

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/progExercise/InputProgExerciseTrustLabel.java
@@ -0,0 +1,12 @@
+package com.CptFranck.SportsPeak.domain.input.progExercise;
+
+import lombok.Data;
+
+@Data
+public class InputProgExerciseTrustLabel {
+
+    private Long id;
+
+    private String trustLabel;
+
+}
