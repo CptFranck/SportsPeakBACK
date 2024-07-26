71d3f8596e4f454e40808106ae6e5e71da55b191
CptFranck <francois.caillet71@gmail.com>
2024-07-26 11:53:43 +0200

    Add DurationToInputDurationConverter Test

---
+++ src/test/java/com/CptFranck/SportsPeak/mappers/typeConverter/DurationToInputDurationConverterTest.java
@@ -0,0 +1,31 @@
+package com.CptFranck.SportsPeak.mappers.typeConverter;
+
+import com.CptFranck.SportsPeak.domain.input.duration.InputDuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Duration;
+
+@ExtendWith(MockitoExtension.class)
+public class DurationToInputDurationConverterTest {
+
+    @InjectMocks
+    private DurationToInputDurationConverter durationConverter;
+
+    @Test
+    void DurationToInputDurationConverter_Success() {
+        Duration duration = Duration
+                .ofHours(0)
+                .plusMinutes(2)
+                .plusSeconds(30);
+        InputDuration inputDuration = durationConverter.convert(duration);
+
+        Assertions.assertEquals(duration.toHoursPart(), inputDuration.getHours());
+        Assertions.assertEquals(duration.toMinutesPart(), inputDuration.getMinutes());
+        Assertions.assertEquals(duration.toSecondsPart(), inputDuration.getSeconds());
+    }
+
+}
