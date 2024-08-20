fd2049a3633dc20b3adcd31df401759c6cbf0b3c
CptFranck <francois.caillet71@gmail.com>
2024-08-20 14:43:41 +0200

    add ExerciseRepositoryTest class

---
+++ src/test/java/com/CptFranck/SportsPeak/repositories/ExerciseRepositoryTest.java
@@ -0,0 +1,82 @@
+package com.CptFranck.SportsPeak.repositories;
+
+import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+import static com.CptFranck.SportsPeak.domain.TestDataUtil.createNewTestExercise;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ExerciseRepositoryTest {
+
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+
+    @Test
+    public void ExerciseRepository_Save_ReturnSavedComplexMuscle() {
+        ExerciseEntity exercise = createNewTestExercise();
+
+        ExerciseEntity savedExercise = exerciseRepository.save(exercise);
+
+        Assertions.assertNotNull(savedExercise);
+        Assertions.assertTrue(exercise.getId() > 0L, "exercise Id must be > 0");
+    }
+
+    @Test
+    public void ExerciseRepository_findAll_ReturnAllMuscle() {
+        ExerciseEntity savedExerciseOne = createNewTestExercise();
+        ExerciseEntity savedExerciseTwo = createNewTestExercise();
+        exerciseRepository.save(savedExerciseOne);
+        exerciseRepository.save(savedExerciseTwo);
+
+        List<ExerciseEntity> exerciseTypeEntities = StreamSupport.stream(
+                        exerciseRepository.findAll().spliterator(),
+                        false)
+                .toList();
+
+        Assertions.assertNotNull(exerciseTypeEntities);
+        Assertions.assertEquals(exerciseTypeEntities.size(), 2);
+    }
+
+    @Test
+    public void ExerciseRepository_findById_ReturnMuscle() {
+        ExerciseEntity exercise = createNewTestExercise();
+        ExerciseEntity savedExercise = exerciseRepository.save(exercise);
+
+        Optional<ExerciseEntity> foundExercise = exerciseRepository.findById(savedExercise.getId());
+
+        Assertions.assertNotNull(foundExercise);
+        Assertions.assertTrue(foundExercise.isPresent());
+        Assertions.assertNotNull(foundExercise.get());
+    }
+
+    @Test
+    public void ExerciseRepository_existById_ReturnTrue() {
+        ExerciseEntity exercise = createNewTestExercise();
+        ExerciseEntity savedExercise = exerciseRepository.save(exercise);
+
+        boolean foundExercise = exerciseRepository.existsById(savedExercise.getId());
+
+        Assertions.assertTrue(foundExercise);
+    }
+
+    @Test
+    public void ExerciseRepository_deleteById_ReturnTrue() {
+        ExerciseEntity exercise = createNewTestExercise();
+        ExerciseEntity savedExercise = exerciseRepository.save(exercise);
+
+        exerciseRepository.deleteById(savedExercise.getId());
+        boolean foundExercise = exerciseRepository.existsById(savedExercise.getId());
+
+        Assertions.assertFalse(foundExercise);
+    }
+}
