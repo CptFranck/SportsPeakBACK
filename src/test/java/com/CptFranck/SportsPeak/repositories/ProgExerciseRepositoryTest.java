0c5d478dc0040d7f569826136999ae261323b49c
CptFranck <francois.caillet71@gmail.com>
2024-08-20 16:34:50 +0200

    add ProgExerciseRepositoryTest class

---
+++ src/test/java/com/CptFranck/SportsPeak/repositories/ProgExerciseRepositoryTest.java
@@ -0,0 +1,101 @@
+package com.CptFranck.SportsPeak.repositories;
+
+import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
+import com.CptFranck.SportsPeak.domain.entity.ProgExerciseEntity;
+import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static com.CptFranck.SportsPeak.domain.TestDataUtil.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class ProgExerciseRepositoryTest {
+
+    private UserEntity creator;
+
+    private ExerciseEntity exercise;
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+    @Autowired
+    private ProgExerciseRepository progExerciseRepository;
+
+    @BeforeEach
+    public void init() {
+        creator = userRepository.save(createNewTestUser());
+        exercise = exerciseRepository.save(createNewTestExercise());
+    }
+
+    @Test
+    public void ExerciseRepository_Save_ReturnSavedExercise() {
+        ProgExerciseEntity progExercise = createTestNewProgExercise(creator, exercise);
+
+        ProgExerciseEntity savedExercise = progExerciseRepository.save(progExercise);
+
+        Assertions.assertNotNull(savedExercise);
+        Assertions.assertTrue(progExercise.getId() > 0L, "progExercise Id must be > 0");
+    }
+
+    @Test
+    public void ExerciseRepository_findAll_ReturnAllExercise() {
+        ProgExerciseEntity progExerciseOne = createTestNewProgExercise(this.creator, this.exercise);
+        ProgExerciseEntity progExerciseTwo = createTestNewProgExercise(this.creator, this.exercise);
+        progExerciseRepository.save(progExerciseOne);
+        progExerciseRepository.save(progExerciseTwo);
+
+        List<ProgExerciseEntity> progExerciseTypeEntities = StreamSupport.stream(progExerciseRepository
+                                .findAll()
+                                .spliterator(),
+                        false)
+                .collect(Collectors.toList());
+
+        Assertions.assertNotNull(progExerciseTypeEntities);
+        Assertions.assertEquals(progExerciseTypeEntities.size(), 2);
+    }
+
+    @Test
+    public void ExerciseRepository_findById_ReturnExercise() {
+        ProgExerciseEntity progExercise = createTestNewProgExercise(this.creator, this.exercise);
+        ProgExerciseEntity savedExercise = progExerciseRepository.save(progExercise);
+
+        Optional<ProgExerciseEntity> foundExercise = progExerciseRepository.findById(savedExercise.getId());
+
+        Assertions.assertNotNull(foundExercise);
+        Assertions.assertTrue(foundExercise.isPresent());
+        Assertions.assertNotNull(foundExercise.get());
+    }
+
+    @Test
+    public void ExerciseRepository_existById_ReturnTrue() {
+        ProgExerciseEntity progExercise = createTestNewProgExercise(this.creator, this.exercise);
+        ProgExerciseEntity savedProgExercise = progExerciseRepository.save(progExercise);
+
+        boolean foundExercise = progExerciseRepository.existsById(savedProgExercise.getId());
+
+        Assertions.assertTrue(foundExercise);
+    }
+
+    @Test
+    public void ExerciseRepository_deleteById_ReturnTrue() {
+        ProgExerciseEntity progExercise = createTestNewProgExercise(this.creator, this.exercise);
+        ProgExerciseEntity savedExercise = progExerciseRepository.save(progExercise);
+
+        progExerciseRepository.deleteById(savedExercise.getId());
+        boolean foundExercise = progExerciseRepository.existsById(savedExercise.getId());
+
+        Assertions.assertFalse(foundExercise);
+    }
+}
