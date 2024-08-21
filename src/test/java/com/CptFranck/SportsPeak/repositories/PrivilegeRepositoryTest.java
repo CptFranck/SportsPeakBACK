2b56e972e624cbdca83b61c1beff9616b88339c0
CptFranck <francois.caillet71@gmail.com>
2024-08-21 16:25:51 +0200

    Update PrivilegeRepositoryTest class

--- src/test/java/com/CptFranck/SportsPeak/repositories/PrivilegeRepositoryTest.java
+++ src/test/java/com/CptFranck/SportsPeak/repositories/PrivilegeRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.CptFranck.SportsPeak.repositories;
 
+import com.CptFranck.SportsPeak.domain.TestDataUtil;
 import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,10 +10,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 import static com.CptFranck.SportsPeak.domain.TestDataUtil.createNewTestPrivilege;
+import static com.CptFranck.SportsPeak.domain.TestDataUtil.createNewTestPrivileges;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -21,9 +24,20 @@ public class PrivilegeRepositoryTest {
     @Autowired
     private PrivilegeRepository privilegeRepository;
 
+    private PrivilegeEntity saveOnePrivilegeInRepository() {
+        PrivilegeEntity user = createNewTestPrivilege(0);
+        return privilegeRepository.save(user);
+    }
+
+    private List<PrivilegeEntity> saveAllPrivilegesInRepository(List<PrivilegeEntity> users) {
+        List<PrivilegeEntity> localUsers = Objects.requireNonNullElseGet(users, TestDataUtil::createNewTestPrivileges);
+        privilegeRepository.saveAll(localUsers);
+        return localUsers;
+    }
+
     @Test
     public void PrivilegeRepository_Save_ReturnSavedPrivilege() {
-        PrivilegeEntity privilege = createNewTestPrivilege();
+        PrivilegeEntity privilege = createNewTestPrivilege(0);
 
         PrivilegeEntity savedPrivilege = privilegeRepository.save(privilege);
 
@@ -32,13 +46,32 @@ public class PrivilegeRepositoryTest {
     }
 
     @Test
+    public void PrivilegeRepository_SaveAll_ReturnSavedPrivileges() {
+        List<PrivilegeEntity> privileges = createNewTestPrivileges();
+
+        List<PrivilegeEntity> savedPrivileges = saveAllPrivilegesInRepository(privileges);
+
+        Assertions.assertNotNull(savedPrivileges);
+        Assertions.assertEquals(privileges.size(), savedPrivileges.size());
+        Assertions.assertTrue(privileges.getFirst().getId() > 0L, "first user Id must be > 0");
+        Assertions.assertTrue(privileges.getLast().getId() > 1L, "second user Id must be > 0");
+    }
+
+    @Test
+    public void PrivilegeRepository_FindById_ReturnPrivilege() {
+        PrivilegeEntity savedPrivilege = saveOnePrivilegeInRepository();
+
+        Optional<PrivilegeEntity> foundPrivilege = privilegeRepository.findById(savedPrivilege.getId());
+
+        Assertions.assertNotNull(foundPrivilege);
+        Assertions.assertTrue(foundPrivilege.isPresent());
+        Assertions.assertNotNull(foundPrivilege.get());
+    }
+
+    @Test
     public void PrivilegeRepository_FindAll_ReturnAllPrivilege() {
-        PrivilegeEntity privilegeOne = createNewTestPrivilege();
-        PrivilegeEntity privilegeTwo = createNewTestPrivilege();
-        privilegeOne.setName("privilegeOne");
-        privilegeTwo.setName("privilegeTwo");
-        privilegeRepository.save(privilegeOne);
-        privilegeRepository.save(privilegeTwo);
+        List<PrivilegeEntity> privileges = createNewTestPrivileges();
+        privilegeRepository.saveAll(privileges);
 
         List<PrivilegeEntity> privilegeEntities = StreamSupport.stream(
                         privilegeRepository.findAll().spliterator(),
@@ -50,21 +83,25 @@ public class PrivilegeRepositoryTest {
     }
 
     @Test
-    public void PrivilegeRepository_FindById_ReturnPrivilege() {
-        PrivilegeEntity privilege = createNewTestPrivilege();
-        PrivilegeEntity savedPrivilege = privilegeRepository.save(privilege);
+    public void PrivilegeRepository_FindAllById_ReturnAllUsers() {
+        List<PrivilegeEntity> users = createNewTestPrivileges();
+        privilegeRepository.saveAll(users);
 
-        Optional<PrivilegeEntity> foundPrivilege = privilegeRepository.findById(savedPrivilege.getId());
+        List<PrivilegeEntity> userEntities = StreamSupport.stream(
+                        privilegeRepository.findAllById(users.stream()
+                                        .map(PrivilegeEntity::getId)
+                                        .toList())
+                                .spliterator(),
+                        false)
+                .toList();
 
-        Assertions.assertNotNull(foundPrivilege);
-        Assertions.assertTrue(foundPrivilege.isPresent());
-        Assertions.assertNotNull(foundPrivilege.get());
+        Assertions.assertNotNull(userEntities);
+        Assertions.assertEquals(2, userEntities.size());
     }
 
     @Test
-    public void PrivilegeRepository_ExistByName_ReturnTrue() {
-        PrivilegeEntity privilege = createNewTestPrivilege();
-        PrivilegeEntity savedPrivilege = privilegeRepository.save(privilege);
+    public void PrivilegeRepository_FindByName_ReturnTrue() {
+        PrivilegeEntity savedPrivilege = saveOnePrivilegeInRepository();
 
         Optional<PrivilegeEntity> foundPrivilege = privilegeRepository.findByName(savedPrivilege.getName());
 
@@ -76,8 +113,7 @@ public class PrivilegeRepositoryTest {
 
     @Test
     public void PrivilegeRepository_ExistById_ReturnTrue() {
-        PrivilegeEntity privilege = createNewTestPrivilege();
-        PrivilegeEntity savedPrivilege = privilegeRepository.save(privilege);
+        PrivilegeEntity savedPrivilege = saveOnePrivilegeInRepository();
 
         boolean foundPrivilege = privilegeRepository.existsById(savedPrivilege.getId());
 
@@ -85,13 +121,46 @@ public class PrivilegeRepositoryTest {
     }
 
     @Test
-    public void PrivilegeRepository_DeleteById_ReturnTrue() {
-        PrivilegeEntity privilege = createNewTestPrivilege();
-        PrivilegeEntity savedPrivilege = privilegeRepository.save(privilege);
+    public void UserRepository_Delete_ReturnFalse() {
+        PrivilegeEntity savedPrivilege = saveOnePrivilegeInRepository();
+
+        privilegeRepository.delete(savedPrivilege);
+        boolean foundUser = privilegeRepository.existsById(savedPrivilege.getId());
+
+        Assertions.assertFalse(foundUser);
+    }
+
+    @Test
+    public void PrivilegeRepository_DeleteById_ReturnFalse() {
+        PrivilegeEntity savedPrivilege = saveOnePrivilegeInRepository();
 
         privilegeRepository.deleteById(savedPrivilege.getId());
         boolean foundPrivilege = privilegeRepository.existsById(savedPrivilege.getId());
 
         Assertions.assertFalse(foundPrivilege);
+    }
+
+    @Test
+    public void PrivilegeRepository_DeleteAllById_ReturnAllFalse() {
+        List<PrivilegeEntity> userEntities = saveAllPrivilegesInRepository(null);
+
+        privilegeRepository.deleteAllById(userEntities.stream().map(PrivilegeEntity::getId).toList());
+
+        userEntities.forEach(user -> {
+            boolean foundUser = privilegeRepository.existsById(user.getId());
+            Assertions.assertFalse(foundUser);
+        });
+    }
+
+    @Test
+    public void PrivilegeRepository_DeleteAll_ReturnAllFalse() {
+        List<PrivilegeEntity> userEntities = saveAllPrivilegesInRepository(null);
+
+        privilegeRepository.deleteAll();
+
+        userEntities.forEach(user -> {
+            boolean foundUser = privilegeRepository.existsById(user.getId());
+            Assertions.assertFalse(foundUser);
+        });
     }
 }
