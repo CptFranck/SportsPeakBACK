c39b132d0b0c444ab275bada9cc5fd0cb491a814
CptFranck <francois.caillet71@gmail.com>
2024-08-21 12:57:08 +0200

    Update ProgExerciseRepositoryTest class

--- src/test/java/com/CptFranck/SportsPeak/repositories/ProgExerciseRepositoryTest.java
+++ src/test/java/com/CptFranck/SportsPeak/repositories/ProgExerciseRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -33,9 +34,22 @@ class ProgExerciseRepositoryTest {
     @Autowired
     private ProgExerciseRepository progExerciseRepository;
 
+    private ProgExerciseEntity saveOneProgExerciseInRepository() {
+        ProgExerciseEntity progExercise = createTestNewProgExercise(creator, exercise);
+        return progExerciseRepository.save(progExercise);
+    }
+
+    private List<ProgExerciseEntity> saveAllProgExercisesInRepository(List<ProgExerciseEntity> progExercises) {
+        List<ProgExerciseEntity> localProgExercises = Objects.requireNonNullElseGet(
+                progExercises,
+                () -> createNewTestProgExercises(creator, exercise));
+        progExerciseRepository.saveAll(localProgExercises);
+        return localProgExercises;
+    }
+
     @BeforeEach
     public void init() {
-        creator = userRepository.save(createNewTestUser());
+        creator = userRepository.save(createNewTestUser(0));
         exercise = exerciseRepository.save(createNewTestExercise());
     }
 
@@ -43,22 +57,42 @@ class ProgExerciseRepositoryTest {
     public void ProgExerciseRepository_Save_ReturnSavedProgExercise() {
         ProgExerciseEntity progExercise = createTestNewProgExercise(creator, exercise);
 
-        ProgExerciseEntity savedExercise = progExerciseRepository.save(progExercise);
+        ProgExerciseEntity savedProgExercise = progExerciseRepository.save(progExercise);
 
-        Assertions.assertNotNull(savedExercise);
+        Assertions.assertNotNull(savedProgExercise);
         Assertions.assertTrue(progExercise.getId() > 0L, "progExercise Id must be > 0");
     }
 
     @Test
-    public void ProgExerciseRepository_FindAll_ReturnAllProgExercise() {
-        ProgExerciseEntity progExerciseOne = createTestNewProgExercise(this.creator, this.exercise);
-        ProgExerciseEntity progExerciseTwo = createTestNewProgExercise(this.creator, this.exercise);
-        progExerciseRepository.save(progExerciseOne);
-        progExerciseRepository.save(progExerciseTwo);
+    public void ProgExerciseRepository_SaveAll_ReturnSavedProgExercises() {
+        List<ProgExerciseEntity> progExercises = createNewTestProgExercises(creator, exercise);
 
-        List<ProgExerciseEntity> progExerciseTypeEntities = StreamSupport.stream(progExerciseRepository
-                                .findAll()
-                                .spliterator(),
+        List<ProgExerciseEntity> savedProgExercises = saveAllProgExercisesInRepository(progExercises);
+
+        Assertions.assertNotNull(savedProgExercises);
+        Assertions.assertEquals(progExercises.size(), savedProgExercises.size());
+        Assertions.assertTrue(progExercises.getFirst().getId() > 0L, "first user Id must be > 0");
+        Assertions.assertTrue(progExercises.getLast().getId() > 1L, "second user Id must be > 0");
+    }
+
+    @Test
+    public void ProgExerciseRepository_FindById_ReturnProgExercise() {
+        ProgExerciseEntity savedProgExercise = saveOneProgExerciseInRepository();
+
+        Optional<ProgExerciseEntity> foundProgExercise = progExerciseRepository.findById(savedProgExercise.getId());
+
+        Assertions.assertNotNull(foundProgExercise);
+        Assertions.assertTrue(foundProgExercise.isPresent());
+        Assertions.assertNotNull(foundProgExercise.get());
+    }
+
+    @Test
+    public void ProgExerciseRepository_FindAll_ReturnAllProgExercises() {
+        List<ProgExerciseEntity> progExercises = createNewTestProgExercises(creator, exercise);
+        progExerciseRepository.saveAll(progExercises);
+
+        List<ProgExerciseEntity> progExerciseTypeEntities = StreamSupport.stream(
+                        progExerciseRepository.findAll().spliterator(),
                         false)
                 .collect(Collectors.toList());
 
@@ -67,35 +101,81 @@ class ProgExerciseRepositoryTest {
     }
 
     @Test
-    public void ProgExerciseRepository_FindById_ReturnProgExercise() {
-        ProgExerciseEntity progExercise = createTestNewProgExercise(this.creator, this.exercise);
-        ProgExerciseEntity savedExercise = progExerciseRepository.save(progExercise);
+    public void ProgExerciseRepository_FindAllById_ReturnAllProgExercises() {
+        List<ProgExerciseEntity> progExercises = createNewTestProgExercises(creator, exercise);
+        progExerciseRepository.saveAll(progExercises);
 
-        Optional<ProgExerciseEntity> foundExercise = progExerciseRepository.findById(savedExercise.getId());
+        List<ProgExerciseEntity> userEntities = StreamSupport.stream(
+                        progExerciseRepository.findAllById(progExercises.stream()
+                                        .map(ProgExerciseEntity::getId)
+                                        .toList())
+                                .spliterator(),
+                        false)
+                .toList();
 
-        Assertions.assertNotNull(foundExercise);
-        Assertions.assertTrue(foundExercise.isPresent());
-        Assertions.assertNotNull(foundExercise.get());
+        Assertions.assertNotNull(userEntities);
+        Assertions.assertEquals(2, userEntities.size());
     }
 
     @Test
     public void ProgExerciseRepository_ExistById_ReturnTrue() {
-        ProgExerciseEntity progExercise = createTestNewProgExercise(this.creator, this.exercise);
-        ProgExerciseEntity savedProgExercise = progExerciseRepository.save(progExercise);
+        ProgExerciseEntity savedProgExercise = saveOneProgExerciseInRepository();
 
-        boolean foundExercise = progExerciseRepository.existsById(savedProgExercise.getId());
+        boolean foundProgExercise = progExerciseRepository.existsById(savedProgExercise.getId());
 
-        Assertions.assertTrue(foundExercise);
+        Assertions.assertTrue(foundProgExercise);
     }
 
     @Test
-    public void ProgExerciseRepository_DeleteById_ReturnTrue() {
-        ProgExerciseEntity progExercise = createTestNewProgExercise(this.creator, this.exercise);
-        ProgExerciseEntity savedExercise = progExerciseRepository.save(progExercise);
+    public void ProgExerciseRepository_Count_ReturnUsersListSize() {
+        List<ProgExerciseEntity> savedProgExercises = saveAllProgExercisesInRepository(null);
 
-        progExerciseRepository.deleteById(savedExercise.getId());
-        boolean foundExercise = progExerciseRepository.existsById(savedExercise.getId());
+        Long progExercisesCount = progExerciseRepository.count();
 
-        Assertions.assertFalse(foundExercise);
+        Assertions.assertEquals(progExercisesCount, savedProgExercises.size());
+    }
+
+    @Test
+    public void ProgExerciseRepository_Delete_ReturnFalse() {
+        ProgExerciseEntity savedProgExercise = saveOneProgExerciseInRepository();
+
+        progExerciseRepository.delete(savedProgExercise);
+        boolean foundProgExercise = progExerciseRepository.existsById(savedProgExercise.getId());
+
+        Assertions.assertFalse(foundProgExercise);
+    }
+
+    @Test
+    public void ProgExerciseRepository_DeleteById_ReturnFalse() {
+        ProgExerciseEntity savedProgExercise = saveOneProgExerciseInRepository();
+
+        progExerciseRepository.deleteById(savedProgExercise.getId());
+        boolean foundProgExercise = progExerciseRepository.existsById(savedProgExercise.getId());
+
+        Assertions.assertFalse(foundProgExercise);
+    }
+
+    @Test
+    public void ProgExerciseRepository_DeleteAllById_ReturnAllFalse() {
+        List<ProgExerciseEntity> progExerciseEntities = saveAllProgExercisesInRepository(null);
+
+        progExerciseRepository.deleteAllById(progExerciseEntities.stream().map(ProgExerciseEntity::getId).toList());
+
+        progExerciseEntities.forEach(progExercise -> {
+            boolean foundProgExercise = progExerciseRepository.existsById(progExercise.getId());
+            Assertions.assertFalse(foundProgExercise);
+        });
+    }
+
+    @Test
+    public void ProgExerciseRepository_DeleteAll_ReturnAllFalse() {
+        List<ProgExerciseEntity> progExerciseEntities = saveAllProgExercisesInRepository(null);
+
+        progExerciseRepository.deleteAll();
+
+        progExerciseEntities.forEach(progExercise -> {
+            boolean foundProgExercise = progExerciseRepository.existsById(progExercise.getId());
+            Assertions.assertFalse(foundProgExercise);
+        });
     }
 }
