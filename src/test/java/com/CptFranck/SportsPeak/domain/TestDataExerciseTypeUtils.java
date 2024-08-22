ffaa95ae125bd9144bce4631f128cfe98dd8d022
CptFranck <francois.caillet71@gmail.com>
2024-08-22 15:01:53 +0200

    Add TestDataExerciseTypeUtils class

---
+++ src/test/java/com/CptFranck/SportsPeak/domain/TestDataExerciseTypeUtils.java
@@ -0,0 +1,29 @@
+package com.CptFranck.SportsPeak.domain;
+
+import com.CptFranck.SportsPeak.domain.dto.ExerciseDto;
+import com.CptFranck.SportsPeak.domain.dto.ExerciseTypeDto;
+import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
+import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
+
+import java.util.HashSet;
+
+public class TestDataExerciseTypeUtils {
+
+    public static ExerciseTypeEntity createTestExerciseType() {
+        return new ExerciseTypeEntity(
+                1L,
+                "Exercise type name",
+                "Exercise type goal",
+                new HashSet<ExerciseEntity>()
+        );
+    }
+
+    public static ExerciseTypeDto createTestExerciseTypeDto() {
+        return new ExerciseTypeDto(
+                1L,
+                "Exercise type name",
+                "Exercise type goal",
+                new HashSet<ExerciseDto>()
+        );
+    }
+}
