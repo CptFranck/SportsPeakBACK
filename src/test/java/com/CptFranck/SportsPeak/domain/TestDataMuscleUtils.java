a9b3390decd2bb67920584ba1a8d70cd6fb98531
CptFranck <francois.caillet71@gmail.com>
2024-08-22 14:57:54 +0200

    Add TestDataMuscleUtils class

---
+++ src/test/java/com/CptFranck/SportsPeak/domain/TestDataMuscleUtils.java
@@ -0,0 +1,31 @@
+package com.CptFranck.SportsPeak.domain;
+
+import com.CptFranck.SportsPeak.domain.dto.ExerciseDto;
+import com.CptFranck.SportsPeak.domain.dto.MuscleDto;
+import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
+import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
+
+import java.util.HashSet;
+
+public class TestDataMuscleUtils {
+
+    public static MuscleEntity createTestMuscle() {
+        return new MuscleEntity(
+                2L,
+                "Muscle name",
+                "Muscle description",
+                "Muscle function",
+                new HashSet<ExerciseEntity>()
+        );
+    }
+
+    public static MuscleDto createTestMuscleDto() {
+        return new MuscleDto(
+                2L,
+                "Muscle name",
+                "Muscle description",
+                "Muscle function",
+                new HashSet<ExerciseDto>()
+        );
+    }
+}
