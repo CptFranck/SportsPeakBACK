10b1c1482a65128c3aee3bba91692af20453a5fd
CptFranck <francois.caillet71@gmail.com>
2024-08-22 17:20:40 +0200

    Add progExerciseTypeMapper_MapFrom_Success function to ProgExerciseMapperImplTest

--- src/test/java/com/CptFranck/SportsPeak/mappers/impl/ProgExerciseMapperImplTest.java
+++ src/test/java/com/CptFranck/SportsPeak/mappers/impl/ProgExerciseMapperImplTest.java
@@ -1,7 +1,9 @@
 package com.CptFranck.SportsPeak.mappers.impl;
 
+import com.CptFranck.SportsPeak.domain.dto.ExerciseDto;
 import com.CptFranck.SportsPeak.domain.dto.ProgExerciseDto;
 import com.CptFranck.SportsPeak.domain.dto.TargetSetDto;
+import com.CptFranck.SportsPeak.domain.dto.UserDto;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.ProgExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.TargetSetEntity;
@@ -13,7 +15,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
-import static com.CptFranck.SportsPeak.domain.TestDataUtil.*;
+import static com.CptFranck.SportsPeak.domain.TestDataExerciseUtils.createTestExercise;
+import static com.CptFranck.SportsPeak.domain.TestDataExerciseUtils.createTestExerciseDto;
+import static com.CptFranck.SportsPeak.domain.TestDataProgExerciseUtils.createTestProgExercise;
+import static com.CptFranck.SportsPeak.domain.TestDataProgExerciseUtils.createTestProgExerciseDto;
+import static com.CptFranck.SportsPeak.domain.TestDataUserUtils.createTestUser;
+import static com.CptFranck.SportsPeak.domain.TestDataUserUtils.createTestUserDto;
+import static com.CptFranck.SportsPeak.domain.utils.TestDataTargetSetUtils.createTestTargetSet;
+import static com.CptFranck.SportsPeak.domain.utils.TestDataTargetSetUtils.createTestTargetSetDto;
 
 @ExtendWith(MockitoExtension.class)
 public class ProgExerciseMapperImplTest {
@@ -25,7 +34,7 @@ public class ProgExerciseMapperImplTest {
     }
 
     @Test
-    void testProgExerciseTypeMapperMapTo_Success() {
+    void progExerciseTypeMapper_MapTo_Success() {
         UserEntity user = createTestUser();
         ExerciseEntity exercise = createTestExercise();
         ProgExerciseEntity progExercise = createTestProgExercise(user, exercise);
@@ -41,11 +50,35 @@ public class ProgExerciseMapperImplTest {
         Assertions.assertEquals(progExercise.getNote(), progExerciseDto.getNote());
         Assertions.assertEquals(progExercise.getVisibility().label, progExerciseDto.getVisibility());
         Assertions.assertEquals(progExercise.getTrustLabel().label, progExerciseDto.getTrustLabel());
-        Assertions.assertEquals(progExercise.getCreator(), progExerciseDto.getCreator());
+        Assertions.assertEquals(progExercise.getCreator().getId(), progExerciseDto.getCreator().getId());
         Assertions.assertEquals(progExercise.getTargetSets().size(), progExerciseDto.getTargetSets().size());
         Assertions.assertArrayEquals(
                 progExercise.getTargetSets().stream().map(TargetSetEntity::getId).toArray(),
                 progExerciseDto.getTargetSets().stream().map(TargetSetDto::getId).toArray()
+        );
+    }
+
+    @Test
+    void progExerciseTypeMapper_MapFrom_Success() {
+        UserDto user = createTestUserDto();
+        ExerciseDto exercise = createTestExerciseDto();
+        ProgExerciseDto progExercise = createTestProgExerciseDto(user, exercise);
+        TargetSetDto targetSet = createTestTargetSetDto(progExercise);
+
+        progExercise.getTargetSets().add(targetSet);
+
+        ProgExerciseEntity progExerciseEntity = progExerciseMapper.mapFrom(progExercise);
+
+        Assertions.assertEquals(progExercise.getId(), progExerciseEntity.getId());
+        Assertions.assertEquals(progExercise.getName(), progExerciseEntity.getName());
+        Assertions.assertEquals(progExercise.getNote(), progExerciseEntity.getNote());
+        Assertions.assertEquals(progExercise.getVisibility(), progExerciseEntity.getVisibility().label);
+        Assertions.assertEquals(progExercise.getTrustLabel(), progExerciseEntity.getTrustLabel().label);
+        Assertions.assertEquals(progExercise.getCreator().getId(), progExerciseEntity.getCreator().getId());
+        Assertions.assertEquals(progExercise.getTargetSets().size(), progExerciseEntity.getTargetSets().size());
+        Assertions.assertArrayEquals(
+                progExercise.getTargetSets().stream().map(TargetSetDto::getId).toArray(),
+                progExerciseEntity.getTargetSets().stream().map(TargetSetEntity::getId).toArray()
         );
     }
 }
