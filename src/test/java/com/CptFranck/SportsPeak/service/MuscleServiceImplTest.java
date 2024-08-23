9dd7ad0f48e87b6837cc1da7f4faebc87480ef19
CptFranck <francois.caillet71@gmail.com>
2024-08-23 15:06:17 +0200

    Add MuscleServiceImplTest class

---
+++ src/test/java/com/CptFranck/SportsPeak/service/MuscleServiceImplTest.java
@@ -0,0 +1,103 @@
+package com.CptFranck.SportsPeak.service;
+
+import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
+import com.CptFranck.SportsPeak.domain.exception.muscle.MuscleNotFoundException;
+import com.CptFranck.SportsPeak.repositories.MuscleRepository;
+import com.CptFranck.SportsPeak.service.impl.MuscleServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.CptFranck.SportsPeak.domain.utils.TestMuscleUtils.createTestMuscle;
+import static com.CptFranck.SportsPeak.domain.utils.TestMuscleUtils.createTestMuscleList;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MuscleServiceImplTest {
+
+    @Mock
+    MuscleRepository muscleRepository;
+
+    @InjectMocks
+    MuscleServiceImpl muscleServiceImpl;
+
+    @Test
+    void muscleService_Save_Success() {
+        MuscleEntity muscleEntity = createTestMuscle(null);
+        MuscleEntity muscleSavedInRepository = createTestMuscle(1L);
+        when(muscleRepository.save(Mockito.any(MuscleEntity.class))).thenReturn(muscleSavedInRepository);
+
+        MuscleEntity muscleSaved = muscleServiceImpl.save(muscleEntity);
+
+        Assertions.assertEquals(muscleSavedInRepository, muscleSaved);
+    }
+
+    @Test
+    void muscleService_FindAll_Success() {
+        List<MuscleEntity> muscleList = createTestMuscleList();
+        when(muscleRepository.findAll()).thenReturn(muscleList);
+
+        List<MuscleEntity> muscleFound = muscleServiceImpl.findAll();
+
+        Assertions.assertEquals(muscleList, muscleFound);
+    }
+
+    @Test
+    void muscleService_FindOne_Success() {
+        MuscleEntity muscleEntity = createTestMuscle(1L);
+        when(muscleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.of(muscleEntity));
+
+        Optional<MuscleEntity> muscleFound = muscleServiceImpl.findOne(muscleEntity.getId());
+
+        Assertions.assertTrue(muscleFound.isPresent());
+        Assertions.assertEquals(muscleEntity, muscleFound.get());
+    }
+
+    @Test
+    void muscleService_FindMany_Success() {
+        List<MuscleEntity> muscleList = createTestMuscleList();
+        Set<Long> muscleIds = muscleList.stream().map(MuscleEntity::getId).collect(Collectors.toSet());
+        when(muscleRepository.findAllById(Mockito.anyIterable())).thenReturn(muscleList);
+
+        Set<MuscleEntity> muscleFound = muscleServiceImpl.findMany(muscleIds);
+        Assertions.assertEquals(new HashSet<>(muscleList), muscleFound);
+    }
+
+    @Test
+    void muscleService_Exists_Success() {
+        MuscleEntity muscleEntity = createTestMuscle(1L);
+        when(muscleRepository.existsById(Mockito.any(Long.class))).thenReturn(true);
+
+        boolean muscleFound = muscleServiceImpl.exists(muscleEntity.getId());
+
+        Assertions.assertTrue(muscleFound);
+    }
+
+    @Test
+    void muscleService_Delete_Success() {
+        MuscleEntity muscleEntity = createTestMuscle(1L);
+        when(muscleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.of(muscleEntity));
+
+        assertAll(() -> muscleServiceImpl.delete(muscleEntity.getId()));
+    }
+
+    @Test
+    void muscleService_Delete_Unsuccessful() {
+        MuscleEntity muscleEntity = createTestMuscle(1L);
+        when(muscleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.empty());
+
+        assertThrows(MuscleNotFoundException.class, () -> muscleServiceImpl.delete(muscleEntity.getId()));
+    }
+}
