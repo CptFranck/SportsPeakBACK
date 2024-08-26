ff629997327269dd1f28228c91bae702e7639b3c
CptFranck <francois.caillet71@gmail.com>
2024-08-26 11:47:49 +0200

    Refactor RoleNotFoundException class

--- src/main/java/com/CptFranck/SportsPeak/domain/exception/role/RoleNotFoundException.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/exception/role/RoleNotFoundException.java
@@ -1,7 +1,7 @@
 package com.CptFranck.SportsPeak.domain.exception.role;
 
 public class RoleNotFoundException extends RuntimeException {
-    public RoleNotFoundException(String role) {
-        super(String.format("The role %s has not been found", role));
+    public RoleNotFoundException(String idOrRole) {
+        super(String.format("The role the id or the name %s has not been found", idOrRole));
     }
 }
