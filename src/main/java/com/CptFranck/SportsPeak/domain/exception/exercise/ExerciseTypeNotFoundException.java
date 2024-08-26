78e6a613d767b85a620a11127518b7f9f2043345
CptFranck <francois.caillet71@gmail.com>
2024-08-26 11:02:04 +0200

    Minor fix PrivilegeNotFoundException class

--- src/main/java/com/CptFranck/SportsPeak/domain/exception/exercise/ExerciseTypeNotFoundException.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/exception/exercise/ExerciseTypeNotFoundException.java
@@ -2,6 +2,6 @@ package com.CptFranck.SportsPeak.domain.exception.exercise;
 
 public class ExerciseTypeNotFoundException extends RuntimeException {
     public ExerciseTypeNotFoundException(Long id) {
-        super(String.format("The exercise with the id %s has not been found", id.toString()));
+        super(String.format("The exerciseType with the id %s has not been found", id.toString()));
     }
 }
--- src/main/java/com/CptFranck/SportsPeak/domain/exception/progExercise/ProgExerciseNotFoundException.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/exception/progExercise/ProgExerciseNotFoundException.java
@@ -2,6 +2,6 @@ package com.CptFranck.SportsPeak.domain.exception.progExercise;
 
 public class ProgExerciseNotFoundException extends RuntimeException {
     public ProgExerciseNotFoundException(Long id) {
-        super(String.format("The exercise with the id %s has not been found", id.toString()));
+        super(String.format("The progExercise with the id %s has not been found", id.toString()));
     }
 }
