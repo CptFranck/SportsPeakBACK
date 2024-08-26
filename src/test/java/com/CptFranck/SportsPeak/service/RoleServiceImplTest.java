f92cb9e80fad7a83d9d3e2486f1a63f17240436b
CptFranck <francois.caillet71@gmail.com>
2024-08-26 12:24:44 +0200

    Update RoleServiceImplTest class

--- src/test/java/com/CptFranck/SportsPeak/service/RoleServiceImplTest.java
+++ src/test/java/com/CptFranck/SportsPeak/service/RoleServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.CptFranck.SportsPeak.service;
 
+import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
 import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
+import com.CptFranck.SportsPeak.domain.exception.role.RoleExistsException;
 import com.CptFranck.SportsPeak.domain.exception.role.RoleNotFoundException;
 import com.CptFranck.SportsPeak.repositories.RoleRepository;
 import com.CptFranck.SportsPeak.service.impl.RoleServiceImpl;
@@ -18,6 +20,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.CptFranck.SportsPeak.domain.utils.TestPrivilegeUtils.createTestPrivilege;
 import static com.CptFranck.SportsPeak.domain.utils.TestRoleUtils.createNewTestRoleList;
 import static com.CptFranck.SportsPeak.domain.utils.TestRoleUtils.createTestRole;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -28,76 +31,125 @@ import static org.mockito.Mockito.when;
 public class RoleServiceImplTest {
 
     @Mock
-    RoleRepository RoleRepository;
+    RoleRepository roleRepository;
 
     @InjectMocks
-    RoleServiceImpl RoleServiceImpl;
+    RoleServiceImpl roleServiceImpl;
 
     @Test
     void RoleService_Save_Success() {
-        RoleEntity Role = createTestRole(null, 0);
-        RoleEntity RoleSavedInRepository = createTestRole(1L, 0);
-        when(RoleRepository.save(Mockito.any(RoleEntity.class))).thenReturn(RoleSavedInRepository);
+        RoleEntity role = createTestRole(null, 0);
+        RoleEntity roleSavedInRepository = createTestRole(1L, 0);
+        when(roleRepository.findByName(Mockito.any(String.class))).thenReturn(Optional.empty());
+        when(roleRepository.save(Mockito.any(RoleEntity.class))).thenReturn(roleSavedInRepository);
 
-        RoleEntity RoleSaved = RoleServiceImpl.save(Role);
+        RoleEntity RoleSaved = roleServiceImpl.save(role);
 
-        Assertions.assertEquals(RoleSavedInRepository, RoleSaved);
+        Assertions.assertEquals(roleSavedInRepository, RoleSaved);
+    }
+
+    @Test
+    void RoleService_Save_UpdateSuccess() {
+        RoleEntity role = createTestRole(1L, 0);
+        when(roleRepository.findByName(Mockito.any(String.class))).thenReturn(Optional.of(role));
+        when(roleRepository.save(Mockito.any(RoleEntity.class))).thenReturn(role);
+
+        RoleEntity RoleSaved = roleServiceImpl.save(role);
+
+        Assertions.assertEquals(role, RoleSaved);
+    }
+
+    @Test
+    void RoleService_Save_UnSuccessful() {
+        RoleEntity role = createTestRole(1L, 0);
+        RoleEntity roleAlreadyRegisteredWithSameName = createTestRole(2L, 0);
+        when(roleRepository.findByName(Mockito.any(String.class))).thenReturn(Optional.of(roleAlreadyRegisteredWithSameName));
+
+        assertThrows(RoleExistsException.class, () -> roleServiceImpl.save(role));
     }
 
     @Test
     void RoleService_FindAll_Success() {
-        List<RoleEntity> RoleList = createNewTestRoleList();
-        when(RoleRepository.findAll()).thenReturn(RoleList);
+        List<RoleEntity> roleList = createNewTestRoleList();
+        when(roleRepository.findAll()).thenReturn(roleList);
 
-        List<RoleEntity> RoleFound = RoleServiceImpl.findAll();
+        List<RoleEntity> roleFound = roleServiceImpl.findAll();
 
-        Assertions.assertEquals(RoleList, RoleFound);
+        Assertions.assertEquals(roleList, roleFound);
     }
 
     @Test
     void RoleService_FindOne_Success() {
-        RoleEntity RoleEntity = createTestRole(1L, 0);
-        when(RoleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.of(RoleEntity));
+        RoleEntity role = createTestRole(1L, 0);
+        when(roleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.of(role));
 
-        Optional<RoleEntity> RoleFound = RoleServiceImpl.findOne(RoleEntity.getId());
+        Optional<RoleEntity> roleFound = roleServiceImpl.findOne(role.getId());
 
-        Assertions.assertTrue(RoleFound.isPresent());
-        Assertions.assertEquals(RoleEntity, RoleFound.get());
+        Assertions.assertTrue(roleFound.isPresent());
+        Assertions.assertEquals(role, roleFound.get());
+    }
+
+    @Test
+    void RoleService_findByName_Success() {
+        RoleEntity role = createTestRole(1L, 0);
+        when(roleRepository.findByName(Mockito.any(String.class))).thenReturn(Optional.of(role));
+
+        Optional<RoleEntity> roleFound = roleServiceImpl.findByName(role.getName());
+
+        Assertions.assertTrue(roleFound.isPresent());
+        Assertions.assertEquals(role, roleFound.get());
     }
 
     @Test
     void RoleService_FindMany_Success() {
-        List<RoleEntity> RoleList = createNewTestRoleList();
-        Set<Long> RoleIds = RoleList.stream().map(RoleEntity::getId).collect(Collectors.toSet());
-        when(RoleRepository.findAllById(Mockito.anyIterable())).thenReturn(RoleList);
+        List<RoleEntity> roleList = createNewTestRoleList();
+        Set<Long> roleIds = roleList.stream().map(RoleEntity::getId).collect(Collectors.toSet());
+        when(roleRepository.findAllById(Mockito.anyIterable())).thenReturn(roleList);
 
-        Set<RoleEntity> RoleFound = RoleServiceImpl.findMany(RoleIds);
-        Assertions.assertEquals(new HashSet<>(RoleList), RoleFound);
+        Set<RoleEntity> roleFound = roleServiceImpl.findMany(roleIds);
+        Assertions.assertEquals(new HashSet<>(roleList), roleFound);
+    }
+
+    @Test
+    void exerciseService_UpdatePrivilegeRelation_Success() {
+        RoleEntity roleOne = createTestRole(1L, 1);
+        RoleEntity roleTwo = createTestRole(2L, 2);
+        Set<Long> oldRoleIds = new HashSet<>();
+        Set<Long> newRoleIds = new HashSet<>();
+        oldRoleIds.add(roleOne.getId());
+        newRoleIds.add(roleTwo.getId());
+
+        PrivilegeEntity privilege = createTestPrivilege(1L);
+
+        when(roleRepository.findAllById(oldRoleIds)).thenReturn(List.of(roleOne));
+        when(roleRepository.findAllById(newRoleIds)).thenReturn(List.of(roleTwo));
+
+        assertAll(() -> roleServiceImpl.updatePrivilegeRelation(newRoleIds, oldRoleIds, privilege));
     }
 
     @Test
     void RoleService_Exists_Success() {
-        RoleEntity RoleEntity = createTestRole(1L, 0);
-        when(RoleRepository.existsById(Mockito.any(Long.class))).thenReturn(true);
+        RoleEntity roleEntity = createTestRole(1L, 0);
+        when(roleRepository.existsById(Mockito.any(Long.class))).thenReturn(true);
 
-        boolean RoleFound = RoleServiceImpl.exists(RoleEntity.getId());
+        boolean roleFound = roleServiceImpl.exists(roleEntity.getId());
 
-        Assertions.assertTrue(RoleFound);
+        Assertions.assertTrue(roleFound);
     }
 
     @Test
     void RoleService_Delete_Success() {
-        RoleEntity RoleEntity = createTestRole(1L, 0);
-        when(RoleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.of(RoleEntity));
+        RoleEntity role = createTestRole(1L, 0);
+        when(roleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.of(role));
 
-        assertAll(() -> RoleServiceImpl.delete(RoleEntity.getId()));
+        assertAll(() -> roleServiceImpl.delete(role.getId()));
     }
 
     @Test
     void RoleService_Delete_Unsuccessful() {
-        RoleEntity RoleEntity = createTestRole(1L, 0);
-        when(RoleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.empty());
+        RoleEntity role = createTestRole(1L, 0);
+        when(roleRepository.findById(Mockito.any(Long.class))).thenReturn(Optional.empty());
 
-        assertThrows(RoleNotFoundException.class, () -> RoleServiceImpl.delete(RoleEntity.getId()));
+        assertThrows(RoleNotFoundException.class, () -> roleServiceImpl.delete(role.getId()));
     }
 }
