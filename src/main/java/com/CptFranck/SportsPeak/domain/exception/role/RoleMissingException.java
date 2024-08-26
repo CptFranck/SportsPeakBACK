e32c42c251e182acf338f46aa7a512eb9ab78508
CptFranck <francois.caillet71@gmail.com>
2024-08-26 17:59:25 +0200

    removed unused file (UserRole enum and RoleMissingException exception)

--- src/main/java/com/CptFranck/SportsPeak/domain/exception/role/RoleMissingException.java
+++
@@ -1,7 +0,0 @@
-package com.CptFranck.SportsPeak.domain.exception.role;
-
-public class RoleMissingException extends RuntimeException {
-    public RoleMissingException() {
-        super("A role has not been found in set");
-    }
-}
