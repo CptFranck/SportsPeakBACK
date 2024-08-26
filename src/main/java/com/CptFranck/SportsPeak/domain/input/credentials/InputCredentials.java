ddfa56e76442daf0b181932bfdc713a3ea1e0c62
CptFranck <francois.caillet71@gmail.com>
2024-08-26 17:01:40 +0200

    Update InputCredentials class

--- src/main/java/com/CptFranck/SportsPeak/domain/input/credentials/InputCredentials.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/credentials/InputCredentials.java
@@ -8,4 +8,9 @@ public class InputCredentials {
     private String email;
 
     private String password;
+
+    public InputCredentials(String mail, String password) {
+        this.email = mail;
+        this.password = password;
+    }
 }
