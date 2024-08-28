212778163594521fa404739a21278b7a8bc337db
CptFranck <francois.caillet71@gmail.com>
2024-08-28 18:19:04 +0200

    Update ExerciseControllerTest

--- src/test/java/com/CptFranck/SportsPeak/controller/ExerciseControllerTest.java
+++ src/test/java/com/CptFranck/SportsPeak/controller/ExerciseControllerTest.java
@@ -13,14 +13,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Optional;
 
-import static com.CptFranck.SportsPeak.domain.utils.TestExerciseUtils.createTestExerciseList;
+import static com.CptFranck.SportsPeak.domain.utils.TestExerciseUtils.createTestExercise;
+import static com.CptFranck.SportsPeak.domain.utils.TestExerciseUtils.createTestExerciseDto;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,59 +39,89 @@ class ExerciseControllerTest {
     private DgsQueryExecutor dgsQueryExecutor;
 
     @MockBean
-    private MuscleService muscleService;
-    @MockBean
-    private ExerciseService exerciseService;
-    @MockBean
-    private ExerciseTypeService exerciseTypeService;
-    @MockBean
     private Mapper<ExerciseEntity, ExerciseDto> exerciseMapper;
 
-    private List<ExerciseEntity> exerciseEntities;
+    @MockBean
+    private MuscleService muscleService;
+
+    @MockBean
+    private ExerciseService exerciseService;
+
+    @MockBean
+    private ExerciseTypeService exerciseTypeService;
+
+    private ExerciseEntity exercise;
+
+    private List<ExerciseEntity> exercises;
+
+    private ExerciseDto exerciseDto;
 
     @BeforeEach
     void init() {
-        exerciseEntities = createTestExerciseList();
+        exercise = createTestExercise(1L);
+        exercises = List.of(exercise);
+        exerciseDto = createTestExerciseDto(1L);
     }
 
     @Test
-    void getExercises() {
+    void ExerciseController_GetExercises_Success() {
+        when(exerciseService.findAll()).thenReturn(exercises);
+        when(exerciseMapper.mapTo(Mockito.any(ExerciseEntity.class))).thenReturn(exerciseDto);
 
-        when(exerciseService.findAll()).thenReturn(exerciseEntities);
-
-        List<ExerciseDto> exerciseDtos = dgsQueryExecutor.executeAndExtractJsonPath("query {\n" +
-                "    getExercises {\n" +
-                "        id\n" +
-                "        exerciseTypes {\n" +
-                "            id\n" +
-                "            name\n" +
-                "            goal\n" +
-                "        }\n" +
-                "        muscles {\n" +
-                "            id\n" +
-                "            name\n" +
-                "            function\n" +
-                "        }\n" +
-                "        progExercises {\n" +
-                "            id\n" +
-                "            name\n" +
-                "            note\n" +
-                "            trustLabel\n" +
-                "            visibility\n" +
-                "        }\n" +
-                "        name\n" +
-                "        description\n" +
+        List<LinkedHashMap<String, Object>> exerciseDtos = dgsQueryExecutor.executeAndExtractJsonPath("query {\n" +
+                "    getExercises {" +
+                "        id" +
+                "        exerciseTypes { id name goal }\n" +
+                "        muscles { id name function }\n" +
+                "        progExercises { id name note trustLabel visibility} " +
+                "        name" +
+                "        description" +
                 "        goal\n" +
                 "    }\n" +
-                "}", "data.getExercises[*].id");
+                "}", "data.getExercises");
 
         Assertions.assertNotNull(exerciseDtos);
     }
 
-//    @Test
-//    void getExerciseById() {
-//    }
-//
+    @Test
+    void ExerciseController_GetExerciseById_Unsuccessful() {
+        when(exerciseService.findOne(Mockito.any(Long.class))).thenReturn(Optional.empty());
+
+        LinkedHashMap<String, Object> exerciseDto = dgsQueryExecutor.executeAndExtractJsonPath("query {\n" +
+                "    getExerciseById(id: 1) {" +
+                "        id" +
+                "        exerciseTypes { id name goal }\n" +
+                "        muscles { id name function }\n" +
+                "        progExercises { id name note trustLabel visibility} " +
+                "        name" +
+                "        description" +
+                "        goal\n" +
+                "    }\n" +
+                "}", "data.getExerciseById");
+
+        Assertions.assertNull(exerciseDto);
+    }
+
+    @Test
+    void ExerciseController_GetExerciseById_Success() {
+        when(exerciseService.findOne(Mockito.any(Long.class))).thenReturn(Optional.of(exercise));
+        when(exerciseMapper.mapTo(Mockito.any(ExerciseEntity.class))).thenReturn(exerciseDto);
+
+        LinkedHashMap<String, Object> exerciseDto = dgsQueryExecutor.executeAndExtractJsonPath("query {\n" +
+                "    getExerciseById(id: 1) {" +
+                "        id" +
+                "        exerciseTypes { id name goal }\n" +
+                "        muscles { id name function }\n" +
+                "        progExercises { id name note trustLabel visibility} " +
+                "        name" +
+                "        description" +
+                "        goal\n" +
+                "    }\n" +
+                "}", "data.getExerciseById");
+
+        Assertions.assertNotNull(exerciseDto);
+    }
+
 //    @Test
 //    void addExercise() {
 //    }
