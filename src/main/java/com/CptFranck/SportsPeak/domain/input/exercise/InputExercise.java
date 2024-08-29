8d80e703dbd52fa83176ac2ca1d6766373d635fd
CptFranck <francois.caillet71@gmail.com>
2024-08-29 12:45:47 +0200

    Refactor InputExercise class

--- src/main/java/com/CptFranck/SportsPeak/domain/input/exercise/InputExercise.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/exercise/InputExercise.java
@@ -3,10 +3,15 @@ package com.CptFranck.SportsPeak.domain.input.exercise;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.List;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class InputExercise extends InputNewExercise {
 
     private Long id;
 
+    public InputExercise(String name, String description, String goal, List<Long> muscleIds, List<Long> exerciseTypeIds) {
+        super(name, description, goal, muscleIds, exerciseTypeIds);
+    }
 }
