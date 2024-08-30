7761caefb93c3ad8d93d4406eb94d449b58a0dc8
CptFranck <francois.caillet71@gmail.com>
2024-08-30 18:03:53 +0200

    Refactor  JWToken

--- src/main/java/com/CptFranck/SportsPeak/domain/model/JWToken.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/model/JWToken.java
@@ -2,11 +2,13 @@ package com.CptFranck.SportsPeak.domain.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class JWToken {
     String token;
     LocalDateTime expiration;
