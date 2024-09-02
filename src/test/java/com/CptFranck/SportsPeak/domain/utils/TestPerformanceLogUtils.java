6abfe25d3f5d006b665adbf71f3d8a1826da7efc
CptFranck <francois.caillet71@gmail.com>
2024-09-02 16:01:20 +0200

    Add createTestInputNewPerformanceLog & createTestInputPerformanceLog to TestPerformanceLogUtils class

--- src/test/java/com/CptFranck/SportsPeak/domain/utils/TestPerformanceLogUtils.java
+++ src/test/java/com/CptFranck/SportsPeak/domain/utils/TestPerformanceLogUtils.java
@@ -5,6 +5,8 @@ import com.CptFranck.SportsPeak.domain.dto.TargetSetDto;
 import com.CptFranck.SportsPeak.domain.entity.PerformanceLogEntity;
 import com.CptFranck.SportsPeak.domain.entity.TargetSetEntity;
 import com.CptFranck.SportsPeak.domain.enumType.WeightUnit;
+import com.CptFranck.SportsPeak.domain.input.performanceLog.InputNewPerformanceLog;
+import com.CptFranck.SportsPeak.domain.input.performanceLog.InputPerformanceLog;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -41,6 +43,31 @@ public class TestPerformanceLogUtils {
                 WeightUnit.KILOGRAMME.label,
                 creationDate,
                 targetSet
+        );
+    }
+
+    public static InputNewPerformanceLog createTestInputNewPerformanceLog(Long targetSetId) {
+        LocalDateTime logDate = LocalDateTime.now();
+        return new InputNewPerformanceLog(
+                1,
+                5,
+                0f,
+                WeightUnit.KILOGRAMME.label,
+                logDate,
+                targetSetId
+        );
+    }
+
+    public static InputPerformanceLog createTestInputPerformanceLog(Long id, Long targetSetId) {
+        LocalDateTime logDate = LocalDateTime.now();
+        return new InputPerformanceLog(
+                id,
+                1,
+                5,
+                0f,
+                WeightUnit.KILOGRAMME.label,
+                logDate,
+                targetSetId
         );
     }
 }
