b09d79739d64db5c1306ce1bc67630e1110f161b
CptFranck <francois.caillet71@gmail.com>
2024-09-02 11:02:14 +0200

    Refactor InputNewExerciseType and InputExerciseType

--- src/main/java/com/CptFranck/SportsPeak/domain/input/exerciseType/InputExerciseType.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/exerciseType/InputExerciseType.java
@@ -1,13 +1,23 @@
 package com.CptFranck.SportsPeak.domain.input.exerciseType;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 
 
-@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class InputExerciseType extends InputNewExerciseType {
 
     private Long id;
 
+    public InputExerciseType(Long id, String name, String goal, ArrayList<Long> longs) {
+        super(name, goal, longs);
+        this.id = id;
+    }
 }
--- src/main/java/com/CptFranck/SportsPeak/domain/input/exerciseType/InputNewExerciseType.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/exerciseType/InputNewExerciseType.java
@@ -1,11 +1,15 @@
 package com.CptFranck.SportsPeak.domain.input.exerciseType;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
-@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class InputNewExerciseType {
 
     private String name;
