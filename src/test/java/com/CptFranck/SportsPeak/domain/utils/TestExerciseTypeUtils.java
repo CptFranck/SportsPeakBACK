edc3614f7adab65babec68051f053de6ea9fa19f
CptFranck <francois.caillet71@gmail.com>
2024-09-02 11:01:47 +0200

    Add InputNewExerciseType and InputExerciseType functions to TestExerciseTypeUtils

--- src/test/java/com/CptFranck/SportsPeak/domain/utils/TestExerciseTypeUtils.java
+++ src/test/java/com/CptFranck/SportsPeak/domain/utils/TestExerciseTypeUtils.java
@@ -4,7 +4,10 @@ import com.CptFranck.SportsPeak.domain.dto.ExerciseDto;
 import com.CptFranck.SportsPeak.domain.dto.ExerciseTypeDto;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
+import com.CptFranck.SportsPeak.domain.input.exerciseType.InputExerciseType;
+import com.CptFranck.SportsPeak.domain.input.exerciseType.InputNewExerciseType;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -29,6 +32,24 @@ public class TestExerciseTypeUtils {
                 "Exercise type name",
                 "Exercise type goal",
                 new HashSet<ExerciseDto>()
+        );
+    }
+
+
+    public static InputNewExerciseType createTestInputNewExerciseType() {
+        return new InputNewExerciseType(
+                "name",
+                "goal",
+                new ArrayList<Long>()
+        );
+    }
+
+    public static InputExerciseType createTestInputExerciseType() {
+        return new InputExerciseType(
+                1L,
+                "name",
+                "goal",
+                new ArrayList<Long>()
         );
     }
 }
