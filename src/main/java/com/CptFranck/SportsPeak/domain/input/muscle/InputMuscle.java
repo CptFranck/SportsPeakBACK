4ad82ee3b11ffe2154f30168ca7dcfa78bc66211
CptFranck <francois.caillet71@gmail.com>
2024-09-02 11:55:01 +0200

    Refactor InputNewMuscle and InputMuscle

--- src/main/java/com/CptFranck/SportsPeak/domain/input/muscle/InputMuscle.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/muscle/InputMuscle.java
@@ -1,12 +1,22 @@
 package com.CptFranck.SportsPeak.domain.input.muscle;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+import java.util.ArrayList;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class InputMuscle extends InputNewMuscle {
 
     private Long id;
 
+    public InputMuscle(Long id, String name, String function, String description, ArrayList<Long> exerciseIds) {
+        super(name, function, description, exerciseIds);
+        this.id = id;
+    }
 }
