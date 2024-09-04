ec72105765a02a645938ad28cb2ffbfae3306607
CptFranck <francois.caillet71@gmail.com>
2024-09-04 11:48:34 +0200

    Refactor TargetSetMapperImpl

--- src/main/java/com/CptFranck/SportsPeak/mappers/impl/TargetSetMapperImpl.java
+++ src/main/java/com/CptFranck/SportsPeak/mappers/impl/TargetSetMapperImpl.java
@@ -2,19 +2,10 @@ package com.CptFranck.SportsPeak.mappers.impl;
 
 import com.CptFranck.SportsPeak.domain.dto.TargetSetDto;
 import com.CptFranck.SportsPeak.domain.entity.TargetSetEntity;
-import com.CptFranck.SportsPeak.domain.enumType.TargetSetState;
-import com.CptFranck.SportsPeak.domain.enumType.WeightUnit;
-import com.CptFranck.SportsPeak.domain.input.duration.InputDuration;
 import com.CptFranck.SportsPeak.mappers.Mapper;
-import com.CptFranck.SportsPeak.mappers.typeConverter.DurationToInputDurationConverter;
-import com.CptFranck.SportsPeak.mappers.typeConverter.TargetSetStateToStringConverter;
-import com.CptFranck.SportsPeak.mappers.typeConverter.WeightUnitToStringConverter;
-import org.modelmapper.Converter;
+import com.CptFranck.SportsPeak.mappers.typeConverter.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
-
-import java.time.Duration;
-
 
 @Component
 public class TargetSetMapperImpl implements Mapper<TargetSetEntity, TargetSetDto> {
@@ -24,22 +15,11 @@ public class TargetSetMapperImpl implements Mapper<TargetSetEntity, TargetSetDto
     public TargetSetMapperImpl(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
         this.modelMapper.addConverter(new DurationToInputDurationConverter());
+        this.modelMapper.addConverter(new InputDurationToDurationConverter());
         this.modelMapper.addConverter(new WeightUnitToStringConverter());
+        this.modelMapper.addConverter(new StringToWeightUnitConverter());
         this.modelMapper.addConverter(new TargetSetStateToStringConverter());
-
-        Converter<String, WeightUnit> toWeightUnit =
-                ctx -> ctx.getSource() == null ? null : WeightUnit.valueOfLabel(ctx.getSource());
-        Converter<String, TargetSetState> toTargetSetState =
-                ctx -> ctx.getSource() == null ? null : TargetSetState.valueOfLabel(ctx.getSource());
-        Converter<InputDuration, Duration> toInputDuration =
-                ctx -> ctx.getSource() == null ? null : ctx.getSource().InputDurationToDuration();
-
-        this.modelMapper.createTypeMap(TargetSetDto.class, TargetSetEntity.class).addMappings(mapper -> {
-            mapper.using(toWeightUnit).map(TargetSetDto::getWeightUnit, TargetSetEntity::setWeightUnit);
-            mapper.using(toTargetSetState).map(TargetSetDto::getState, TargetSetEntity::setState);
-            mapper.using(toInputDuration).map(TargetSetDto::getPhysicalExertionUnitTime, TargetSetEntity::setPhysicalExertionUnitTime);
-            mapper.using(toInputDuration).map(TargetSetDto::getRestTime, TargetSetEntity::setRestTime);
-        });
+        this.modelMapper.addConverter(new StringToTargetSetStateConverter());
     }
 
     @Override
