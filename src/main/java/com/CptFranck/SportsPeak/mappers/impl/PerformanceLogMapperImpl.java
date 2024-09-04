cc44d9b4a9b22b4084f511cefdea3fc17192e8e0
CptFranck <francois.caillet71@gmail.com>
2024-09-04 11:49:53 +0200

    Refactor PerformanceLogMapperImpl

--- src/main/java/com/CptFranck/SportsPeak/mappers/impl/PerformanceLogMapperImpl.java
+++ src/main/java/com/CptFranck/SportsPeak/mappers/impl/PerformanceLogMapperImpl.java
@@ -2,10 +2,9 @@ package com.CptFranck.SportsPeak.mappers.impl;
 
 import com.CptFranck.SportsPeak.domain.dto.PerformanceLogDto;
 import com.CptFranck.SportsPeak.domain.entity.PerformanceLogEntity;
-import com.CptFranck.SportsPeak.domain.enumType.WeightUnit;
 import com.CptFranck.SportsPeak.mappers.Mapper;
+import com.CptFranck.SportsPeak.mappers.typeConverter.StringToWeightUnitConverter;
 import com.CptFranck.SportsPeak.mappers.typeConverter.WeightUnitToStringConverter;
-import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +16,7 @@ public class PerformanceLogMapperImpl implements Mapper<PerformanceLogEntity, Pe
     public PerformanceLogMapperImpl(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
         this.modelMapper.addConverter(new WeightUnitToStringConverter());
-
-        Converter<String, WeightUnit> toWeightUnit =
-                ctx -> ctx.getSource() == null ? null : WeightUnit.valueOfLabel(ctx.getSource());
-
-        this.modelMapper.createTypeMap(PerformanceLogDto.class, PerformanceLogEntity.class).addMappings(mapper -> {
-            mapper.using(toWeightUnit).map(PerformanceLogDto::getWeightUnit, PerformanceLogEntity::setWeightUnit);
-        });
+        this.modelMapper.addConverter(new StringToWeightUnitConverter());
     }
 
     @Override
