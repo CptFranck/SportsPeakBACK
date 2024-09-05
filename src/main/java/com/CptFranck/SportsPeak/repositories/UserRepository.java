5d9b32cfa9a90dfd83126104d58fdf64cc8f6fb6
CptFranck <francois.caillet71@gmail.com>
2024-09-05 16:12:30 +0200

    Add findAllBySubscribedProgExercisesContaining method to UserRepository interface

--- src/main/java/com/CptFranck/SportsPeak/repositories/UserRepository.java
+++ src/main/java/com/CptFranck/SportsPeak/repositories/UserRepository.java
@@ -1,12 +1,16 @@
 package com.CptFranck.SportsPeak.repositories;
 
+import com.CptFranck.SportsPeak.domain.entity.ProgExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String login);
 
     Optional<UserEntity> findByUsername(String login);
+
+    List<UserEntity> findAllBySubscribedProgExercisesContaining(ProgExerciseEntity progExercise);
 }
