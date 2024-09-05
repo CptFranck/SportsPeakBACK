4282b86346bcd4aafa18a4cf620c395205654da6
CptFranck <francois.caillet71@gmail.com>
2024-09-05 16:12:51 +0200

    Add userRepository_FindAllBySubscribedProgExercisesContaining_ReturnUser to  UserRepositoryTest class

--- src/test/java/com/CptFranck/SportsPeak/repositories/UserRepositoryTest.java
+++ src/test/java/com/CptFranck/SportsPeak/repositories/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.CptFranck.SportsPeak.repositories;
 
+import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
+import com.CptFranck.SportsPeak.domain.entity.ProgExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,9 +10,13 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 
+import static com.CptFranck.SportsPeak.domain.utils.TestExerciseUtils.createTestExercise;
+import static com.CptFranck.SportsPeak.domain.utils.TestProgExerciseUtils.createTestProgExercise;
 import static com.CptFranck.SportsPeak.domain.utils.TestUserUtils.createTestUser;
+import static com.CptFranck.SportsPeak.domain.utils.TestUserUtils.createTestUserBis;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -18,6 +24,12 @@ public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+
+    @Autowired
+    private ProgExerciseRepository progExerciseRepository;
 
     private UserEntity saveOneUserInRepository() {
         UserEntity user = createTestUser(null);
@@ -44,5 +56,22 @@ public class UserRepositoryTest {
         Assertions.assertNotNull(foundUser);
         Assertions.assertTrue(foundUser.isPresent());
         Assertions.assertNotNull(foundUser.get());
+    }
+
+    @Test
+    public void userRepository_FindAllBySubscribedProgExercisesContaining_ReturnUser() {
+        UserEntity creator = saveOneUserInRepository();
+        UserEntity subscriber = userRepository.save(createTestUserBis(null));
+        ExerciseEntity exercise = exerciseRepository.save(createTestExercise(1L));
+        ProgExerciseEntity progExercise = progExerciseRepository.save(createTestProgExercise(1L, creator, exercise));
+        creator.getSubscribedProgExercises().add(progExercise);
+        creator.getProgExercisesCreated().add(progExercise);
+        subscriber.getSubscribedProgExercises().add(progExercise);
+
+        List<UserEntity> foundUsers = userRepository.findAllBySubscribedProgExercisesContaining(progExercise);
+
+        Assertions.assertNotNull(foundUsers);
+        Assertions.assertTrue(foundUsers.contains(creator));
+        Assertions.assertTrue(foundUsers.contains(subscriber));
     }
 }
