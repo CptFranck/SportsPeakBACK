42dd10e3805b28c241fbe3c0ef973e5649d9b251
CptFranck <francois.caillet71@gmail.com>
2024-09-06 11:22:03 +0200

    Remove unused exception

--- src/main/java/com/CptFranck/SportsPeak/domain/exception/progExercise/ProgExercisePublicException.java
+++
@@ -1,7 +0,0 @@
-package com.CptFranck.SportsPeak.domain.exception.progExercise;
-
-public class ProgExercisePublicException extends RuntimeException {
-    public ProgExercisePublicException(Long id) {
-        super(String.format("The progExercise with the id %s still in public, set it to private first before delete it", id.toString()));
-    }
-}
