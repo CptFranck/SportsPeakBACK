9440b21f85d93f0ae36a3f1a26a77540ec82c51a
CptFranck <francois.caillet71@gmail.com>
2024-10-03 12:17:38 +0200

    Add PerformanceLogController unit Test

---
+++ src/test/java/com/CptFranck/SportsPeak/controller/UnitTest/PerformanceLogControllerTest.java
@@ -0,0 +1,191 @@
+package com.CptFranck.SportsPeak.controller.UnitTest;
+
+import com.CptFranck.SportsPeak.config.graphql.LocalDateTimeScalar;
+import com.CptFranck.SportsPeak.controller.PerformanceLogController;
+import com.CptFranck.SportsPeak.domain.dto.*;
+import com.CptFranck.SportsPeak.domain.entity.*;
+import com.CptFranck.SportsPeak.mappers.Mapper;
+import com.CptFranck.SportsPeak.service.PerformanceLogService;
+import com.CptFranck.SportsPeak.service.TargetSetService;
+import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration;
+import com.netflix.graphql.dgs.exceptions.QueryException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.CptFranck.SportsPeak.domain.utils.TestExerciseUtils.createTestExercise;
+import static com.CptFranck.SportsPeak.domain.utils.TestExerciseUtils.createTestExerciseDto;
+import static com.CptFranck.SportsPeak.domain.utils.TestPerformanceLogUtils.*;
+import static com.CptFranck.SportsPeak.domain.utils.TestProgExerciseUtils.createTestProgExercise;
+import static com.CptFranck.SportsPeak.domain.utils.TestProgExerciseUtils.createTestProgExerciseDto;
+import static com.CptFranck.SportsPeak.domain.utils.TestTargetSetUtils.createTestTargetSet;
+import static com.CptFranck.SportsPeak.domain.utils.TestTargetSetUtils.createTestTargetSetDto;
+import static com.CptFranck.SportsPeak.domain.utils.TestUserUtils.createTestUser;
+import static com.CptFranck.SportsPeak.domain.utils.TestUserUtils.createTestUserDto;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = {
+        DgsAutoConfiguration.class,
+        LocalDateTimeScalar.class,
+        PerformanceLogController.class
+})
+class PerformanceLogControllerTest {
+
+    @InjectMocks
+    private PerformanceLogController performanceLogController;
+
+    @Mock
+    private Mapper<PerformanceLogEntity, PerformanceLogDto> performanceLogMapper;
+
+    @Mock
+    private PerformanceLogService performanceLogService;
+
+    @Mock
+    private TargetSetService targetSetService;
+
+    private PerformanceLogEntity performanceLog;
+    private PerformanceLogDto performanceLogDto;
+    private TargetSetEntity targetSet;
+
+    @BeforeEach
+    void init() {
+        UserEntity user = createTestUser(1L);
+        UserDto userDto = createTestUserDto(1L);
+        ExerciseEntity exercise = createTestExercise(1L);
+        ExerciseDto exerciseDto = createTestExerciseDto(1L);
+        ProgExerciseEntity progExercise = createTestProgExercise(1L, user, exercise);
+        ProgExerciseDto progExerciseDto = createTestProgExerciseDto(1L, userDto, exerciseDto);
+        targetSet = createTestTargetSet(1L, progExercise, null);
+        TargetSetDto targetSetDto = createTestTargetSetDto(1L, progExerciseDto, null);
+        performanceLog = createTestPerformanceLog(1L, targetSet);
+        performanceLogDto = createTestPerformanceLogDto(1L, targetSetDto);
+    }
+
+    @Test
+    void PerformanceLogController_GetPerformanceLogs_Success() {
+        when(performanceLogService.findAll()).thenReturn(List.of(performanceLog));
+        when(performanceLogMapper.mapTo(Mockito.any(PerformanceLogEntity.class))).thenReturn(performanceLogDto);
+
+        List<PerformanceLogDto> performanceLogDtos = performanceLogController.getPerformanceLogs();
+
+        Assertions.assertNotNull(performanceLogDtos);
+    }
+
+    @Test
+    void PerformanceLogController_GetPerformanceLogById_Unsuccessful() {
+        when(performanceLogService.findOne(Mockito.any(Long.class))).thenReturn(Optional.empty());
+
+        PerformanceLogDto performanceLogDto = performanceLogController.getPerformanceLogById(1L);
+
+        Assertions.assertNull(performanceLogDto);
+    }
+
+    @Test
+    void PerformanceLogController_GetPerformanceLogById_Success() {
+        when(performanceLogService.findOne(Mockito.any(Long.class))).thenReturn(Optional.of(performanceLog));
+        when(performanceLogMapper.mapTo(Mockito.any(PerformanceLogEntity.class))).thenReturn(performanceLogDto);
+
+        PerformanceLogDto performanceLogDto = performanceLogController.getPerformanceLogById(1L);
+
+        Assertions.assertNotNull(performanceLogDto);
+    }
+
+    @Test
+    void PerformanceLogController_GetPerformanceLogsByTargetId_Success() {
+        when(performanceLogService.findAllByTargetSetId(Mockito.any(Long.class))).thenReturn(List.of(performanceLog));
+        when(performanceLogMapper.mapTo(Mockito.any(PerformanceLogEntity.class))).thenReturn(performanceLogDto);
+
+        List<PerformanceLogDto> performanceLogDtos = performanceLogController.getPerformanceLogsByTargetSetsId(1L);
+
+        Assertions.assertNotNull(performanceLogDtos);
+    }
+
+    @Test
+    void PerformanceLogController_AddPerformanceLog_UnsuccessfulTargetSetNotFound() {
+        when(targetSetService.findOne(Mockito.any(Long.class))).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(QueryException.class,
+                () -> performanceLogController.addPerformanceLog(
+                        createTestInputNewPerformanceLog(1L, false))
+        );
+    }
+
+    @Test
+    void PerformanceLogController_AddPerformanceLog_UnsuccessfulWrongLabel() {
+        when(targetSetService.findOne(Mockito.any(Long.class))).thenReturn(Optional.of(targetSet));
+
+        Assertions.assertThrows(QueryException.class,
+                () -> performanceLogController.addPerformanceLog(
+                        createTestInputNewPerformanceLog(1L, true))
+        );
+    }
+
+    @Test
+    void PerformanceLogController_AddPerformanceLog_Success() {
+        when(targetSetService.findOne(Mockito.any(Long.class))).thenReturn(Optional.of(targetSet));
+        when(performanceLogService.save(Mockito.any(PerformanceLogEntity.class))).thenReturn(performanceLog);
+        when(performanceLogMapper.mapTo(Mockito.any(PerformanceLogEntity.class))).thenReturn(performanceLogDto);
+
+        PerformanceLogDto performanceLogDto = performanceLogController.addPerformanceLog(
+                createTestInputNewPerformanceLog(1L, false)
+        );
+
+        Assertions.assertNotNull(performanceLogDto);
+    }
+
+    @Test
+    void PerformanceLogController_ModifyPerformanceLog_UnsuccessfulDoesNotExist() {
+        when(performanceLogService.exists(Mockito.any(Long.class))).thenReturn(false);
+
+        PerformanceLogDto performanceLogDto = performanceLogController.modifyPerformanceLog(
+                createTestInputPerformanceLog(1L, 1L, false)
+        );
+
+        Assertions.assertNull(performanceLogDto);
+    }
+
+    @Test
+    void PerformanceLogController_ModifyPerformanceLog_UnsuccessfulWrongLabel() {
+        when(performanceLogService.exists(Mockito.any(Long.class))).thenReturn(true);
+        when(targetSetService.findOne(Mockito.any(Long.class))).thenReturn(Optional.of(targetSet));
+
+        Assertions.assertThrows(QueryException.class,
+                () -> performanceLogController.modifyPerformanceLog(
+                        createTestInputPerformanceLog(1L, 1L, true)
+                )
+        );
+    }
+
+    @Test
+    void PerformanceLogController_ModifyPerformanceLog_Success() {
+        when(performanceLogService.exists(Mockito.any(Long.class))).thenReturn(true);
+        when(targetSetService.findOne(Mockito.any(Long.class))).thenReturn(Optional.of(targetSet));
+        when(performanceLogService.save(Mockito.any(PerformanceLogEntity.class))).thenReturn(performanceLog);
+        when(performanceLogMapper.mapTo(Mockito.any(PerformanceLogEntity.class))).thenReturn(performanceLogDto);
+
+        PerformanceLogDto performanceLogDto = performanceLogController.modifyPerformanceLog(
+                createTestInputPerformanceLog(1L, 1L, false)
+        );
+
+        Assertions.assertNotNull(performanceLogDto);
+    }
+
+    @Test
+    void PerformanceLogController_DeletePerformanceLog_Success() {
+        when(performanceLogService.exists(Mockito.any(Long.class))).thenReturn(true);
+
+        Long id = performanceLogController.deletePerformanceLog(1L);
+
+        Assertions.assertNotNull(id);
+    }
+}
