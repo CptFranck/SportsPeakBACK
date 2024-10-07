c2eddef55c4e15415d44fa0179d856ef99d328ce
CptFranck <francois.caillet71@gmail.com>
2024-10-07 16:02:17 +0200

    Refactor Auth Controller Integration Test

--- src/test/java/com/CptFranck/SportsPeak/controller/IntegrationTest/AuthControllerIntTest.java
+++ src/test/java/com/CptFranck/SportsPeak/controller/IntegrationTest/AuthControllerIntTest.java
@@ -1,149 +1,96 @@
 package com.CptFranck.SportsPeak.controller.IntegrationTest;
 
-import com.CptFranck.SportsPeak.config.graphql.LocalDateTimeScalar;
-import com.CptFranck.SportsPeak.config.security.JwtProvider;
 import com.CptFranck.SportsPeak.controller.AuthController;
-import com.CptFranck.SportsPeak.domain.dto.UserDto;
-import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
+import com.CptFranck.SportsPeak.domain.dto.AuthDto;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import com.CptFranck.SportsPeak.domain.exception.userAuth.EmailAlreadyUsedException;
 import com.CptFranck.SportsPeak.domain.input.credentials.InputCredentials;
 import com.CptFranck.SportsPeak.domain.input.user.InputRegisterNewUser;
-import com.CptFranck.SportsPeak.domain.model.JWToken;
-import com.CptFranck.SportsPeak.mappers.Mapper;
+import com.CptFranck.SportsPeak.repositories.RoleRepository;
+import com.CptFranck.SportsPeak.repositories.UserRepository;
 import com.CptFranck.SportsPeak.service.AuthService;
-import com.CptFranck.SportsPeak.service.RoleService;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.netflix.graphql.dgs.DgsQueryExecutor;
-import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration;
-import com.netflix.graphql.dgs.exceptions.QueryException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.context.TestPropertySource;
 
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Optional;
-
-import static com.CptFranck.SportsPeak.controller.IntegrationTest.graphqlQuery.AuthQuery.loginQuery;
-import static com.CptFranck.SportsPeak.controller.IntegrationTest.graphqlQuery.AuthQuery.registerQuery;
+import static com.CptFranck.SportsPeak.domain.utils.TestRoleUtils.createTestRole;
 import static com.CptFranck.SportsPeak.domain.utils.TestUserUtils.createTestUser;
-import static com.CptFranck.SportsPeak.domain.utils.TestUserUtils.createTestUserDto;
-import static org.mockito.Mockito.when;
+import static com.CptFranck.SportsPeak.domain.utils.TestUserUtils.createTestUserBis;
 
-@SpringBootTest(classes = {
-        DgsAutoConfiguration.class,
-        LocalDateTimeScalar.class,
-        AuthController.class
-})
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
 public class AuthControllerIntTest {
 
     @Autowired
-    private DgsQueryExecutor dgsQueryExecutor;
+    private AuthController authController;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
     private AuthService authService;
 
-    @MockBean
-    private RoleService roleService;
+    @Autowired
+    private UserRepository userRepository;
 
-    @MockBean
-    private JwtProvider userAuthProvider;
-
-    @MockBean
-    private Mapper<UserEntity, UserDto> userMapper;
-
-    @MockBean
-    private AuthenticationManager authenticationManager;
+    @Autowired
+    private RoleRepository roleRepository;
 
     private UserEntity user;
-    private UserDto userDto;
-    private JWToken jwToken;
-    private LinkedHashMap<String, Object> variables;
+
+    private String rawPassword;
 
     @BeforeEach
     void setUp() {
-        user = createTestUser(1L);
-        userDto = createTestUserDto(1L);
-        jwToken = new JWToken("token", LocalDateTime.now());
-        variables = new LinkedHashMap<>();
+        user = createTestUser(null);
+        rawPassword = user.getPassword();
+        user = authService.register(user);
+        roleRepository.save(createTestRole(null, 0));
+    }
+
+    @AfterEach
+    public void afterEach() {
+        userRepository.deleteAll();
+        roleRepository.deleteAll();
     }
 
     @Test
     public void AuthController_Login_ReturnAuthDto() {
-        variables.put("inputCredentials", objectMapper.convertValue(
-                new InputCredentials(user.getEmail(), user.getPassword()),
-                new TypeReference<LinkedHashMap<String, Object>>() {
-                }
-                )
-        );
-        when(authService.login(Mockito.any(InputCredentials.class))).thenReturn(user);
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
-        when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(null);
-        when(userAuthProvider.generateToken(Mockito.any())).thenReturn(jwToken);
+        InputCredentials inputCredentials = new InputCredentials(user.getEmail(), rawPassword);
 
-        LinkedHashMap<String, Object> authDto =
-                dgsQueryExecutor.executeAndExtractJsonPath(loginQuery, "data.login", variables);
+        AuthDto authDto = authController.login(inputCredentials);
 
         Assertions.assertNotNull(authDto);
     }
 
     @Test
-    public void AuthController_Register_Unsuccessful() {
-        variables.put("inputRegisterNewUser", objectMapper.convertValue(
-                new InputRegisterNewUser(
-                                user.getEmail(),
-                                user.getFirstName(),
-                                user.getLastName(),
-                                user.getLastName(),
-                                user.getPassword()
-                        ),
-                new TypeReference<LinkedHashMap<String, Object>>() {
-                }
-                )
+    public void AuthController_Register_UnsuccessfulEmailAlreadyUsed() {
+        InputRegisterNewUser inputCredentials = new InputRegisterNewUser(
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getLastName(),
+                rawPassword
         );
-        when(roleService.findByName(Mockito.any(String.class))).thenReturn(Optional.empty());
 
-        Assertions.assertThrows(QueryException.class,
-                () -> dgsQueryExecutor.executeAndExtractJsonPath(registerQuery, "data.register", variables));
+        Assertions.assertThrows(EmailAlreadyUsedException.class,
+                () -> authController.register(inputCredentials));
     }
 
     @Test
     public void AuthController_Register_ReturnAuthDto() {
-        variables.put("inputRegisterNewUser", objectMapper.convertValue(
-                new InputRegisterNewUser(
-                        user.getEmail(),
-                        user.getFirstName(),
-                        user.getLastName(),
-                        user.getLastName(),
-                        user.getPassword()
-                ),
-                new TypeReference<LinkedHashMap<String, Object>>() {
-                }
-                )
-        );
-        RoleEntity role = new RoleEntity(1L, "Role", new HashSet<>(), new HashSet<>());
-        when(roleService.findByName(Mockito.any(String.class))).thenReturn(Optional.of(role));
-        when(authService.register(Mockito.any(UserEntity.class))).thenReturn(user);
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
-        when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(null);
-        when(userAuthProvider.generateToken(Mockito.any())).thenReturn(jwToken);
+        UserEntity userBis = createTestUserBis(null);
 
-        LinkedHashMap<String, Object> authDto =
-                dgsQueryExecutor.executeAndExtractJsonPath(registerQuery, "data.register", variables);
+        InputRegisterNewUser inputCredentials = new InputRegisterNewUser(
+                userBis.getEmail(),
+                userBis.getFirstName(),
+                userBis.getLastName(),
+                userBis.getLastName(),
+                userBis.getPassword()
+        );
+
+        AuthDto authDto = authController.register(inputCredentials);
 
         Assertions.assertNotNull(authDto);
     }
