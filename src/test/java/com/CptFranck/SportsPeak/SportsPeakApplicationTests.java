ce3f6dd92ec74b3eb2c2a0cddf064c2cbdf1c94e
CptFranck <francois.caillet71@gmail.com>
2024-10-22 16:56:20 +0200

    Try to fix test

--- src/test/java/com/CptFranck/SportsPeak/SportsPeakApplicationTests.java
+++ src/test/java/com/CptFranck/SportsPeak/SportsPeakApplicationTests.java
@@ -2,8 +2,10 @@ package com.CptFranck.SportsPeak;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
 class SportsPeakApplicationTests {
 
     @Test
