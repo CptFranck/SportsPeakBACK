3d0cc106f118cbd3a1048e784f6bfe9c23c485ea
CptFranck <francois.caillet71@gmail.com>
2025-04-22 15:40:16 +0200

    update MuscleQuery.java

--- src/test/java/com/CptFranck/SportsPeak/controller/IntegrationTest/graphqlQuery/MuscleQuery.java
+++ src/test/java/com/CptFranck/SportsPeak/controller/IntegrationTest/graphqlQuery/MuscleQuery.java
@@ -7,72 +7,76 @@ public class MuscleQuery {
     @Language("GraphQL")
     public static String getMusclesQuery = """
             query {
-                     getMuscles {
+                 getMuscles {
+                     id
+                     name
+                     latinName
+                     function
+                     exercises {
                          id
-                         exercises {
-                             id
-                             name
-                             goal
-                         }
                          name
-                         function
+                         goal
                      }
                  }
+             }
             """;
 
     @Language("GraphQL")
     public static String getMuscleByIdQuery = """
             query ($id : Int!) {
-                      getMuscleById (id : $id) {
+                  getMuscleById (id : $id) {
+                      id
+                      name
+                      latinName
+                      function
+                      exercises {
                           id
-                          exercises {
-                              id
-                              name
-                              goal
-                          }
                           name
-                          function
+                          goal
                       }
                   }
+              }
             """;
 
     @Language("GraphQL")
     public static String addMuscleQuery = """
             mutation ($inputNewMuscle : InputNewMuscle!){
-                       addMuscle(inputNewMuscle: $inputNewMuscle) {
+                   addMuscle(inputNewMuscle: $inputNewMuscle) {
+                       id
+                       name
+                       latinName
+                       function
+                       exercises {
                            id
                            name
-                           function
-                           exercises {
-                               id
-                               name
-                               goal
-                           }
+                           goal
                        }
                    }
+               }
              """;
 
     @Language("GraphQL")
     public static String modifyMuscleQuery = """
             mutation ($inputMuscle : InputMuscle!){
-                       modifyMuscle(inputMuscle: $inputMuscle) {
+                   modifyMuscle(inputMuscle: $inputMuscle) {
+                       id
+                       name
+                       latinName
+                       function
+                       exercises {
                            id
                            name
-                           function
-                           exercises {
-                               id
-                               name
-                               goal
-                           }
+                           goal
                        }
                    }
+               }
             """;
 
     @Language("GraphQL")
     public static String deleteMuscleQuery = """
              mutation ($muscleId : Int!){
-                        deleteMuscle(muscleId: $muscleId)
-                    }
+                    deleteMuscle(muscleId: $muscleId)
+                }
             """;
 }
 
