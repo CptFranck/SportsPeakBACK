d6957bbe0ace507344a075c9c47354ee1494d251
CptFranck <francois.caillet71@gmail.com>
2025-04-25 11:31:20 +0200

    add StorageFileNotFoundException class

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/exception/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package com.CptFranck.SportsPeak.domain.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
