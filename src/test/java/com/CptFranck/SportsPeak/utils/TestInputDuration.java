0787443e7c0fb5c939431340ca628b6f4ebcaf70
CptFranck <francois.caillet71@gmail.com>
2025-04-30 13:11:37 +0200

    add assertInputDuration with TestInputDuration file

---
+++ src/test/java/com/CptFranck/SportsPeak/utils/TestInputDuration.java
@@ -0,0 +1,14 @@
+package com.CptFranck.SportsPeak.utils;
+
+import com.CptFranck.SportsPeak.domain.input.duration.InputDuration;
+import org.junit.jupiter.api.Assertions;
+
+
+public class TestInputDuration {
+
+    public static void assertInputDuration(InputDuration expected, InputDuration actual) {
+        Assertions.assertEquals(expected.getHours(), actual.getHours());
+        Assertions.assertEquals(expected.getMinutes(), actual.getMinutes());
+        Assertions.assertEquals(expected.getSeconds(), actual.getSeconds());
+    }
+}
