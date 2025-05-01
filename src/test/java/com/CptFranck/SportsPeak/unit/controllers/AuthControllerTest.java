de3e5cea4cfc89a37b7e3a64c21f015ecba35117
CptFranck <francois.caillet71@gmail.com>
2025-05-01 16:09:14 +0200

    Update AuthControllerTest

--- src/test/java/com/CptFranck/SportsPeak/unit/controllers/AuthControllerTest.java
+++ src/test/java/com/CptFranck/SportsPeak/unit/controllers/AuthControllerTest.java
@@ -4,15 +4,12 @@ import com.CptFranck.SportsPeak.config.security.JwtProvider;
 import com.CptFranck.SportsPeak.controller.AuthController;
 import com.CptFranck.SportsPeak.domain.dto.AuthDto;
 import com.CptFranck.SportsPeak.domain.dto.UserDto;
-import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
-import com.CptFranck.SportsPeak.domain.exception.role.RoleNotFoundException;
 import com.CptFranck.SportsPeak.domain.input.credentials.InputCredentials;
 import com.CptFranck.SportsPeak.domain.input.user.InputRegisterNewUser;
 import com.CptFranck.SportsPeak.domain.model.JWToken;
 import com.CptFranck.SportsPeak.mappers.Mapper;
 import com.CptFranck.SportsPeak.service.AuthService;
-import com.CptFranck.SportsPeak.service.RoleService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,9 +22,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Optional;
 
+import static com.CptFranck.SportsPeak.utils.AuthUtils.createInputCredentials;
+import static com.CptFranck.SportsPeak.utils.AuthUtils.createInputRegisterNewUser;
 import static com.CptFranck.SportsPeak.utils.TestUserUtils.createTestUser;
 import static com.CptFranck.SportsPeak.utils.TestUserUtils.createTestUserDto;
 import static org.mockito.Mockito.when;
@@ -40,9 +37,6 @@ public class AuthControllerTest {
 
     @Mock
     private AuthService authService;
-
-    @Mock
-    private RoleService roleService;
 
     @Mock
     private JwtProvider userAuthProvider;
@@ -65,52 +59,26 @@ public class AuthControllerTest {
         jwToken = new JWToken("token", LocalDateTime.now());
         authDto = new AuthDto(jwToken, userDto);
     }
-
     @Test
-    public void AuthController_Login_ReturnAuthDto() {
+    public void login_ValidCredentials_ReturnsAuthDto() {
         when(authService.login(Mockito.any(InputCredentials.class))).thenReturn(user);
         when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
         when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(null);
         when(userAuthProvider.generateToken(Mockito.any())).thenReturn(jwToken);
 
-        AuthDto authDtoReturn = authController.login(new InputCredentials(user.getEmail(), user.getPassword()));
+        AuthDto authDtoReturn = authController.login(createInputCredentials(user));
 
         assertEqualsAuth(authDto, authDtoReturn);
     }
 
     @Test
-    public void AuthController_Register_Unsuccessful() {
-        when(roleService.findByName(Mockito.any(String.class))).thenReturn(Optional.empty());
-
-        Assertions.assertThrows(RoleNotFoundException.class,
-                () -> authController.register(new InputRegisterNewUser(
-                                user.getEmail(),
-                                user.getFirstName(),
-                                user.getLastName(),
-                                user.getLastName(),
-                                user.getPassword()
-                        )
-                )
-        );
-    }
-
-    @Test
-    public void AuthController_Register_ReturnAuthDto() {
-        RoleEntity role = new RoleEntity(1L, "Role", new HashSet<>(), new HashSet<>());
-        when(roleService.findByName(Mockito.any(String.class))).thenReturn(Optional.of(role));
-        when(authService.register(Mockito.any(UserEntity.class))).thenReturn(user);
+    public void register_ValidCredentials_ReturnsAuthDto() {
+        when(authService.register(Mockito.any(InputRegisterNewUser.class))).thenReturn(user);
         when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
         when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(null);
         when(userAuthProvider.generateToken(Mockito.any())).thenReturn(jwToken);
 
-        AuthDto authDtoReturn = authController.register(new InputRegisterNewUser(
-                        user.getEmail(),
-                        user.getFirstName(),
-                        user.getLastName(),
-                        user.getLastName(),
-                        user.getPassword()
-                )
-        );
+        AuthDto authDtoReturn = authController.register(createInputRegisterNewUser(user));
 
         assertEqualsAuth(authDto, authDtoReturn);
     }
