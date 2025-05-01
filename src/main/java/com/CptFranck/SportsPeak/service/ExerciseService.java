36134a57080bf5321999ac84bda9e24206ece7f8
CptFranck <francois.caillet71@gmail.com>
2025-05-01 17:59:00 +0200

    Update ExerciseService

--- src/main/java/com/CptFranck/SportsPeak/service/ExerciseService.java
+++ src/main/java/com/CptFranck/SportsPeak/service/ExerciseService.java
@@ -4,18 +4,27 @@ import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
 import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
 import com.CptFranck.SportsPeak.domain.entity.ProgExerciseEntity;
+import com.CptFranck.SportsPeak.domain.input.exercise.InputExercise;
+import com.CptFranck.SportsPeak.domain.input.exercise.InputNewExercise;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface ExerciseService {
 
     List<ExerciseEntity> findAll();
 
-    Optional<ExerciseEntity> findOne(Long id);
+    ExerciseEntity findOne(Long id);
 
     Set<ExerciseEntity> findMany(Set<Long> ids);
+
+    ExerciseEntity create(InputNewExercise inputNewExercise);
+
+    ExerciseEntity update(InputExercise inputExercise);
+
+    ExerciseEntity save(ExerciseEntity exercise);
+
+    void delete(Long id);
 
     void updateExerciseTypeRelation(Set<Long> newIds, Set<Long> oldIds, ExerciseTypeEntity exerciseType);
 
@@ -24,8 +33,4 @@ public interface ExerciseService {
     void updateProgExerciseRelation(ExerciseEntity newExercise, ExerciseEntity oldExercise, ProgExerciseEntity progExercise);
 
     boolean exists(Long id);
-
-    ExerciseEntity save(ExerciseEntity exercise);
-
-    void delete(Long id);
 }
