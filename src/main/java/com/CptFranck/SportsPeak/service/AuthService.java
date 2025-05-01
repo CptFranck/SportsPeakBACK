ae964076fdfe0ea9e0dbefd5cc21706b08d976f2
CptFranck <francois.caillet71@gmail.com>
2025-05-01 16:08:41 +0200

    Refactor AuthService interface

--- src/main/java/com/CptFranck/SportsPeak/service/AuthService.java
+++ src/main/java/com/CptFranck/SportsPeak/service/AuthService.java
@@ -3,11 +3,12 @@ package com.CptFranck.SportsPeak.service;
 
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
 import com.CptFranck.SportsPeak.domain.input.credentials.InputCredentials;
+import com.CptFranck.SportsPeak.domain.input.user.InputRegisterNewUser;
 
 public interface AuthService {
 
     UserEntity login(InputCredentials credentials);
 
-    UserEntity register(UserEntity user);
+    UserEntity register(InputRegisterNewUser inputRegisterNewUser);
 
 }
