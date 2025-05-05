e5ab69f1ff7e70821413f057db77c96b57dd0409
CptFranck <francois.caillet71@gmail.com>
2025-05-05 19:44:32 +0200

    Modify ExerciseTypeServiceImpl

--- src/main/java/com/CptFranck/SportsPeak/service/impl/ExerciseTypeServiceImpl.java
+++ src/main/java/com/CptFranck/SportsPeak/service/impl/ExerciseTypeServiceImpl.java
@@ -1,18 +1,11 @@
 package com.CptFranck.SportsPeak.service.impl;
 
-import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
 import com.CptFranck.SportsPeak.domain.exception.exercise.ExerciseTypeNotFoundException;
-import com.CptFranck.SportsPeak.domain.input.exerciseType.InputExerciseType;
-import com.CptFranck.SportsPeak.domain.input.exerciseType.InputNewExerciseType;
 import com.CptFranck.SportsPeak.repositories.ExerciseTypeRepository;
-import com.CptFranck.SportsPeak.resolvers.ExerciseTypeInputResolver;
-import com.CptFranck.SportsPeak.service.ExerciseService;
 import com.CptFranck.SportsPeak.service.ExerciseTypeService;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -21,48 +14,41 @@ import java.util.stream.StreamSupport;
 @Service
 public class ExerciseTypeServiceImpl implements ExerciseTypeService {
 
-    private final ExerciseService exerciseService;
+//    private final ExerciseService exerciseService;
 
     private final ExerciseTypeRepository exerciseTypeRepository;
 
-    private final ExerciseTypeInputResolver exerciseTypeInputResolver;
 
-    public ExerciseTypeServiceImpl(ExerciseService exerciseService, ExerciseTypeRepository exerciseTypeRepository, ExerciseTypeInputResolver exerciseTypeInputResolver) {
-        this.exerciseService = exerciseService;
+    public ExerciseTypeServiceImpl(
+//            ExerciseService exerciseService,
+            ExerciseTypeRepository exerciseTypeRepository) {
+//        this.exerciseService = exerciseService;
         this.exerciseTypeRepository = exerciseTypeRepository;
-        this.exerciseTypeInputResolver = exerciseTypeInputResolver;
     }
 
     @Override
-    public ExerciseTypeEntity create(InputNewExerciseType inputNewExercise) {
-        ExerciseTypeEntity exerciseType = exerciseTypeInputResolver.resolveInput(inputNewExercise);
-        ExerciseTypeEntity exerciseTypeSaved = exerciseTypeRepository.save(exerciseType);
+    public ExerciseTypeEntity create(ExerciseTypeEntity newExerciseType) {
+        ExerciseTypeEntity exerciseTypeSaved = exerciseTypeRepository.save(newExerciseType);
 
-        exerciseService.updateExerciseTypeRelation(
-                new HashSet<>(inputNewExercise.getExerciseIds()),
-                Collections.emptySet(),
-                exerciseTypeSaved);
+//        exerciseService.updateExerciseTypeRelation(
+//                newExerciseType.getExercises().stream().map(ExerciseEntity::getId).collect(Collectors.toSet()),
+//                Collections.emptySet(),
+//                exerciseTypeSaved);
 
         return exerciseTypeSaved;
     }
 
     @Override
-    public ExerciseTypeEntity update(InputExerciseType inputExercise) {
-        ExerciseTypeEntity oldExerciseType = this.findOne(inputExercise.getId());
-        ExerciseTypeEntity updatedExerciseType = exerciseTypeInputResolver.resolveInput(inputExercise, oldExerciseType);
+    public ExerciseTypeEntity update(ExerciseTypeEntity updatedExerciseType) {
+        ExerciseTypeEntity oldExerciseType = this.findOne(updatedExerciseType.getId());
         ExerciseTypeEntity exerciseTypeSaved = exerciseTypeRepository.save(updatedExerciseType);
 
-        exerciseService.updateExerciseTypeRelation(
-                new HashSet<>(inputExercise.getExerciseIds()),
-                oldExerciseType.getExercises().stream().map(ExerciseEntity::getId).collect(Collectors.toSet()),
-                exerciseTypeSaved);
+//        exerciseService.updateExerciseTypeRelation(
+//                updatedExerciseType.getExercises().stream().map(ExerciseEntity::getId).collect(Collectors.toSet()),
+//                oldExerciseType.getExercises().stream().map(ExerciseEntity::getId).collect(Collectors.toSet()),
+//                exerciseTypeSaved);
 
         return exerciseTypeSaved;
-    }
-
-    @Override
-    public ExerciseTypeEntity save(ExerciseTypeEntity exerciseTypeEntity) {
-        return exerciseTypeRepository.save(exerciseTypeEntity);
     }
 
     @Override
