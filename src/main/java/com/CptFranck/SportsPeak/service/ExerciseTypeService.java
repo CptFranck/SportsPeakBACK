c1379e52a0780f7d3a20727a344945de12e4fe6a
CptFranck <francois.caillet71@gmail.com>
2025-05-05 17:10:32 +0200

    update ExerciseTypeService

--- src/main/java/com/CptFranck/SportsPeak/service/ExerciseTypeService.java
+++ src/main/java/com/CptFranck/SportsPeak/service/ExerciseTypeService.java
@@ -1,22 +1,27 @@
 package com.CptFranck.SportsPeak.service;
 
 import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
+import com.CptFranck.SportsPeak.domain.input.exerciseType.InputExerciseType;
+import com.CptFranck.SportsPeak.domain.input.exerciseType.InputNewExerciseType;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface ExerciseTypeService {
 
     List<ExerciseTypeEntity> findAll();
 
-    Optional<ExerciseTypeEntity> findOne(Long id);
+    ExerciseTypeEntity findOne(Long id);
 
     Set<ExerciseTypeEntity> findMany(Set<Long> ids);
 
-    boolean exists(Long id);
+    ExerciseTypeEntity create(InputNewExerciseType inputNewExercise);
 
-    ExerciseTypeEntity save(ExerciseTypeEntity exerciseTypeEntity);
+    ExerciseTypeEntity update(InputExerciseType inputExercise);
+
+    ExerciseTypeEntity save(ExerciseTypeEntity exercise);
 
     void delete(Long id);
+
+    boolean exists(Long id);
 }
