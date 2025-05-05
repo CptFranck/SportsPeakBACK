4053f6c4ac3ca1455522aed1564c5d6fd915d677
CptFranck <francois.caillet71@gmail.com>
2025-05-05 19:46:51 +0200

    Add ExerciseTypeInputResolverTest

---
+++ src/test/java/com/CptFranck/SportsPeak/unit/resolvers/ExerciseTypeInputResolverTest.java
@@ -0,0 +1,57 @@
+package com.CptFranck.SportsPeak.unit.resolvers;
+
+import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
+import com.CptFranck.SportsPeak.domain.input.exerciseType.InputExerciseType;
+import com.CptFranck.SportsPeak.domain.input.exerciseType.InputNewExerciseType;
+import com.CptFranck.SportsPeak.resolvers.ExerciseTypeInputResolver;
+import com.CptFranck.SportsPeak.service.ExerciseService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+
+import static com.CptFranck.SportsPeak.utils.TestExerciseTypeUtils.createTestInputExerciseType;
+import static com.CptFranck.SportsPeak.utils.TestExerciseTypeUtils.createTestInputNewExerciseType;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ExerciseTypeInputResolverTest {
+
+    @Mock
+    private ExerciseService exerciseService;
+
+    @InjectMocks
+    private ExerciseTypeInputResolver exerciseTypeInputResolver;
+
+    @Test
+    void resolveInput_ValidInputNewExercise_ReturnExerciseEntity() {
+        InputNewExerciseType inputNewExercise = createTestInputNewExerciseType();
+
+        when(exerciseService.findMany(Mockito.anySet())).thenReturn(new HashSet<>());
+
+        ExerciseTypeEntity exerciseTypeSaved = exerciseTypeInputResolver.resolveInput(inputNewExercise);
+
+        Assertions.assertEquals(inputNewExercise.getName(), exerciseTypeSaved.getName());
+        Assertions.assertEquals(inputNewExercise.getGoal(), exerciseTypeSaved.getGoal());
+        Assertions.assertEquals(inputNewExercise.getExerciseIds().size(), exerciseTypeSaved.getExercises().size());
+    }
+
+    @Test
+    void resolveInput_ValidInputExercise_ReturnExerciseEntity() {
+        InputExerciseType inputExerciseType = createTestInputExerciseType(1L);
+
+        when(exerciseService.findMany(Mockito.anySet())).thenReturn(new HashSet<>());
+
+        ExerciseTypeEntity exerciseTypeSaved = exerciseTypeInputResolver.resolveInput(inputExerciseType);
+
+        Assertions.assertEquals(inputExerciseType.getId(), exerciseTypeSaved.getId());
+        Assertions.assertEquals(inputExerciseType.getName(), exerciseTypeSaved.getName());
+        Assertions.assertEquals(inputExerciseType.getGoal(), exerciseTypeSaved.getGoal());
+        Assertions.assertEquals(inputExerciseType.getExerciseIds().size(), exerciseTypeSaved.getExercises().size());
+    }
+}
