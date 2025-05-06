b74e0ba81d81e9ce9acedcfc8139f70a8ee02568
CptFranck <francois.caillet71@gmail.com>
2025-05-06 14:47:27 +0200

    Update ExerciseInputResolver

--- src/main/java/com/CptFranck/SportsPeak/resolvers/ExerciseInputResolver.java
+++ src/main/java/com/CptFranck/SportsPeak/resolvers/ExerciseInputResolver.java
@@ -3,10 +3,13 @@ package com.CptFranck.SportsPeak.resolvers;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
 import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
+import com.CptFranck.SportsPeak.domain.entity.ProgExerciseEntity;
 import com.CptFranck.SportsPeak.domain.input.exercise.InputExercise;
 import com.CptFranck.SportsPeak.domain.input.exercise.InputNewExercise;
+import com.CptFranck.SportsPeak.service.ExerciseService;
 import com.CptFranck.SportsPeak.service.ExerciseTypeService;
 import com.CptFranck.SportsPeak.service.MuscleService;
+import com.CptFranck.SportsPeak.service.ProgExerciseService;
 import graphql.com.google.common.collect.Sets;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +18,16 @@ import java.util.Set;
 
 @Component
 public class ExerciseInputResolver {
+
     private final MuscleService muscleService;
+
+    private final ExerciseService exerciseService;
+
     private final ExerciseTypeService exerciseTypeService;
 
-    public ExerciseInputResolver(MuscleService muscleService, ExerciseTypeService exerciseTypeService) {
+    public ExerciseInputResolver(MuscleService muscleService, ExerciseTypeService exerciseTypeService, ProgExerciseService progExerciseService, ExerciseService exerciseService) {
         this.muscleService = muscleService;
+        this.exerciseService = exerciseService;
         this.exerciseTypeService = exerciseTypeService;
     }
 
@@ -30,23 +38,24 @@ public class ExerciseInputResolver {
         Set<MuscleEntity> muscles = muscleService.findMany(muscleIds);
         Set<ExerciseTypeEntity> exerciseTypes = exerciseTypeService.findMany(exerciseTypeIds);
 
+        Long id;
+        Set<ProgExerciseEntity> progExercises;
+        if (input instanceof InputExercise) {
+            id = ((InputExercise) input).getId();
+            progExercises = exerciseService.findOne(id).getProgExercises();
+        } else {
+            id = null;
+            progExercises = new HashSet<>();
+        }
+
         return new ExerciseEntity(
-                null,
+                id,
                 input.getName(),
                 input.getDescription(),
                 input.getGoal(),
                 muscles,
                 exerciseTypes,
-                new HashSet<>()
+                progExercises
         );
-    }
-
-    public ExerciseEntity resolveInput(InputExercise input, ExerciseEntity existingEntity) {
-        ExerciseEntity base = resolveInput(input);
-
-        base.setId(input.getId());
-        base.setProgExercises(existingEntity.getProgExercises());
-
-        return base;
     }
 }
