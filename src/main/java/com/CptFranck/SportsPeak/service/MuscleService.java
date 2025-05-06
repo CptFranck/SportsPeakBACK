0a4285a07c569c086f5f75226901012a16b504de
CptFranck <francois.caillet71@gmail.com>
2025-05-06 16:41:12 +0200

    Modify MuscleService

--- src/main/java/com/CptFranck/SportsPeak/service/MuscleService.java
+++ src/main/java/com/CptFranck/SportsPeak/service/MuscleService.java
@@ -4,20 +4,19 @@ package com.CptFranck.SportsPeak.service;
 import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface MuscleService {
 
     List<MuscleEntity> findAll();
 
-    Optional<MuscleEntity> findOne(Long id);
+    MuscleEntity findOne(Long id);
 
     Set<MuscleEntity> findMany(Set<Long> ids);
-
-    boolean exists(Long id);
 
     MuscleEntity save(MuscleEntity muscleEntity);
 
     void delete(Long id);
+
+    boolean exists(Long id);
 }
