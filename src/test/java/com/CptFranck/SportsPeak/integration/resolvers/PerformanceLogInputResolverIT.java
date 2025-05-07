84b686735fb616ef3f4c07690aaa3c6bb81a9342
CptFranck <francois.caillet71@gmail.com>
2025-05-07 18:23:10 +0200

    add PerformanceLogInputResolverIT

---
+++ src/test/java/com/CptFranck/SportsPeak/integration/resolvers/PerformanceLogInputResolverIT.java
@@ -0,0 +1,90 @@
+package com.CptFranck.SportsPeak.integration.resolvers;
+
+import com.CptFranck.SportsPeak.domain.entity.*;
+import com.CptFranck.SportsPeak.domain.input.performanceLog.InputNewPerformanceLog;
+import com.CptFranck.SportsPeak.domain.input.performanceLog.InputPerformanceLog;
+import com.CptFranck.SportsPeak.repositories.ExerciseRepository;
+import com.CptFranck.SportsPeak.repositories.ProgExerciseRepository;
+import com.CptFranck.SportsPeak.repositories.TargetSetRepository;
+import com.CptFranck.SportsPeak.repositories.UserRepository;
+import com.CptFranck.SportsPeak.resolvers.PerformanceLogInputResolver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static com.CptFranck.SportsPeak.utils.TestExerciseUtils.createTestExercise;
+import static com.CptFranck.SportsPeak.utils.TestPerformanceLogUtils.createTestInputNewPerformanceLog;
+import static com.CptFranck.SportsPeak.utils.TestPerformanceLogUtils.createTestInputPerformanceLog;
+import static com.CptFranck.SportsPeak.utils.TestProgExerciseUtils.createTestProgExercise;
+import static com.CptFranck.SportsPeak.utils.TestTargetSetUtils.createTestTargetSet;
+import static com.CptFranck.SportsPeak.utils.TestUserUtils.createTestUser;
+
+@SpringBootTest()
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
+public class PerformanceLogInputResolverIT {
+
+    @Autowired
+    private PerformanceLogInputResolver performanceLogInputResolver;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+
+    @Autowired
+    private TargetSetRepository targetSetRepository;
+
+    @Autowired
+    private ProgExerciseRepository progExerciseRepository;
+
+    private TargetSetEntity targetSet;
+
+    @BeforeEach
+    void setUp() {
+        UserEntity creator = userRepository.save(createTestUser(null));
+        ExerciseEntity exercise = exerciseRepository.save(createTestExercise(null));
+        ProgExerciseEntity progExercise = progExerciseRepository.save(createTestProgExercise(null, creator, exercise));
+        targetSet = targetSetRepository.save(createTestTargetSet(null, progExercise, null));
+    }
+
+    @AfterEach
+    void afterEach() {
+        targetSetRepository.deleteAll();
+        progExerciseRepository.deleteAll();
+        exerciseRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void resolveInput_ValidInputNewPerformanceLog_ReturnExerciseTypeEntity() {
+        InputNewPerformanceLog newPerformanceLog = createTestInputNewPerformanceLog(targetSet.getId(), false);
+
+        PerformanceLogEntity performanceLogSaved = performanceLogInputResolver.resolveInput(newPerformanceLog);
+
+        assertPerformanceLogInputAndEntity(newPerformanceLog, performanceLogSaved);
+    }
+
+    @Test
+    void resolveInput_ValidInputPerformanceLog_ReturnExerciseTypeEntity() {
+        InputPerformanceLog newPerformanceLog = createTestInputPerformanceLog(1L, targetSet.getId(), false);
+
+        PerformanceLogEntity performanceLogSaved = performanceLogInputResolver.resolveInput(newPerformanceLog);
+
+        Assertions.assertEquals(newPerformanceLog.getId(), performanceLogSaved.getId());
+        assertPerformanceLogInputAndEntity(newPerformanceLog, performanceLogSaved);
+    }
+
+    private void assertPerformanceLogInputAndEntity(InputNewPerformanceLog expectedPerformanceLog, PerformanceLogEntity actualPerformanceLog) {
+        Assertions.assertEquals(expectedPerformanceLog.getSetIndex(), actualPerformanceLog.getSetIndex());
+        Assertions.assertEquals(expectedPerformanceLog.getRepetitionNumber(), actualPerformanceLog.getRepetitionNumber());
+        Assertions.assertEquals(expectedPerformanceLog.getWeight(), actualPerformanceLog.getWeight());
+        Assertions.assertEquals(expectedPerformanceLog.getWeightUnit(), actualPerformanceLog.getWeightUnit().label);
+        Assertions.assertEquals(expectedPerformanceLog.getLogDate(), actualPerformanceLog.getLogDate());
+        Assertions.assertEquals(expectedPerformanceLog.getTargetSetId(), actualPerformanceLog.getTargetSet().getId());
+    }
+}
