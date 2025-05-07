3f50fd6f4b5001e52e1a812d5db8e29273cd64a7
CptFranck <francois.caillet71@gmail.com>
2025-05-07 15:29:47 +0200

    Modify PerformanceLogService

--- src/main/java/com/CptFranck/SportsPeak/service/PerformanceLogService.java
+++ src/main/java/com/CptFranck/SportsPeak/service/PerformanceLogService.java
@@ -3,22 +3,21 @@ package com.CptFranck.SportsPeak.service;
 import com.CptFranck.SportsPeak.domain.entity.PerformanceLogEntity;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface PerformanceLogService {
 
     List<PerformanceLogEntity> findAll();
 
-    Optional<PerformanceLogEntity> findOne(Long id);
+    PerformanceLogEntity findOne(Long id);
 
     Set<PerformanceLogEntity> findMany(Set<Long> ids);
 
     List<PerformanceLogEntity> findAllByTargetSetId(Long id);
 
-    boolean exists(Long id);
-
     PerformanceLogEntity save(PerformanceLogEntity targetExerciseSet);
 
     void delete(Long id);
+
+    boolean exists(Long id);
 }
