b4768dc694d63586b5a9b3047e8dab653f056f8f
CptFranck <francois.caillet71@gmail.com>
2025-05-07 15:30:22 +0200

    Update PerformanceLogController

--- src/main/java/com/CptFranck/SportsPeak/controller/PerformanceLogController.java
+++ src/main/java/com/CptFranck/SportsPeak/controller/PerformanceLogController.java
@@ -2,15 +2,11 @@ package com.CptFranck.SportsPeak.controller;
 
 import com.CptFranck.SportsPeak.domain.dto.PerformanceLogDto;
 import com.CptFranck.SportsPeak.domain.entity.PerformanceLogEntity;
-import com.CptFranck.SportsPeak.domain.entity.TargetSetEntity;
-import com.CptFranck.SportsPeak.domain.enumType.WeightUnit;
-import com.CptFranck.SportsPeak.domain.exception.performanceLog.PerformanceLogNotFoundException;
-import com.CptFranck.SportsPeak.domain.exception.tartgetSet.TargetSetNotFoundException;
 import com.CptFranck.SportsPeak.domain.input.performanceLog.InputNewPerformanceLog;
 import com.CptFranck.SportsPeak.domain.input.performanceLog.InputPerformanceLog;
 import com.CptFranck.SportsPeak.mappers.Mapper;
+import com.CptFranck.SportsPeak.resolvers.PerformanceLogInputResolver;
 import com.CptFranck.SportsPeak.service.PerformanceLogService;
-import com.CptFranck.SportsPeak.service.TargetSetService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
@@ -22,14 +18,14 @@ import java.util.List;
 @DgsComponent
 public class PerformanceLogController {
 
-    private final TargetSetService targetSetService;
     private final PerformanceLogService performanceLogService;
+    private final PerformanceLogInputResolver performanceLogInputResolver;
     private final Mapper<PerformanceLogEntity, PerformanceLogDto> performanceLogMapper;
 
-    public PerformanceLogController(TargetSetService targetSetService, PerformanceLogService performanceLogService, Mapper<PerformanceLogEntity, PerformanceLogDto> performanceLogMapper) {
-        this.targetSetService = targetSetService;
+    public PerformanceLogController(PerformanceLogService performanceLogService, PerformanceLogInputResolver performanceLogInputResolver, Mapper<PerformanceLogEntity, PerformanceLogDto> performanceLogMapper) {
         this.performanceLogMapper = performanceLogMapper;
         this.performanceLogService = performanceLogService;
+        this.performanceLogInputResolver = performanceLogInputResolver;
     }
 
     @DgsQuery
@@ -39,8 +35,7 @@ public class PerformanceLogController {
 
     @DgsQuery
     public PerformanceLogDto getPerformanceLogById(@InputArgument Long id) {
-        PerformanceLogEntity performanceLog = performanceLogService.findOne(id)
-                .orElseThrow(() -> new PerformanceLogNotFoundException(id));
+        PerformanceLogEntity performanceLog = performanceLogService.findOne(id);
         return performanceLogMapper.mapTo(performanceLog);
     }
 
@@ -54,13 +49,15 @@ public class PerformanceLogController {
     @PreAuthorize("hasRole('ROLE_USER')")
     @DgsMutation
     public PerformanceLogDto addPerformanceLog(@InputArgument InputNewPerformanceLog inputNewPerformanceLog) {
-        return performanceLogMapper.mapTo(inputToEntity(inputNewPerformanceLog));
+        PerformanceLogEntity performanceLog = performanceLogInputResolver.resolveInput(inputNewPerformanceLog);
+        return performanceLogMapper.mapTo(performanceLogService.save(performanceLog));
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @DgsMutation
     public PerformanceLogDto modifyPerformanceLog(@InputArgument InputPerformanceLog inputPerformanceLog) {
-        return performanceLogMapper.mapTo(inputToEntity(inputPerformanceLog));
+        PerformanceLogEntity performanceLog = performanceLogInputResolver.resolveInput(inputPerformanceLog);
+        return performanceLogMapper.mapTo(performanceLogService.save(performanceLog));
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
@@ -68,36 +65,5 @@ public class PerformanceLogController {
     public Long deletePerformanceLog(@InputArgument Long performanceLogId) {
         performanceLogService.delete(performanceLogId);
         return performanceLogId;
-    }
-
-    private PerformanceLogEntity inputToEntity(InputNewPerformanceLog inputNewPerformanceLog) {
-        TargetSetEntity targetSet = targetSetService.findOne(inputNewPerformanceLog.getTargetSetId()).orElseThrow(
-                () -> new TargetSetNotFoundException(inputNewPerformanceLog.getTargetSetId()));
-
-        Long id;
-        if (inputNewPerformanceLog instanceof InputPerformanceLog) {
-            id = ((InputPerformanceLog) inputNewPerformanceLog).getId();
-            if (!performanceLogService.exists(id))
-                throw new PerformanceLogNotFoundException(id);
-        } else {
-            id = null;
-        }
-
-        PerformanceLogEntity performanceLogEntity = new PerformanceLogEntity(
-                id,
-                inputNewPerformanceLog.getSetIndex(),
-                inputNewPerformanceLog.getRepetitionNumber(),
-                inputNewPerformanceLog.getWeight(),
-                WeightUnit.valueOfLabel(inputNewPerformanceLog.getWeightUnit()),
-                inputNewPerformanceLog.getLogDate(),
-                targetSet
-        );
-
-        performanceLogEntity = performanceLogService.save(performanceLogEntity);
-        if (id == null) {
-            targetSet.getPerformanceLogs().add(performanceLogEntity);
-            targetSetService.save(targetSet);
-        }
-        return performanceLogEntity;
     }
 }
