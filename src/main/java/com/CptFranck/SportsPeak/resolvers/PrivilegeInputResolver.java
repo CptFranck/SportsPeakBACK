556b82d6c005a968b58b44a0b918e223f0eece0c
CptFranck <francois.caillet71@gmail.com>
2025-05-09 11:04:09 +0200

    Add PrivilegeInputResolver

---
+++ src/main/java/com/CptFranck/SportsPeak/resolvers/PrivilegeInputResolver.java
@@ -0,0 +1,39 @@
+package com.CptFranck.SportsPeak.resolvers;
+
+import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
+import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
+import com.CptFranck.SportsPeak.domain.input.privilege.InputNewPrivilege;
+import com.CptFranck.SportsPeak.domain.input.privilege.InputPrivilege;
+import com.CptFranck.SportsPeak.service.RoleService;
+import graphql.com.google.common.collect.Sets;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class PrivilegeInputResolver {
+
+    private final RoleService roleService;
+
+    public PrivilegeInputResolver(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    public PrivilegeEntity resolveInput(InputNewPrivilege inputNewPrivilege) {
+        Set<Long> newRoleIds = Sets.newHashSet(inputNewPrivilege.getRoleIds());
+        Set<RoleEntity> roles = roleService.findMany(newRoleIds);
+
+        Long id;
+        if (inputNewPrivilege instanceof InputPrivilege) {
+            id = ((InputPrivilege) inputNewPrivilege).getId();
+        } else {
+            id = null;
+        }
+
+        return new PrivilegeEntity(
+                id,
+                inputNewPrivilege.getName(),
+                roles
+        );
+    }
+}
