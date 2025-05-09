6fd52e9eaccd776edd80ed75bd345d3ea736f4ef
CptFranck <francois.caillet71@gmail.com>
2025-05-09 11:34:29 +0200

    Update PrivilegeController

--- src/main/java/com/CptFranck/SportsPeak/controller/PrivilegeController.java
+++ src/main/java/com/CptFranck/SportsPeak/controller/PrivilegeController.java
@@ -2,35 +2,32 @@ package com.CptFranck.SportsPeak.controller;
 
 import com.CptFranck.SportsPeak.domain.dto.PrivilegeDto;
 import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
-import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
-import com.CptFranck.SportsPeak.domain.exception.privilege.PrivilegeNotFoundException;
 import com.CptFranck.SportsPeak.domain.input.privilege.InputNewPrivilege;
 import com.CptFranck.SportsPeak.domain.input.privilege.InputPrivilege;
 import com.CptFranck.SportsPeak.mappers.Mapper;
+import com.CptFranck.SportsPeak.resolvers.PrivilegeInputResolver;
 import com.CptFranck.SportsPeak.service.PrivilegeService;
-import com.CptFranck.SportsPeak.service.RoleService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
-import graphql.com.google.common.collect.Sets;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @DgsComponent
 public class PrivilegeController {
 
-    private final RoleService roleService;
     private final PrivilegeService privilegeService;
+
+    private final PrivilegeInputResolver privilegeInputResolver;
+
     private final Mapper<PrivilegeEntity, PrivilegeDto> privilegeMapper;
 
-    public PrivilegeController(RoleService exerciseService, PrivilegeService privilegeService, Mapper<PrivilegeEntity, PrivilegeDto> privilegeMapper) {
-        this.roleService = exerciseService;
-        this.privilegeService = privilegeService;
+    public PrivilegeController(PrivilegeService privilegeService, PrivilegeInputResolver privilegeInputResolver, Mapper<PrivilegeEntity, PrivilegeDto> privilegeMapper) {
         this.privilegeMapper = privilegeMapper;
+        this.privilegeService = privilegeService;
+        this.privilegeInputResolver = privilegeInputResolver;
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -42,21 +39,22 @@ public class PrivilegeController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DgsQuery
     public PrivilegeDto getPrivilegeById(@InputArgument Long id) {
-        PrivilegeEntity privilege = privilegeService.findOne(id)
-                .orElseThrow(() -> new PrivilegeNotFoundException(id));
+        PrivilegeEntity privilege = privilegeService.findOne(id);
         return privilegeMapper.mapTo(privilege);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DgsMutation
     public PrivilegeDto addPrivilege(@InputArgument InputNewPrivilege inputNewPrivilege) {
-        return privilegeMapper.mapTo(inputToEntity(inputNewPrivilege));
+        PrivilegeEntity privilege = privilegeInputResolver.resolveInput(inputNewPrivilege);
+        return privilegeMapper.mapTo(privilegeService.save(privilege));
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DgsMutation
     public PrivilegeDto modifyPrivilege(@InputArgument InputPrivilege inputPrivilege) {
-        return privilegeMapper.mapTo(inputToEntity(inputPrivilege));
+        PrivilegeEntity privilege = privilegeInputResolver.resolveInput(inputPrivilege);
+        return privilegeMapper.mapTo(privilegeService.save(privilege));
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -64,31 +62,5 @@ public class PrivilegeController {
     public Long deletePrivilege(@InputArgument Long privilegeId) {
         privilegeService.delete(privilegeId);
         return privilegeId;
-    }
-
-    private PrivilegeEntity inputToEntity(InputNewPrivilege inputNewPrivilege) {
-        Set<Long> oldRoleIds = new HashSet<>();
-        Set<Long> newRoleIds = Sets.newHashSet(inputNewPrivilege.getRoleIds());
-
-        Set<RoleEntity> roles = roleService.findMany(newRoleIds);
-
-        Long id;
-        if (inputNewPrivilege instanceof InputPrivilege) {
-            id = ((InputPrivilege) inputNewPrivilege).getId();
-            PrivilegeEntity privilege = privilegeService.findOne(id)
-                    .orElseThrow(() -> new PrivilegeNotFoundException(id));
-            privilege.getRoles().forEach(r -> oldRoleIds.add(r.getId()));
-        } else {
-            id = null;
-        }
-        PrivilegeEntity privilege = new PrivilegeEntity(
-                id,
-                inputNewPrivilege.getName(),
-                roles
-        );
-
-        privilege = privilegeService.save(privilege);
-        roleService.updatePrivilegeRelation(newRoleIds, oldRoleIds, privilege);
-        return privilege;
     }
 }
