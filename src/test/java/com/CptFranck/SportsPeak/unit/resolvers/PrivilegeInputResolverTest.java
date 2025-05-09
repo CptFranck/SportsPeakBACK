c03b154f6f8e6dd04bef78e62c9e3871b43e1710
CptFranck <francois.caillet71@gmail.com>
2025-05-09 15:23:05 +0200

    Add PrivilegeInputResolverTest

---
+++ src/test/java/com/CptFranck/SportsPeak/unit/resolvers/PrivilegeInputResolverTest.java
@@ -0,0 +1,56 @@
+package com.CptFranck.SportsPeak.unit.resolvers;
+
+import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
+import com.CptFranck.SportsPeak.domain.input.privilege.InputNewPrivilege;
+import com.CptFranck.SportsPeak.domain.input.privilege.InputPrivilege;
+import com.CptFranck.SportsPeak.resolvers.PrivilegeInputResolver;
+import com.CptFranck.SportsPeak.service.RoleService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+
+import static com.CptFranck.SportsPeak.utils.TestPrivilegeUtils.createTestInputNewPrivilege;
+import static com.CptFranck.SportsPeak.utils.TestPrivilegeUtils.createTestInputPrivilege;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PrivilegeInputResolverTest {
+
+    @InjectMocks
+    private PrivilegeInputResolver privilegeInputResolver;
+
+    @Mock
+    private RoleService roleService;
+
+    @Test
+    void resolveInput_ValidInputNewMuscle_ReturnMuscleEntity() {
+        InputNewPrivilege inputNewPrivilege = createTestInputNewPrivilege();
+        when(roleService.findMany(Mockito.anySet())).thenReturn(new HashSet<>());
+
+        PrivilegeEntity privilegeSaved = privilegeInputResolver.resolveInput(inputNewPrivilege);
+
+        assertPrivilegeInputAndEntity(inputNewPrivilege, privilegeSaved);
+    }
+
+    @Test
+    void resolveInput_ValidInputMuscle_ReturnMuscleEntity() {
+        InputPrivilege inputPrivilege = createTestInputPrivilege(1L);
+        when(roleService.findMany(Mockito.anySet())).thenReturn(new HashSet<>());
+
+        PrivilegeEntity muscleSaved = privilegeInputResolver.resolveInput(inputPrivilege);
+
+        Assertions.assertEquals(inputPrivilege.getId(), muscleSaved.getId());
+        assertPrivilegeInputAndEntity(inputPrivilege, muscleSaved);
+    }
+
+    private void assertPrivilegeInputAndEntity(InputNewPrivilege expectedPrivilege, PrivilegeEntity actualPrivilege) {
+        Assertions.assertEquals(expectedPrivilege.getName(), actualPrivilege.getName());
+        Assertions.assertEquals(expectedPrivilege.getRoleIds().size(), actualPrivilege.getRoles().size());
+    }
+}
