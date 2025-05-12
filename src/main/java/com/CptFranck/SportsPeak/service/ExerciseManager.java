840586f1e37d9b7d74411165a6e9fd325d441cc4
CptFranck <francois.caillet71@gmail.com>
2025-05-12 16:03:08 +0200

    Modify ExerciseManager

--- src/main/java/com/CptFranck/SportsPeak/service/ExerciseManager.java
+++ src/main/java/com/CptFranck/SportsPeak/service/ExerciseManager.java
@@ -1,9 +1,12 @@
 package com.CptFranck.SportsPeak.service;
 
 import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
+import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
 
 public interface ExerciseManager {
 
     ExerciseTypeEntity saveExerciseType(ExerciseTypeEntity exerciseType);
+
+    MuscleEntity saveMuscle(MuscleEntity muscleEntity);
 
 }
