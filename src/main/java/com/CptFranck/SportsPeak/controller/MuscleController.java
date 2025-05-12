eccc19aab6685b1822a307438e07c5d10c1ec004
CptFranck <francois.caillet71@gmail.com>
2025-05-12 16:05:16 +0200

    Update MuscleController

--- src/main/java/com/CptFranck/SportsPeak/controller/MuscleController.java
+++ src/main/java/com/CptFranck/SportsPeak/controller/MuscleController.java
@@ -6,6 +6,7 @@ import com.CptFranck.SportsPeak.domain.input.muscle.InputMuscle;
 import com.CptFranck.SportsPeak.domain.input.muscle.InputNewMuscle;
 import com.CptFranck.SportsPeak.mappers.Mapper;
 import com.CptFranck.SportsPeak.resolvers.MuscleInputResolver;
+import com.CptFranck.SportsPeak.service.ExerciseManager;
 import com.CptFranck.SportsPeak.service.MuscleService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
@@ -20,11 +21,14 @@ public class MuscleController {
 
     private final MuscleService muscleService;
 
+    private final ExerciseManager exerciseManager;
+
     private final MuscleInputResolver muscleInputResolver;
 
     private final Mapper<MuscleEntity, MuscleDto> muscleMapper;
 
-    public MuscleController(MuscleService muscleService, MuscleInputResolver muscleInputResolver, Mapper<MuscleEntity, MuscleDto> muscleMapper) {
+    public MuscleController(MuscleService muscleService, ExerciseManager exerciseManager, MuscleInputResolver muscleInputResolver, Mapper<MuscleEntity, MuscleDto> muscleMapper) {
+        this.exerciseManager = exerciseManager;
         this.muscleMapper = muscleMapper;
         this.muscleService = muscleService;
         this.muscleInputResolver = muscleInputResolver;
@@ -45,14 +49,14 @@ public class MuscleController {
     @DgsMutation
     public MuscleDto addMuscle(@InputArgument InputNewMuscle inputNewMuscle) {
         MuscleEntity muscle = muscleInputResolver.resolveInput(inputNewMuscle);
-        return muscleMapper.mapTo(muscleService.save(muscle));
+        return muscleMapper.mapTo(exerciseManager.saveMuscle(muscle));
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DgsMutation
     public MuscleDto modifyMuscle(@InputArgument InputMuscle inputMuscle) {
         MuscleEntity muscle = muscleInputResolver.resolveInput(inputMuscle);
-        return muscleMapper.mapTo(muscleService.save(muscle));
+        return muscleMapper.mapTo(exerciseManager.saveMuscle(muscle));
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
