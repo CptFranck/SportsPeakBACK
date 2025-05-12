15399a3bce4ae9fd73376d45c9899d7c29d68097
CptFranck <francois.caillet71@gmail.com>
2025-05-12 13:47:25 +0200

    Add ExerciseManager

---
+++ src/main/java/com/CptFranck/SportsPeak/service/ExerciseManager.java
@@ -0,0 +1,9 @@
+package com.CptFranck.SportsPeak.service;
+
+import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
+
+public interface ExerciseManager {
+
+    ExerciseTypeEntity saveExerciseType(ExerciseTypeEntity exerciseType);
+
+}
