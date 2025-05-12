dd173aedf67587192a95a2b010f8738ee07ed074
CptFranck <francois.caillet71@gmail.com>
2025-05-12 16:03:43 +0200

    Update MuscleServiceImpl

--- src/main/java/com/CptFranck/SportsPeak/service/impl/MuscleServiceImpl.java
+++ src/main/java/com/CptFranck/SportsPeak/service/impl/MuscleServiceImpl.java
@@ -1,15 +1,12 @@
 package com.CptFranck.SportsPeak.service.impl;
 
-import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
 import com.CptFranck.SportsPeak.domain.exception.muscle.MuscleNotFoundException;
 import com.CptFranck.SportsPeak.domain.exception.muscle.MuscleStillUsedInExerciseException;
 import com.CptFranck.SportsPeak.repositories.MuscleRepository;
-import com.CptFranck.SportsPeak.service.ExerciseService;
 import com.CptFranck.SportsPeak.service.MuscleService;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,12 +15,9 @@ import java.util.stream.StreamSupport;
 @Service
 public class MuscleServiceImpl implements MuscleService {
 
-    private final ExerciseService exerciseService;
-
     private final MuscleRepository muscleRepository;
 
-    public MuscleServiceImpl(ExerciseService exerciseService, MuscleRepository muscleRepository) {
-        this.exerciseService = exerciseService;
+    public MuscleServiceImpl(MuscleRepository muscleRepository) {
         this.muscleRepository = muscleRepository;
     }
 
@@ -52,20 +46,11 @@ public class MuscleServiceImpl implements MuscleService {
 
     @Override
     public MuscleEntity save(MuscleEntity muscle) {
-        Set<Long> oldExerciseIds;
         if (muscle.getId() == null)
-            oldExerciseIds = Collections.emptySet();
-        else
-            oldExerciseIds = this.findOne(muscle.getId()).getExercises()
-                    .stream().map(ExerciseEntity::getId).collect(Collectors.toSet());
-        Set<Long> newExerciseIds = muscle.getExercises()
-                .stream().map(ExerciseEntity::getId).collect(Collectors.toSet());
-
-        MuscleEntity muscleSaved = muscleRepository.save(muscle);
-
-        exerciseService.updateMuscleRelation(newExerciseIds, oldExerciseIds, muscleSaved);
-
-        return muscleSaved;
+            return muscleRepository.save(muscle);
+        if (this.exists(muscle.getId()))
+            return muscleRepository.save(muscle);
+        throw new MuscleNotFoundException(muscle.getId());
     }
 
     @Override
