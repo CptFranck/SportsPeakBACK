2fee803665d82cf4861f2deae7d730322565975e
CptFranck <francois.caillet71@gmail.com>
2025-05-12 16:03:29 +0200

    Update ExerciseManagerImpl

--- src/main/java/com/CptFranck/SportsPeak/service/impl/ExerciseManagerImpl.java
+++ src/main/java/com/CptFranck/SportsPeak/service/impl/ExerciseManagerImpl.java
@@ -2,9 +2,11 @@ package com.CptFranck.SportsPeak.service.impl;
 
 import com.CptFranck.SportsPeak.domain.entity.ExerciseEntity;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
+import com.CptFranck.SportsPeak.domain.entity.MuscleEntity;
 import com.CptFranck.SportsPeak.service.ExerciseManager;
 import com.CptFranck.SportsPeak.service.ExerciseService;
 import com.CptFranck.SportsPeak.service.ExerciseTypeService;
+import com.CptFranck.SportsPeak.service.MuscleService;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -14,11 +16,14 @@ import java.util.stream.Collectors;
 @Service
 public class ExerciseManagerImpl implements ExerciseManager {
 
+    private final MuscleService muscleService;
+
     private final ExerciseService exerciseService;
 
     private final ExerciseTypeService exerciseTypeService;
 
-    public ExerciseManagerImpl(ExerciseService exerciseService, ExerciseTypeService exerciseTypeService) {
+    public ExerciseManagerImpl(MuscleService muscleService, ExerciseService exerciseService, ExerciseTypeService exerciseTypeService) {
+        this.muscleService = muscleService;
         this.exerciseService = exerciseService;
         this.exerciseTypeService = exerciseTypeService;
     }
@@ -43,5 +48,23 @@ public class ExerciseManagerImpl implements ExerciseManager {
                 exerciseTypeSaved);
 
         return exerciseTypeSaved;
+    }
+
+    @Override
+    public MuscleEntity saveMuscle(MuscleEntity muscle) {
+        Set<Long> oldExerciseIds;
+        if (muscle.getId() == null)
+            oldExerciseIds = Collections.emptySet();
+        else
+            oldExerciseIds = muscleService.findOne(muscle.getId()).getExercises()
+                    .stream().map(ExerciseEntity::getId).collect(Collectors.toSet());
+        Set<Long> newExerciseIds = muscle.getExercises()
+                .stream().map(ExerciseEntity::getId).collect(Collectors.toSet());
+
+        MuscleEntity muscleSaved = muscleService.save(muscle);
+
+        exerciseService.updateMuscleRelation(newExerciseIds, oldExerciseIds, muscleSaved);
+
+        return muscleSaved;
     }
 }
