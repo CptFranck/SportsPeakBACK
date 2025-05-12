eb84ccc024f739b17236e8b7bd54b036b1a2fc3f
CptFranck <francois.caillet71@gmail.com>
2025-05-12 17:10:42 +0200

    add RoleManager

---
+++ src/main/java/com/CptFranck/SportsPeak/service/RoleManager.java
@@ -0,0 +1,9 @@
+package com.CptFranck.SportsPeak.service;
+
+import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
+
+public interface RoleManager {
+
+    PrivilegeEntity savePrivilege(PrivilegeEntity privilegeEntity);
+
+}
