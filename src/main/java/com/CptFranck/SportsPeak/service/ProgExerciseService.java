d03d595e53d0442955c37942838f7e459243edec
CptFranck <francois.caillet71@gmail.com>
2025-05-13 18:43:26 +0200

    Update

--- src/main/java/com/CptFranck/SportsPeak/service/ProgExerciseService.java
+++ src/main/java/com/CptFranck/SportsPeak/service/ProgExerciseService.java
@@ -3,20 +3,19 @@ package com.CptFranck.SportsPeak.service;
 import com.CptFranck.SportsPeak.domain.entity.ProgExerciseEntity;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface ProgExerciseService {
 
     List<ProgExerciseEntity> findAll();
 
-    Optional<ProgExerciseEntity> findOne(Long id);
+    ProgExerciseEntity findOne(Long id);
 
     Set<ProgExerciseEntity> findMany(Set<Long> ids);
-
-    boolean exists(Long id);
 
     ProgExerciseEntity save(ProgExerciseEntity exercise);
 
     void delete(Long id);
+
+    boolean exists(Long id);
 }
