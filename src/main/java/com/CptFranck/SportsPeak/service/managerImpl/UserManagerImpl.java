cd0436b44261e89f8988c8a01a3c1d17e1b5a483
CptFranck <francois.caillet71@gmail.com>
2025-05-14 17:00:23 +0200

    add UserManagerImpl

---
+++ src/main/java/com/CptFranck/SportsPeak/service/managerImpl/UserManagerImpl.java
@@ -0,0 +1,44 @@
+package com.CptFranck.SportsPeak.service.managerImpl;
+
+import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
+import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import com.CptFranck.SportsPeak.service.RoleService;
+import com.CptFranck.SportsPeak.service.UserManager;
+import com.CptFranck.SportsPeak.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserManagerImpl implements UserManager {
+
+    private final UserService userService;
+
+    private final RoleService roleService;
+
+    public UserManagerImpl(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @Override
+    public RoleEntity saveRole(RoleEntity role) {
+        Set<Long> oldUserIds;
+        if (role.getId() == null)
+            oldUserIds = Collections.emptySet();
+        else
+            oldUserIds = roleService.findOne(role.getId()).getUsers()
+                    .stream().map(UserEntity::getId).collect(Collectors.toSet());
+
+        Set<Long> newUserIds = role.getUsers()
+                .stream().map(UserEntity::getId).collect(Collectors.toSet());
+
+        RoleEntity roleSaved = roleService.save(role);
+
+        userService.updateRoleRelation(newUserIds, oldUserIds, role);
+
+        return roleSaved;
+    }
+}
