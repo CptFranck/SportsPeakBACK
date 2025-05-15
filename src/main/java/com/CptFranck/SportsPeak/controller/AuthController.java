2250417ef1a208be23fc080528a5d8f96ed73b50
CptFranck <francois.caillet71@gmail.com>
2025-05-15 17:08:23 +0200

    Update AuthController Interface

--- src/main/java/com/CptFranck/SportsPeak/controller/AuthController.java
+++ src/main/java/com/CptFranck/SportsPeak/controller/AuthController.java
@@ -1,54 +1,38 @@
 package com.CptFranck.SportsPeak.controller;
 
-import com.CptFranck.SportsPeak.config.security.JwtProvider;
 import com.CptFranck.SportsPeak.domain.dto.AuthDto;
 import com.CptFranck.SportsPeak.domain.dto.UserDto;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
 import com.CptFranck.SportsPeak.domain.input.credentials.InputCredentials;
 import com.CptFranck.SportsPeak.domain.input.user.InputRegisterNewUser;
+import com.CptFranck.SportsPeak.domain.model.UserToken;
 import com.CptFranck.SportsPeak.mappers.Mapper;
 import com.CptFranck.SportsPeak.service.AuthService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.InputArgument;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @DgsComponent
 public class AuthController {
 
     private final AuthService authService;
-    private final JwtProvider userAuthProvider;
-    private final Mapper<UserEntity, UserDto> userMapper;
-    private final AuthenticationManager authenticationManager;
 
-    public AuthController(AuthenticationManager authenticationManager,
-                          AuthService authService,
-                          JwtProvider userAuthProvider,
-                          Mapper<UserEntity, UserDto> userMapper) {
+    private final Mapper<UserEntity, UserDto> userMapper;
+
+    public AuthController(AuthService authService, Mapper<UserEntity, UserDto> userMapper) {
         this.userMapper = userMapper;
         this.authService = authService;
-        this.userAuthProvider = userAuthProvider;
-        this.authenticationManager = authenticationManager;
     }
 
     @DgsMutation
     public AuthDto login(@InputArgument InputCredentials inputCredentials) {
-        UserDto user = userMapper.mapTo(authService.login(inputCredentials));
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(inputCredentials.getEmail(), inputCredentials.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new AuthDto(userAuthProvider.generateToken(authentication), user);
+        UserToken userToken = authService.login(inputCredentials);
+        return new AuthDto(userToken.getToken(), userMapper.mapTo(userToken.getUser()));
     }
 
     @DgsMutation
     public AuthDto register(@InputArgument InputRegisterNewUser inputRegisterNewUser) {
-        UserDto user = userMapper.mapTo(authService.register(inputRegisterNewUser));
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(inputRegisterNewUser.getEmail(), inputRegisterNewUser.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new AuthDto(userAuthProvider.generateToken(authentication), user);
+        UserToken userToken = authService.register(inputRegisterNewUser);
+        return new AuthDto(userToken.getToken(), userMapper.mapTo(userToken.getUser()));
     }
 }
