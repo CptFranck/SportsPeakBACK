eb28705a27a739142c7ef88ed7423abe06fa0b18
CptFranck <francois.caillet71@gmail.com>
2025-05-15 16:09:50 +0200

    update EmailUnknownException

--- src/main/java/com/CptFranck/SportsPeak/domain/exception/userAuth/EmailUnknownException.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/exception/userAuth/EmailUnknownException.java
@@ -1,7 +1,7 @@
 package com.CptFranck.SportsPeak.domain.exception.userAuth;
 
 public class EmailUnknownException extends RuntimeException {
-    public EmailUnknownException() {
-        super("Email unknown");
+    public EmailUnknownException(String email) {
+        super(String.format("Email %s unknown", email));
     }
 }
