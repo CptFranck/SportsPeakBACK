58ddddc28bc2da3da0d08bb6f18ec4c3e81554f1
CptFranck <francois.caillet71@gmail.com>
2025-05-15 13:07:08 +0200

    Update InputNewRole for test

--- src/main/java/com/CptFranck/SportsPeak/domain/input/role/InputNewRole.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/role/InputNewRole.java
@@ -3,9 +3,11 @@ package com.CptFranck.SportsPeak.domain.input.role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
