76baf89f0cb791c1c00df979c6f1f26aaaf41148
CptFranck <francois.caillet71@gmail.com>
2025-05-15 17:01:20 +0200

    Add UserToken class

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/model/UserToken.java
@@ -0,0 +1,12 @@
+package com.CptFranck.SportsPeak.domain.model;
+
+import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserToken {
+    JWToken token;
+    UserEntity user;
+}
