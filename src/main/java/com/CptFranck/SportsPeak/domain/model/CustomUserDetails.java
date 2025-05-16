ec33c065d7bb44f0d8930687d2bfabf52d381fb2
CptFranck <francois.caillet71@gmail.com>
2025-05-16 16:01:30 +0200

    Add CustomUserDetails class

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/model/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package com.CptFranck.SportsPeak.domain.model;
+
+import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
+import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
+import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public record CustomUserDetails(UserEntity user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (RoleEntity role : user.getRoles())
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+
+        for (RoleEntity role : user.getRoles())
+            for (PrivilegeEntity privilege : role.getPrivileges())
+                authorities.add(new SimpleGrantedAuthority(privilege.getName()));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
