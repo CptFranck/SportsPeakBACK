0473aabe0521e721ad7372f0bb1b48ed46b28908
CptFranck <francois.caillet71@gmail.com>
2025-05-16 20:07:58 +0200

    update JwtAuthenticationFilter

--- src/main/java/com/CptFranck/SportsPeak/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/CptFranck/SportsPeak/config/security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.CptFranck.SportsPeak.config.security;
 
-import com.CptFranck.SportsPeak.service.impl.UserServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,9 +8,9 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -20,11 +19,12 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
-    private final UserServiceImpl userServiceImpl;
 
-    public JwtAuthenticationFilter(JwtProvider jwtProvider, UserServiceImpl userServiceImpl) {
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtProvider jwtProvider, UserDetailsService userDetailsService) {
         this.jwtProvider = jwtProvider;
-        this.userServiceImpl = userServiceImpl;
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -33,23 +33,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NotNull HttpServletResponse response,
             @NotNull FilterChain filterChain
     ) throws ServletException, IOException {
-        String token = getJWTFromRequest(request);
-        if (StringUtils.hasText(token) && this.jwtProvider.validateToken(token)) {
-            String login = this.jwtProvider.getUsernameFromJWT(token);
-            UserDetails userDetails = userServiceImpl.loadUserByUsername(login);
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        final String bearerToken = request.getHeader("Authorization");
+
+        String jwt = null;
+        String username = null;
+
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            jwt = bearerToken.substring(7);
+            username = jwtProvider.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if (jwtProvider.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
         }
         filterChain.doFilter(request, response);
-    }
-
-    private String getJWTFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
     }
 }
