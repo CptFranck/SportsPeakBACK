1059c9fb799066327291735f74d6e5d895052bcb
CptFranck <francois.caillet71@gmail.com>
2025-05-16 16:46:42 +0200

    update CustomUserDetailsService

--- src/main/java/com/CptFranck/SportsPeak/service/serviceImpl/CustomUserDetailsService.java
+++ src/main/java/com/CptFranck/SportsPeak/service/serviceImpl/CustomUserDetailsService.java
@@ -2,7 +2,7 @@ package com.CptFranck.SportsPeak.service.serviceImpl;
 
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
 import com.CptFranck.SportsPeak.domain.model.CustomUserDetails;
-import com.CptFranck.SportsPeak.repositories.UserRepository;
+import com.CptFranck.SportsPeak.service.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,18 +11,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String identifier) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByEmail(identifier)
-                .or(() -> userRepository.findByUsername(identifier))
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        UserEntity user = userService.findByLogin(login);
         return new CustomUserDetails(user);
     }
 }
