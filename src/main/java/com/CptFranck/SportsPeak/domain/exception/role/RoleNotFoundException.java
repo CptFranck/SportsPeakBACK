8ff5986b60bb3a869d9b53c2957f316862884066
CptFranck <francois.caillet71@gmail.com>
2025-05-19 18:03:06 +0200

    update RoleNotFoundException

--- src/main/java/com/CptFranck/SportsPeak/domain/exception/role/RoleNotFoundException.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/exception/role/RoleNotFoundException.java
@@ -2,6 +2,6 @@ package com.CptFranck.SportsPeak.domain.exception.role;
 
 public class RoleNotFoundException extends RuntimeException {
     public RoleNotFoundException(String idOrRole) {
-        super(String.format("The role the id or the name %s has not been found", idOrRole));
+        super(String.format("The role id or name %s has not been found", idOrRole));
     }
 }
