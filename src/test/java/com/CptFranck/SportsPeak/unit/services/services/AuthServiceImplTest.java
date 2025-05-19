c499e9a85d4c323dea4e47a21f5386b071fadf42
CptFranck <francois.caillet71@gmail.com>
2025-05-19 15:04:14 +0200

    update AuthServiceImplTest

--- src/test/java/com/CptFranck/SportsPeak/unit/services/services/AuthServiceImplTest.java
+++ src/test/java/com/CptFranck/SportsPeak/unit/services/services/AuthServiceImplTest.java
@@ -1,16 +1,17 @@
 package com.CptFranck.SportsPeak.unit.services.services;
 
+import com.CptFranck.SportsPeak.config.security.JwtUtils;
 import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
-import com.CptFranck.SportsPeak.domain.exception.role.RoleNotFoundException;
-import com.CptFranck.SportsPeak.domain.exception.userAuth.EmailAlreadyUsedException;
-import com.CptFranck.SportsPeak.domain.exception.userAuth.EmailUnknownException;
 import com.CptFranck.SportsPeak.domain.exception.userAuth.IncorrectPasswordException;
-import com.CptFranck.SportsPeak.domain.exception.userAuth.UsernameExistsException;
 import com.CptFranck.SportsPeak.domain.input.credentials.InputCredentials;
 import com.CptFranck.SportsPeak.domain.input.user.InputRegisterNewUser;
-import com.CptFranck.SportsPeak.repositories.UserRepository;
+import com.CptFranck.SportsPeak.domain.input.user.InputUserEmail;
+import com.CptFranck.SportsPeak.domain.input.user.InputUserPassword;
+import com.CptFranck.SportsPeak.domain.model.CustomUserDetails;
+import com.CptFranck.SportsPeak.domain.model.UserToken;
 import com.CptFranck.SportsPeak.service.RoleService;
+import com.CptFranck.SportsPeak.service.UserService;
 import com.CptFranck.SportsPeak.service.serviceImpl.AuthServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,104 +21,148 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Optional;
 
 import static com.CptFranck.SportsPeak.utils.AuthUtils.createInputCredentials;
 import static com.CptFranck.SportsPeak.utils.AuthUtils.createInputRegisterNewUser;
 import static com.CptFranck.SportsPeak.utils.TestRoleUtils.createTestRole;
-import static com.CptFranck.SportsPeak.utils.TestUserUtils.createTestUser;
+import static com.CptFranck.SportsPeak.utils.TestUserUtils.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceImplTest {
 
-    @Mock
-    private RoleService roleService;
+    @InjectMocks
+    private AuthServiceImpl authServiceImpl;
 
     @Mock
-    private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private UserDetailsService userDetailsService;
 
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    @InjectMocks
-    private AuthServiceImpl authServiceImpl;
+    @Mock
+    private UserService userService;
 
+    @Mock
+    private RoleService roleService;
+
+    @Mock
+    private JwtUtils jwtUtils;
+
+    private String token;
     private RoleEntity role;
     private UserEntity user;
-    private InputCredentials inputCredentials;
-    private InputRegisterNewUser inputRegisterNewUser;
+    private UserToken userToken;
+    private UserDetails userDetails;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
+        token = "token";
         role = createTestRole(1L, 0);
         user = createTestUser(1L);
-        inputCredentials = createInputCredentials(user);
-        inputRegisterNewUser = createInputRegisterNewUser(user);
+        userToken = new UserToken(token, user);
+        userDetails = new CustomUserDetails(user);
     }
 
     @Test
-    void login_EmailNotFound_ThrowEmailUnknownException() {
-        when(userRepository.findByEmail(Mockito.any(String.class))).thenReturn(Optional.empty());
+    void login_authentificationFailed_ThrowRuntimeException() {
+        when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class)))
+                .thenThrow(new BadCredentialsException("Bad credentials"));
+        ;
 
-        assertThrows(EmailUnknownException.class, () -> authServiceImpl.login(inputCredentials));
+        InputCredentials inputCredentials = createInputCredentials(user);
+        assertThrows(RuntimeException.class, () -> authServiceImpl.login(inputCredentials));
     }
 
     @Test
-    void login_IncorrectPassword_ThrowIncorrectPasswordException() {
-        when(userRepository.findByEmail(Mockito.any(String.class))).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(Mockito.any(String.class), Mockito.any(String.class))).thenReturn(false);
+    void login_CorrectCredentials_ReturnUserTokenEntity() {
+        when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(mock(Authentication.class));
+        when(userDetailsService.loadUserByUsername(Mockito.any(String.class))).thenReturn(userDetails);
+        when(jwtUtils.generateToken(Mockito.any(UserDetails.class))).thenReturn(token);
+        when(userService.findByLogin(Mockito.any(String.class))).thenReturn(user);
 
-        assertThrows(IncorrectPasswordException.class, () -> authServiceImpl.login(inputCredentials));
-    }
+        InputCredentials inputCredentials = createInputCredentials(user);
+        UserToken returnedUser = authServiceImpl.login(inputCredentials);
 
-    @Test
-    void login_CorrectCredentials_ReturnUserEntity() {
-        when(userRepository.findByEmail(Mockito.any(String.class))).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(Mockito.any(String.class), Mockito.any(String.class))).thenReturn(true);
-
-        UserEntity returnedUser = authServiceImpl.login(inputCredentials);
-
-        Assertions.assertEquals(user, returnedUser);
-    }
-
-    @Test
-    void register_MissingUserRoleInDB_ThrowRoleNotFoundException() {
-        when(roleService.findByName(Mockito.any(String.class))).thenReturn(Optional.empty());
-        when(userRepository.findByEmail(Mockito.any(String.class))).thenReturn(Optional.of(user));
-
-        Assertions.assertThrows(RoleNotFoundException.class, () -> authServiceImpl.register(inputRegisterNewUser));
-    }
-
-    @Test
-    void register_UserEmailAlreadyTaken_ThrowEmailAlreadyUsedException() {
-        when(roleService.findByName(Mockito.any(String.class))).thenReturn(Optional.of(role));
-        when(userRepository.findByEmail(Mockito.any(String.class))).thenReturn(Optional.of(user));
-
-        Assertions.assertThrows(EmailAlreadyUsedException.class, () -> authServiceImpl.register(inputRegisterNewUser));
-    }
-
-    @Test
-    void register_UserUsernameAlreadyUsed_ThrowUsernameExistsException() {
-        when(roleService.findByName(Mockito.any(String.class))).thenReturn(Optional.of(role));
-        when(userRepository.findByEmail(Mockito.any(String.class))).thenReturn(Optional.empty());
-        when(userRepository.findByUsername(Mockito.any(String.class))).thenReturn(Optional.of(user));
-
-        Assertions.assertThrows(UsernameExistsException.class, () -> authServiceImpl.register(inputRegisterNewUser));
+        Assertions.assertEquals(userToken.getToken(), returnedUser.getToken());
+        Assertions.assertEquals(userToken.getUser(), returnedUser.getUser());
     }
 
     @Test
     void register_CorrectCredentials_ReturnUserEntity() {
-        when(roleService.findByName(Mockito.any(String.class))).thenReturn(Optional.of(role));
-        when(userRepository.findByEmail(Mockito.any(String.class))).thenReturn(Optional.empty());
-        when(userRepository.findByUsername(Mockito.any(String.class))).thenReturn(Optional.empty());
-        when(userRepository.save(Mockito.any(UserEntity.class))).thenReturn(user);
+        when(roleService.findByName(Mockito.any(String.class))).thenReturn(role);
+        when(passwordEncoder.encode(Mockito.any(String.class))).thenReturn(user.getPassword());
+        when(userService.save(Mockito.any(UserEntity.class))).thenReturn(user);
+        when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(mock(Authentication.class));
+        when(userDetailsService.loadUserByUsername(Mockito.any(String.class))).thenReturn(userDetails);
+        when(jwtUtils.generateToken(Mockito.any(UserDetails.class))).thenReturn(token);
 
-        UserEntity registeredUser = authServiceImpl.register(inputRegisterNewUser);
+        InputRegisterNewUser inputRegisterNewUser = createInputRegisterNewUser(user);
+        UserToken returnedUser = authServiceImpl.register(inputRegisterNewUser);
 
-        Assertions.assertEquals(user, registeredUser);
+        Assertions.assertEquals(userToken.getToken(), returnedUser.getToken());
+        Assertions.assertEquals(userToken.getUser(), returnedUser.getUser());
+    }
+
+    @Test
+    void updateEmail_InvalidPassword_ThrowsIncorrectPasswordException() {
+        when(userService.findOne(Mockito.any(Long.class))).thenReturn(user);
+        when(passwordEncoder.matches(Mockito.any(CharSequence.class), Mockito.any(String.class))).thenThrow(IncorrectPasswordException.class);
+
+        InputUserEmail inputUserEmail = createTestInputUserEmail(user.getId(), user.getPassword());
+        assertThrows(RuntimeException.class, () -> authServiceImpl.updateEmail(inputUserEmail));
+    }
+
+    @Test
+    void updateEmail_ValidInputs_ReturnUserEntity() {
+        when(userService.findOne(Mockito.any(Long.class))).thenReturn(user);
+        when(passwordEncoder.matches(Mockito.any(CharSequence.class), Mockito.any(String.class))).thenReturn(true);
+        when(userService.save(Mockito.any(UserEntity.class))).thenReturn(user);
+        when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(mock(Authentication.class));
+        when(userDetailsService.loadUserByUsername(Mockito.any(String.class))).thenReturn(userDetails);
+        when(jwtUtils.generateToken(Mockito.any(UserDetails.class))).thenReturn(token);
+
+        InputUserEmail inputUserEmail = createTestInputUserEmail(user.getId(), user.getPassword());
+        UserToken returnedUser = authServiceImpl.updateEmail(inputUserEmail);
+
+        Assertions.assertEquals(userToken.getToken(), returnedUser.getToken());
+        Assertions.assertEquals(userToken.getUser(), returnedUser.getUser());
+    }
+
+    @Test
+    void updatePassword_InvalidPassword_ThrowsIncorrectPasswordException() {
+        when(userService.findOne(Mockito.any(Long.class))).thenReturn(user);
+        when(passwordEncoder.matches(Mockito.any(CharSequence.class), Mockito.any(String.class))).thenReturn(false);
+
+        InputUserPassword inputUserPassword = createTestInputUserPassword(user.getId(), user.getPassword());
+        assertThrows(IncorrectPasswordException.class, () -> authServiceImpl.updatePassword(inputUserPassword));
+    }
+
+    @Test
+    void updatePassword_ValidInputs_ReturnUserEntity() {
+        when(userService.findOne(Mockito.any(Long.class))).thenReturn(user);
+        when(passwordEncoder.matches(Mockito.any(CharSequence.class), Mockito.any(String.class))).thenReturn(true);
+        when(userService.save(Mockito.any(UserEntity.class))).thenReturn(user);
+        when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(mock(Authentication.class));
+        when(userDetailsService.loadUserByUsername(Mockito.any(String.class))).thenReturn(userDetails);
+        when(jwtUtils.generateToken(Mockito.any(UserDetails.class))).thenReturn(token);
+
+        InputUserPassword inputUserPassword = createTestInputUserPassword(user.getId(), user.getPassword());
+        UserToken returnedUser = authServiceImpl.updatePassword(inputUserPassword);
+
+        Assertions.assertEquals(userToken.getToken(), returnedUser.getToken());
+        Assertions.assertEquals(userToken.getUser(), returnedUser.getUser());
     }
 }
