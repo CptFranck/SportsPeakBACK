b281fd8322442cb7a0be61403c0aea24627bf9b2
CptFranck <francois.caillet71@gmail.com>
2025-05-19 12:19:18 +0200

    update AuthDto

--- src/main/java/com/CptFranck/SportsPeak/domain/dto/AuthDto.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/dto/AuthDto.java
@@ -1,20 +1,15 @@
 package com.CptFranck.SportsPeak.domain.dto;
 
-import com.CptFranck.SportsPeak.domain.model.JWToken;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class AuthDto {
-    private final LocalDateTime expiration;
-    private final String accessToken;
+    private final String token;
     private final String tokenType = "Bearer";
     private final UserDto user;
 
-    public AuthDto(JWToken jwToken, UserDto user) {
+    public AuthDto(String token, UserDto user) {
+        this.token = token;
         this.user = user;
-        this.accessToken = jwToken.getToken();
-        this.expiration = jwToken.getExpiration();
     }
 }
