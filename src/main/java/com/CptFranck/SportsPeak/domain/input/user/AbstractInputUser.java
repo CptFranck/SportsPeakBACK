bd3f2cd73e122b80a7ad24c4562f94b303527295
CptFranck <francois.caillet71@gmail.com>
2025-05-20 11:02:16 +0200

    add AbstractInputUser

---
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/user/AbstractInputUser.java
@@ -0,0 +1,12 @@
+package com.CptFranck.SportsPeak.domain.input.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class AbstractInputUser {
+    private Long id;
+}
