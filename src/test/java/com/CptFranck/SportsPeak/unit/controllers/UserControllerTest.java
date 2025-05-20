fe5a7076e53385bf00291de65a81e30057eae64c
CptFranck <francois.caillet71@gmail.com>
2025-05-20 13:03:29 +0200

    Update UserControllerTest

--- src/test/java/com/CptFranck/SportsPeak/unit/controllers/UserControllerTest.java
+++ src/test/java/com/CptFranck/SportsPeak/unit/controllers/UserControllerTest.java
@@ -1,16 +1,16 @@
 package com.CptFranck.SportsPeak.unit.controllers;
 
-import com.CptFranck.SportsPeak.config.security.JwtProvider;
 import com.CptFranck.SportsPeak.controller.UserController;
 import com.CptFranck.SportsPeak.domain.dto.AuthDto;
 import com.CptFranck.SportsPeak.domain.dto.ProgExerciseDto;
 import com.CptFranck.SportsPeak.domain.dto.UserDto;
-import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
-import com.CptFranck.SportsPeak.domain.exception.userAuth.UserNotFoundException;
-import com.CptFranck.SportsPeak.domain.model.JWToken;
+import com.CptFranck.SportsPeak.domain.input.user.*;
+import com.CptFranck.SportsPeak.domain.model.UserToken;
 import com.CptFranck.SportsPeak.mappers.Mapper;
-import com.CptFranck.SportsPeak.service.RoleService;
+import com.CptFranck.SportsPeak.resolvers.UserInputResolver;
+import com.CptFranck.SportsPeak.service.AuthService;
+import com.CptFranck.SportsPeak.service.UserManager;
 import com.CptFranck.SportsPeak.service.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,15 +20,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 import static com.CptFranck.SportsPeak.utils.TestUserUtils.*;
 import static org.mockito.Mockito.when;
@@ -44,65 +38,49 @@ class UserControllerTest {
     private Mapper<UserEntity, UserDto> userMapper;
 
     @Mock
+    private UserInputResolver userInputResolver;
+
+    @Mock
+    private UserManager userManager;
+
+    @Mock
     private UserService userService;
 
     @Mock
-    private RoleService roleService;
-
-    @Mock
-    private JwtProvider userAuthProvider;
-
-    @Mock
-    private AuthenticationManager authenticationManager;
+    private AuthService authService;
 
     private UserEntity user;
-    private UserDto userDtoMapped;
+    private UserDto userDto;
 
     @BeforeEach
     void init() {
         user = createTestUser(1L);
-        userDtoMapped = createTestUserDto(1L);
+        userDto = createTestUserDto(1L);
     }
 
     @Test
-    void UserController_GetUsers_Success() {
+    void getUsers_ValidUse_ReturnListOfUserDto() {
         when(userService.findAll()).thenReturn(List.of(user));
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDtoMapped);
+        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
 
         List<UserDto> userDtos = userController.getUsers();
 
-        Assertions.assertEquals(List.of(this.userDtoMapped), userDtos);
+        Assertions.assertEquals(List.of(this.userDto), userDtos);
     }
 
     @Test
-    void UserController_GetUserById_Unsuccessful() {
-        when(userService.findOne(Mockito.any(Long.class))).thenReturn(Optional.empty());
-
-        Assertions.assertThrows(UserNotFoundException.class,
-                () -> userController.getUserById(1L)
-        );
-    }
-
-    @Test
-    void UserController_GetUserById_Success() {
-        when(userService.findOne(Mockito.any(Long.class))).thenReturn(Optional.of(user));
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDtoMapped);
+    void getUserById_ValidInput_ReturnUserDto() {
+        when(userService.findOne(Mockito.any(Long.class))).thenReturn(user);
+        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
 
         UserDto userDto = userController.getUserById(1L);
 
-        Assertions.assertEquals(this.userDtoMapped, userDto);
+        Assertions.assertEquals(this.userDto, userDto);
     }
 
     @Test
-    void UserController_GetUserProgExercises_UnsuccessfulUserNotFound() {
-        when(userService.findOne(Mockito.any(Long.class))).thenReturn(Optional.empty());
-
-        Assertions.assertThrows(UserNotFoundException.class, () -> userController.getUserProgExercises(1L));
-    }
-
-    @Test
-    void UserController_GetUserProgExercises_Success() {
-        when(userService.findOne(Mockito.any(Long.class))).thenReturn(Optional.of(user));
+    void getUserProgExercises_ValidInput_ReturnProgExerciseDto() {
+        when(userService.findOne(Mockito.any(Long.class))).thenReturn(user);
 
         List<ProgExerciseDto> userProgExercises = userController.getUserProgExercises(1L);
 
@@ -110,66 +88,67 @@ class UserControllerTest {
     }
 
     @Test
-    void UserController_ModifyUserIdentity_Success() {
-        when(userService.changeIdentity(Mockito.any(Long.class), Mockito.any(String.class), Mockito.any(String.class))).thenReturn(user);
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDtoMapped);
+    void modifyUserIdentity_ValidInput_ReturnUserDto() {
+        when(userInputResolver.resolveInput(Mockito.any(InputUserIdentity.class))).thenReturn(user);
+        when(userService.save(Mockito.any(UserEntity.class))).thenReturn(user);
+        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
 
         UserDto userDto = userController.modifyUserIdentity(createTestInputUserIdentity(1L));
 
-        Assertions.assertEquals(this.userDtoMapped, userDto);
+        Assertions.assertEquals(this.userDto, userDto);
     }
 
     @Test
-    void UserController_ModifyUserRoles_Success() {
-        Set<RoleEntity> roles = new HashSet<RoleEntity>();
-        when(roleService.findMany(Mockito.anySet())).thenReturn(roles);
-        when(userService.changeRoles(Mockito.any(Long.class), Mockito.anySet())).thenReturn(user);
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDtoMapped);
+    void modifyUserRoles_ValidInput_ReturnUserDto() {
+        when(userManager.updateUserRoles(Mockito.any(InputUserRoles.class))).thenReturn(user);
+        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
 
         UserDto userDto = userController.modifyUserRoles(createTestInputUserRoles(1L));
 
-        Assertions.assertEquals(this.userDtoMapped, userDto);
+        Assertions.assertEquals(this.userDto, userDto);
     }
 
     @Test
-    void UserController_modifyUserEmailQuery_Success() {
-        JWToken jwToken = new JWToken("token", LocalDateTime.now());
-        AuthDto returnValue = new AuthDto(jwToken, userDtoMapped);
+    void modifyUserEmail_ValidInput_ReturnUserDto() {
+        UserToken userToken = new UserToken("token", user);
+        AuthDto returnValue = new AuthDto("token", userDto);
 
-        when(userService.changeEmail(Mockito.any(Long.class), Mockito.any(String.class), Mockito.any(String.class))).thenReturn(user);
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDtoMapped);
-        when(authenticationManager.authenticate(Mockito.any(UsernamePasswordAuthenticationToken.class))).thenReturn(null);
-        when(userAuthProvider.generateToken(Mockito.any())).thenReturn(jwToken);
+        when(authService.updateEmail(Mockito.any(InputUserEmail.class))).thenReturn(userToken);
+        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
 
         AuthDto authDto = userController.modifyUserEmail(createTestInputUserEmail(1L, "rawPassword"));
 
-        Assertions.assertEquals(returnValue.getAccessToken(), authDto.getAccessToken());
+        Assertions.assertEquals(returnValue.getToken(), authDto.getToken());
         Assertions.assertEquals(returnValue.getUser(), authDto.getUser());
-        Assertions.assertEquals(returnValue.getExpiration(), authDto.getExpiration());
     }
 
     @Test
-    void UserController_ModifyUserUsername_Success() {
-        when(userService.changeUsername(Mockito.any(Long.class), Mockito.any(String.class))).thenReturn(user);
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDtoMapped);
+    void modifyUserUsername_ValidInput_ReturnUserDto() {
+        when(userInputResolver.resolveInput(Mockito.any(InputUserUsername.class))).thenReturn(user);
+        when(userService.save(Mockito.any(UserEntity.class))).thenReturn(user);
+        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
 
         UserDto userDto = userController.modifyUserUsername(createTestInputUserUsername(1L));
 
-        Assertions.assertEquals(this.userDtoMapped, userDto);
+        Assertions.assertEquals(this.userDto, userDto);
     }
 
     @Test
-    void UserController_ModifyUserPassword_Success() {
-        when(userService.changePassword(Mockito.any(Long.class), Mockito.any(String.class), Mockito.any(String.class))).thenReturn(user);
-        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDtoMapped);
+    void modifyUserPassword_ValidInput_ReturnUserDto() {
+        UserToken userToken = new UserToken("token", user);
+        AuthDto returnValue = new AuthDto("token", userDto);
 
-        UserDto userDto = userController.modifyUserPassword(createTestInputUserPassword(1L, "rawPassword"));
+        when(authService.updatePassword(Mockito.any(InputUserPassword.class))).thenReturn(userToken);
+        when(userMapper.mapTo(Mockito.any(UserEntity.class))).thenReturn(userDto);
 
-        Assertions.assertEquals(this.userDtoMapped, userDto);
+        AuthDto authDto = userController.modifyUserPassword(createTestInputUserPassword(1L, "rawPassword"));
+
+        Assertions.assertEquals(returnValue.getToken(), authDto.getToken());
+        Assertions.assertEquals(returnValue.getUser(), authDto.getUser());
     }
 
     @Test
-    void UserController_DeleteUser_Success() {
+    void deleteUser_ValidInput_ReturnUserId() {
         Long id = userController.deleteUser(1L);
 
         Assertions.assertEquals(1L, id);
