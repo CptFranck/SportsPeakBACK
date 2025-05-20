654c6231c635b17a39d68db671a23edefc168dfe
CptFranck <francois.caillet71@gmail.com>
2025-05-20 19:03:58 +0200

    update InputUserUsername

--- src/main/java/com/CptFranck/SportsPeak/domain/input/user/InputUserUsername.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/input/user/InputUserUsername.java
@@ -3,7 +3,9 @@ package com.CptFranck.SportsPeak.domain.input.user;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
