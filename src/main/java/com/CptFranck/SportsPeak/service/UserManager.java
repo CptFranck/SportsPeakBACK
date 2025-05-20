7608c281ab29e7c74896c36edb2b4d0ab2830b5e
CptFranck <francois.caillet71@gmail.com>
2025-05-20 12:01:41 +0200

    Update UserManager

--- src/main/java/com/CptFranck/SportsPeak/service/UserManager.java
+++ src/main/java/com/CptFranck/SportsPeak/service/UserManager.java
@@ -1,9 +1,12 @@
 package com.CptFranck.SportsPeak.service;
 
 import com.CptFranck.SportsPeak.domain.entity.RoleEntity;
+import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import com.CptFranck.SportsPeak.domain.input.user.InputUserRoles;
 
 public interface UserManager {
 
     RoleEntity saveRole(RoleEntity role);
 
+    UserEntity updateUserRoles(InputUserRoles inputUserRoles);
 }
