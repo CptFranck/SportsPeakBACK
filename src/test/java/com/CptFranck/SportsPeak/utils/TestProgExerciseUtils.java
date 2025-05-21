760e6f1a0edb2cdf0a3e2eff9306fae6025e76bd
CptFranck <francois.caillet71@gmail.com>
2025-05-21 18:56:43 +0200

    Move duplicate assert functions of exercise type entity and dto

--- src/test/java/com/CptFranck/SportsPeak/utils/TestProgExerciseUtils.java
+++ src/test/java/com/CptFranck/SportsPeak/utils/TestProgExerciseUtils.java
@@ -11,6 +11,7 @@ import com.CptFranck.SportsPeak.domain.enumType.VisibilityLabel;
 import com.CptFranck.SportsPeak.domain.input.progExercise.InputNewProgExercise;
 import com.CptFranck.SportsPeak.domain.input.progExercise.InputProgExercise;
 import com.CptFranck.SportsPeak.domain.input.progExercise.InputProgExerciseTrustLabel;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.HashSet;
 import java.util.List;
@@ -79,5 +80,33 @@ public class TestProgExerciseUtils {
                 id,
                 label
         );
+    }
+
+    public static void assertProgExerciseDtoAndEntity(ProgExerciseEntity progExercise, ProgExerciseDto progExerciseDto) {
+        Assertions.assertNotNull(progExerciseDto);
+        Assertions.assertEquals(progExercise.getName(), progExerciseDto.getName());
+        Assertions.assertEquals(progExercise.getNote(), progExerciseDto.getNote());
+        Assertions.assertEquals(progExercise.getVisibility().label, progExerciseDto.getVisibility());
+        Assertions.assertEquals(progExercise.getTrustLabel().label, progExerciseDto.getTrustLabel());
+        Assertions.assertEquals(progExercise.getCreator().getId(), progExerciseDto.getCreator().getId());
+        Assertions.assertEquals(progExercise.getExercise().getId(), progExerciseDto.getExercise().getId());
+        Assertions.assertEquals(progExercise.getTargetSets().size(), progExerciseDto.getTargetSets().size());
+    }
+
+    public static void assertProgExerciseDtoAndInputNew(InputNewProgExercise inputNewProgExercise, ProgExerciseDto progExerciseDto) {
+        Assertions.assertNotNull(progExerciseDto);
+        Assertions.assertEquals(inputNewProgExercise.getName(), progExerciseDto.getName());
+        Assertions.assertEquals(inputNewProgExercise.getNote(), progExerciseDto.getNote());
+        Assertions.assertEquals(inputNewProgExercise.getVisibility(), progExerciseDto.getVisibility());
+        Assertions.assertEquals(inputNewProgExercise.getCreatorId(), progExerciseDto.getCreator().getId());
+        Assertions.assertEquals(inputNewProgExercise.getExerciseId(), progExerciseDto.getExercise().getId());
+    }
+
+    public static void assertProgExerciseDtoAndInput(InputProgExercise inputProgExercise, ProgExerciseDto progExerciseDto) {
+        Assertions.assertNotNull(progExerciseDto);
+        Assertions.assertEquals(inputProgExercise.getName(), progExerciseDto.getName());
+        Assertions.assertEquals(inputProgExercise.getNote(), progExerciseDto.getNote());
+        Assertions.assertEquals(inputProgExercise.getVisibility(), progExerciseDto.getVisibility());
+        Assertions.assertEquals(inputProgExercise.getExerciseId(), progExerciseDto.getExercise().getId());
     }
 }
