fac1184f5e6fd33832d5f0a8f4c508e737fb49ca
CptFranck <francois.caillet71@gmail.com>
2025-05-21 15:19:34 +0200

    add CustomUserDetailsServiceIT

---
+++ src/test/java/com/CptFranck/SportsPeak/integration/services/services/CustomUserDetailsServiceIT.java
@@ -0,0 +1,47 @@
+package com.CptFranck.SportsPeak.integration.services.services;
+
+import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import com.CptFranck.SportsPeak.domain.model.CustomUserDetails;
+import com.CptFranck.SportsPeak.repositories.UserRepository;
+import com.CptFranck.SportsPeak.service.serviceImpl.CustomUserDetailsService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.TestPropertySource;
+
+import static com.CptFranck.SportsPeak.utils.TestUserUtils.createTestUser;
+
+@SpringBootTest()
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
+public class CustomUserDetailsServiceIT {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private UserEntity user;
+
+    @BeforeEach
+    void setUp() {
+        user = userRepository.save(createTestUser(null));
+    }
+
+    @AfterEach
+    void afterEach() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void loadUserByUsername_ValidUse_ReturnListOfUserEntity() {
+        UserDetails userFound = customUserDetailsService.loadUserByUsername(user.getUsername());
+
+        UserDetails userDetails = new CustomUserDetails(user);
+        Assertions.assertEquals(userDetails.getUsername(), userFound.getUsername());
+    }
+}
