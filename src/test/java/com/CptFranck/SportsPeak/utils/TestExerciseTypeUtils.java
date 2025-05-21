5578cd8698e7c35feb6756d96de7f78035303aad
CptFranck <francois.caillet71@gmail.com>
2025-05-21 18:37:00 +0200

    Move duplicate assert functions of exercise type entity and dto

--- src/test/java/com/CptFranck/SportsPeak/utils/TestExerciseTypeUtils.java
+++ src/test/java/com/CptFranck/SportsPeak/utils/TestExerciseTypeUtils.java
@@ -4,6 +4,7 @@ import com.CptFranck.SportsPeak.domain.dto.ExerciseTypeDto;
 import com.CptFranck.SportsPeak.domain.entity.ExerciseTypeEntity;
 import com.CptFranck.SportsPeak.domain.input.exerciseType.InputExerciseType;
 import com.CptFranck.SportsPeak.domain.input.exerciseType.InputNewExerciseType;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -52,5 +53,26 @@ public class TestExerciseTypeUtils {
                 "goal",
                 new ArrayList<>()
         );
+    }
+
+    public static void asserEqualExerciseType(ExerciseTypeEntity exerciseTypeExpected, ExerciseTypeEntity exerciseTypeObtain) {
+        Assertions.assertEquals(exerciseTypeExpected.getId(), exerciseTypeObtain.getId());
+        Assertions.assertEquals(exerciseTypeExpected.getName(), exerciseTypeObtain.getName());
+        Assertions.assertEquals(exerciseTypeExpected.getGoal(), exerciseTypeObtain.getGoal());
+        Assertions.assertEquals(exerciseTypeExpected.getExercises().size(), exerciseTypeObtain.getExercises().size());
+    }
+
+    public static void assertExerciseTypeDtoAndEntity(ExerciseTypeEntity exerciseTypeEntity, ExerciseTypeDto exerciseTypeDto) {
+        Assertions.assertNotNull(exerciseTypeDto);
+        Assertions.assertEquals(exerciseTypeEntity.getName(), exerciseTypeDto.getName());
+        Assertions.assertEquals(exerciseTypeEntity.getGoal(), exerciseTypeDto.getGoal());
+        Assertions.assertEquals(exerciseTypeEntity.getExercises().size(), exerciseTypeDto.getExercises().size());
+    }
+
+    public static void assertExerciseTypeDtoAndInput(InputNewExerciseType inputNewExercise, ExerciseTypeDto exerciseDto) {
+        Assertions.assertNotNull(exerciseDto);
+        Assertions.assertEquals(inputNewExercise.getName(), exerciseDto.getName());
+        Assertions.assertEquals(inputNewExercise.getGoal(), exerciseDto.getGoal());
+        Assertions.assertEquals(inputNewExercise.getExerciseIds().size(), exerciseDto.getExercises().size());
     }
 }
