c2839c5507a9c05cc297daea0ce34dc5d59c9cdc
CptFranck <francois.caillet71@gmail.com>
2025-05-21 16:33:35 +0200

    Add RoleManagerImplTest

---
+++ src/test/java/com/CptFranck/SportsPeak/unit/services/managers/RoleManagerImplTest.java
@@ -0,0 +1,50 @@
+package com.CptFranck.SportsPeak.unit.services.managers;
+
+import com.CptFranck.SportsPeak.domain.entity.PrivilegeEntity;
+import com.CptFranck.SportsPeak.service.PrivilegeService;
+import com.CptFranck.SportsPeak.service.RoleService;
+import com.CptFranck.SportsPeak.service.managerImpl.RoleManagerImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.CptFranck.SportsPeak.utils.TestPrivilegeUtils.createTestPrivilege;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class RoleManagerImplTest {
+
+    @InjectMocks
+    private RoleManagerImpl roleManager;
+
+    @Mock
+    private PrivilegeService privilegeService;
+
+    @Mock
+    private RoleService roleService;
+
+    @Test
+    void savePrivilege_AddNewPrivilege_ReturnPrivilegeEntity() {
+        PrivilegeEntity privilege = createTestPrivilege(null, 0);
+        when(privilegeService.save(Mockito.any(PrivilegeEntity.class))).thenReturn(privilege);
+
+        PrivilegeEntity privilegeResolved = roleManager.savePrivilege(privilege);
+
+        Assertions.assertEquals(privilege, privilegeResolved);
+    }
+
+    @Test
+    void savePrivilege_UpdatePrivilege_ReturnPrivilegeEntity() {
+        PrivilegeEntity privilege = createTestPrivilege(1L, 0);
+        when(privilegeService.findOne(Mockito.any(Long.class))).thenReturn(privilege);
+        when(privilegeService.save(Mockito.any(PrivilegeEntity.class))).thenReturn(privilege);
+
+        PrivilegeEntity privilegeResolved = roleManager.savePrivilege(privilege);
+
+        Assertions.assertEquals(privilege, privilegeResolved);
+    }
+}
