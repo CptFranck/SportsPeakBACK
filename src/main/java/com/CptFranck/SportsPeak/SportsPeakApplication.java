aa4c28e50c47f045a619fe22b8813ee18130721a
CptFranck <francois.caillet71@gmail.com>
2025-05-26 19:03:41 +0200

    update main

--- src/main/java/com/CptFranck/SportsPeak/SportsPeakApplication.java
+++ src/main/java/com/CptFranck/SportsPeak/SportsPeakApplication.java
@@ -1,25 +1,20 @@
 package com.CptFranck.SportsPeak;
 
+import com.CptFranck.SportsPeak.config.graphql.DgsProperties;
+import com.CptFranck.SportsPeak.config.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        StorageProperties.class,
+        DgsProperties.class
+})
 public class SportsPeakApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SportsPeakApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/service/api/graphql").allowedOrigins("http://localhost:4200");
-            }
-        };
-    }
 }
