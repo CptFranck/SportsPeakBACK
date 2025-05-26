ba025f71d36c94f4c29859e0bb607515a19ea50b
CptFranck <francois.caillet71@gmail.com>
2025-05-26 16:58:08 +0200

    add HttpsRedirectionConfig

---
+++ src/main/java/com/CptFranck/SportsPeak/config/security/HttpsRedirectionConfig.java
@@ -0,0 +1,25 @@
+package com.CptFranck.SportsPeak.config.security;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsRedirectionConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return factory -> factory.addAdditionalTomcatConnectors(httpConnector());
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
