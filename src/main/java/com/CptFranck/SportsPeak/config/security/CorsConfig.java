dab845b596e7b536e8b2dbe663258730b41de0c3
CptFranck <francois.caillet71@gmail.com>
2025-06-04 12:21:25 +0200

    update CorsConfig

--- src/main/java/com/CptFranck/SportsPeak/config/security/CorsConfig.java
+++ src/main/java/com/CptFranck/SportsPeak/config/security/CorsConfig.java
@@ -1,21 +1,33 @@
 package com.CptFranck.SportsPeak.config.security;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NotNull CorsRegistry registry) {
-                registry.addMapping("/service/api/graphql")
-                        .allowedOrigins("http://localhost:4200");
-            }
-        };
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration restApiCors = new CorsConfiguration();
+        restApiCors.setAllowedOrigins(List.of("http://localhost:4200"));
+        restApiCors.setAllowedMethods(List.of("GET", "POST")); //, "PUT", "DELETE", "OPTIONS"
+        restApiCors.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
+        restApiCors.setAllowCredentials(true);
+
+        CorsConfiguration graphqlApiCors = new CorsConfiguration();
+        graphqlApiCors.setAllowedOrigins(List.of("http://localhost:4200"));
+        graphqlApiCors.setAllowedMethods(List.of("POST", "OPTIONS"));
+        graphqlApiCors.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        graphqlApiCors.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/service/api/graphql/**", graphqlApiCors);
+        source.registerCorsConfiguration("/service/api/rest/**", restApiCors);
+
+        return source;
     }
 }
