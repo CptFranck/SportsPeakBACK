beb6b384734e92ad9106daa26a79a02fabdacc09
CptFranck <francois.caillet71@gmail.com>
2025-06-11 11:57:56 +0200

    fix

--- src/main/java/com/CptFranck/SportsPeak/SportsPeakApplication.java
+++ src/main/java/com/CptFranck/SportsPeak/SportsPeakApplication.java
@@ -1,16 +1,12 @@
 package com.CptFranck.SportsPeak;
 
-import com.CptFranck.SportsPeak.config.graphql.DgsProperties;
 import com.CptFranck.SportsPeak.config.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-        StorageProperties.class,
-        DgsProperties.class
-})
+@EnableConfigurationProperties({StorageProperties.class})
 public class SportsPeakApplication {
 
     public static void main(String[] args) {
