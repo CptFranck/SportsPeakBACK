a6bf7f1af90c2b203631acdda3840e2b059525de
CptFranck <francois.caillet71@gmail.com>
2025-06-11 11:58:13 +0200

    update CorsConfig

--- src/main/java/com/CptFranck/SportsPeak/config/security/CorsConfig.java
+++ src/main/java/com/CptFranck/SportsPeak/config/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.CptFranck.SportsPeak.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,18 +11,30 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    private static final List<String> HEADERS_ACCEPTED = List.of(
+            "Authorization",
+            "Content-Type",
+            "Accept",
+            "Origin",
+            "X-Requested-With"
+    );
+    private static final List<String> METHODS_REST = List.of("GET", "POST");
+    private static final List<String> METHODS_GRAPHQL = List.of("POST", "OPTIONS");
+    @Value("${security.cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration restApiCors = new CorsConfiguration();
-        restApiCors.setAllowedOrigins(List.of("http://localhost:4200"));
-        restApiCors.setAllowedMethods(List.of("GET", "POST")); //, "PUT", "DELETE", "OPTIONS"
-        restApiCors.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
+        restApiCors.setAllowedOrigins(List.of(allowedOrigin));
+        restApiCors.setAllowedMethods(METHODS_REST);
+        restApiCors.setAllowedHeaders(HEADERS_ACCEPTED);
         restApiCors.setAllowCredentials(true);
 
         CorsConfiguration graphqlApiCors = new CorsConfiguration();
-        graphqlApiCors.setAllowedOrigins(List.of("http://localhost:4200"));
-        graphqlApiCors.setAllowedMethods(List.of("POST", "OPTIONS"));
-        graphqlApiCors.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        graphqlApiCors.setAllowedOrigins(List.of(allowedOrigin));
+        graphqlApiCors.setAllowedMethods(METHODS_GRAPHQL);
+        graphqlApiCors.setAllowedHeaders(HEADERS_ACCEPTED);
         graphqlApiCors.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
