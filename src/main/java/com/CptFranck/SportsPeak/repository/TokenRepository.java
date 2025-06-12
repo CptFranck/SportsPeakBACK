bbd9a2acd1d875215b3035de9ebe383a385ca3cc
CptFranck <francois.caillet71@gmail.com>
2025-06-12 16:00:07 +0200

    update token repository

--- src/main/java/com/CptFranck/SportsPeak/repository/TokenRepository.java
+++ src/main/java/com/CptFranck/SportsPeak/repository/TokenRepository.java
@@ -2,6 +2,17 @@ package com.CptFranck.SportsPeak.repository;
 
 import com.CptFranck.SportsPeak.domain.entity.TokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
+
+    @Query("SELECT t FROM TokenEntity t" +
+            " INNER JOIN UserEntity u on u.id = t.user.id" +
+            " WHERE u.id = :userId AND t.expired = false AND t.revoked = false")
+    List<TokenEntity> findAllValidTokenByUser(Long userId);
+
+    Optional<TokenEntity> findByToken(String token);
 }
