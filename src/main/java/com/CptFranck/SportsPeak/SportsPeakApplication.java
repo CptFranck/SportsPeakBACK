4b8b110273f94e345e4283aae3c1e99d5651afc9
CptFranck <francois.caillet71@gmail.com>
2025-06-13 17:30:32 +0200

    update SportsPeakApplication

--- src/main/java/com/CptFranck/SportsPeak/SportsPeakApplication.java
+++ src/main/java/com/CptFranck/SportsPeak/SportsPeakApplication.java
@@ -1,9 +1,12 @@
 package com.CptFranck.SportsPeak;
 
 import com.CptFranck.SportsPeak.config.storage.StorageProperties;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableConfigurationProperties({StorageProperties.class})
@@ -13,4 +16,8 @@ public class SportsPeakApplication {
         SpringApplication.run(SportsPeakApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
