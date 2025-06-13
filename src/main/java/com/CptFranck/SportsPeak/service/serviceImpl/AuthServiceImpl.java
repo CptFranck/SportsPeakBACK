b98174418141e39974a27d4983168e06358fe7cb
CptFranck <francois.caillet71@gmail.com>
2025-06-13 17:30:42 +0200

    update AuthServiceImpl

--- src/main/java/com/CptFranck/SportsPeak/service/serviceImpl/AuthServiceImpl.java
+++ src/main/java/com/CptFranck/SportsPeak/service/serviceImpl/AuthServiceImpl.java
@@ -156,11 +156,19 @@ public class AuthServiceImpl implements AuthService {
 
         String accessToken = jwtUtils.generateAccessToken(userDetails);
 
-        TokenEntity tokenAccessEntity = new TokenEntity(tokenService.hashToken(accessToken), TokenType.ACCESS, user);
+        TokenEntity tokenAccessEntity = new TokenEntity(
+                tokenService.hashToken(accessToken),
+                TokenType.ACCESS,
+                jwtUtils.extractExpiration(accessToken).toInstant(),
+                user);
         tokenService.save(tokenAccessEntity);
 
         String refreshToken = jwtUtils.generateRefreshToken(userDetails);
-        TokenEntity tokenRefreshEntity = new TokenEntity(tokenService.hashToken(refreshToken), TokenType.REFRESH, user);
+        TokenEntity tokenRefreshEntity = new TokenEntity(
+                tokenService.hashToken(refreshToken),
+                TokenType.REFRESH,
+                jwtUtils.extractExpiration(refreshToken).toInstant(),
+                user);
         tokenService.save(tokenRefreshEntity);
 
         return new UserTokens(user, accessToken, refreshToken);
