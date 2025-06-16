6e8bfcadbde7abd217cebfff0964670fa9bbbd31
CptFranck <francois.caillet71@gmail.com>
2025-06-16 15:31:20 +0200

    update UserTokens

--- src/main/java/com/CptFranck/SportsPeak/domain/model/UserTokens.java
+++ src/main/java/com/CptFranck/SportsPeak/domain/model/UserTokens.java
@@ -1,13 +1,14 @@
 package com.CptFranck.SportsPeak.domain.model;
 
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
-public class UserTokens {
-    private final UserEntity user;
-    private final String accessToken;
+public class UserTokens extends UserAccessToken {
     private final String refreshToken;
+
+    public UserTokens(UserEntity user, String accessToken, String refreshToken) {
+        super(user, accessToken);
+        this.refreshToken = refreshToken;
+    }
 }
