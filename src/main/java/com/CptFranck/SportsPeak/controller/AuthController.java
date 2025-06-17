51223deaf357c8cd815f29b040c4cd0b83ff58b3
CptFranck <francois.caillet71@gmail.com>
2025-06-17 16:39:47 +0200

    update auth controller with try catch

--- src/main/java/com/CptFranck/SportsPeak/controller/AuthController.java
+++ src/main/java/com/CptFranck/SportsPeak/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.CptFranck.SportsPeak.config.security.jwt.RefreshTokenCookieHandler;
 import com.CptFranck.SportsPeak.domain.dto.AuthDto;
 import com.CptFranck.SportsPeak.domain.dto.UserDto;
 import com.CptFranck.SportsPeak.domain.entity.UserEntity;
+import com.CptFranck.SportsPeak.domain.exception.token.RefreshTokenExpiredException;
 import com.CptFranck.SportsPeak.domain.exception.token.TokenMissingException;
 import com.CptFranck.SportsPeak.domain.input.credentials.InputCredentials;
 import com.CptFranck.SportsPeak.domain.input.credentials.RegisterInput;
@@ -14,6 +15,7 @@ import com.CptFranck.SportsPeak.service.TokenService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.InputArgument;
+import com.netflix.graphql.dgs.exceptions.DgsBadRequestException;
 import graphql.GraphQLContext;
 import graphql.schema.DataFetchingEnvironment;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -57,14 +59,18 @@ public class AuthController {
     public AuthDto refreshToken(DataFetchingEnvironment env) {
         GraphQLContext context = env.getGraphQlContext();
         String refreshToken = context.get("refreshToken");
+        try {
+            if (refreshToken == null) throw new TokenMissingException("Refresh token missing");
 
-        if (refreshToken == null)
-            throw new TokenMissingException("Refresh token missing");
+            UserTokens userToken = authService.refreshAccessToken(refreshToken);
+            refreshTokenCookieHandler.addRefreshTokenToCookie(userToken.getRefreshToken());
 
-        UserTokens userToken = authService.refreshAccessToken(refreshToken);
-        refreshTokenCookieHandler.addRefreshTokenToCookie(userToken.getRefreshToken());
-
-        return new AuthDto(userToken.getAccessToken(), userMapper.mapTo(userToken.getUser()));
+            return new AuthDto(userToken.getAccessToken(), userMapper.mapTo(userToken.getUser()));
+        } catch (RefreshTokenExpiredException | TokenMissingException e) {
+            SecurityContextHolder.clearContext();
+            refreshTokenCookieHandler.clearRefreshTokenCookie();
+            throw new DgsBadRequestException("REFRESH_TOKEN_EXPIRED");
+        }
     }
 
     @DgsMutation
@@ -74,15 +80,15 @@ public class AuthController {
         String accessToken = context.get("accessToken");
         String refreshToken = context.get("refreshToken");
 
+        SecurityContextHolder.clearContext();
+        refreshTokenCookieHandler.clearRefreshTokenCookie();
+
         if (accessToken == null || refreshToken == null)
-            throw new TokenMissingException("access and / or refresh missing");
+            throw new TokenMissingException("Access and / or refresh missing");
 
         tokenService.revokeToken(accessToken);
         tokenService.revokeToken(refreshToken);
 
-        SecurityContextHolder.clearContext();
-
-        refreshTokenCookieHandler.clearRefreshTokenCookie();
         return true;
     }
 }
