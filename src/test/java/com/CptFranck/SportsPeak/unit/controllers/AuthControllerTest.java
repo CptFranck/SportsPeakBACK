bafa02cfb8538d0d31b25ba6ffa72303653aee23
CptFranck <francois.caillet71@gmail.com>
2025-06-19 18:46:29 +0200

    update AuthControllerTest

--- src/test/java/com/CptFranck/SportsPeak/unit/controllers/AuthControllerTest.java
+++ src/test/java/com/CptFranck/SportsPeak/unit/controllers/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.CptFranck.SportsPeak.unit.controllers;
 
+import com.CptFranck.SportsPeak.config.security.jwt.RefreshTokenCookieHandler;
 import com.CptFranck.SportsPeak.controller.AuthController;
 import com.CptFranck.SportsPeak.domain.dto.AuthDto;
 import com.CptFranck.SportsPeak.domain.dto.UserDto;
@@ -29,6 +30,9 @@ public class AuthControllerTest {
 
     @InjectMocks
     private AuthController authController;
+
+    @Mock
+    private RefreshTokenCookieHandler refreshTokenCookieHandler;
 
     @Mock
     private AuthService authService;
