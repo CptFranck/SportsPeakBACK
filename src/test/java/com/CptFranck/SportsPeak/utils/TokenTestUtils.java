7a5b24a56ea6cb67902bb348f63fdcb452ed8dca
CptFranck <francois.caillet71@gmail.com>
2025-06-20 16:55:37 +0200

    update TokenTestUtils with assertUserTokens

--- src/test/java/com/CptFranck/SportsPeak/utils/TokenTestUtils.java
+++ src/test/java/com/CptFranck/SportsPeak/utils/TokenTestUtils.java
@@ -1,9 +1,16 @@
 package com.CptFranck.SportsPeak.utils;
 
 import com.CptFranck.SportsPeak.domain.entity.TokenEntity;
+import com.CptFranck.SportsPeak.domain.model.UserTokens;
 import org.junit.jupiter.api.Assertions;
 
 public class TokenTestUtils {
+
+    public static void assertUserTokens(UserTokens expected, UserTokens actual) {
+        Assertions.assertEquals(expected.getAccessToken(), actual.getAccessToken());
+        Assertions.assertEquals(expected.getRefreshToken(), actual.getRefreshToken());
+        Assertions.assertEquals(expected.getUser(), actual.getUser());
+    }
 
     public static void assertTokenEntity(TokenEntity expected, TokenEntity actual, boolean justBeenCreated) {
         if (!justBeenCreated)
